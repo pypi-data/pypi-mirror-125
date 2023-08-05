28688dd74d80a6f2a7dab773bac7639b4154bf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:17 +0000

    Add package resolwe/29.0.0a5/resolwe-29.0.0a5.tar.gz

---
+++ packages/resolwe/resolwe-29.0.0a5.tar.gz/resolwe-29.0.0a5/resolwe/flow/views/entity.py
@@ -0,0 +1,129 @@
+"""Entity viewset."""
+from django.db.models import Prefetch, Q
+
+from rest_framework import exceptions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from resolwe.flow.filters import EntityFilter
+from resolwe.flow.models import Collection, DescriptorSchema, Entity
+from resolwe.flow.serializers import EntitySerializer
+from resolwe.permissions.models import PermissionModel, get_anonymous_user
+from resolwe.permissions.utils import get_user
+
+from .collection import BaseCollectionViewSet
+from .utils import get_collection_for_user
+
+
+class EntityViewSet(BaseCollectionViewSet):
+    """API view for entities."""
+
+    serializer_class = EntitySerializer
+    filter_class = EntityFilter
+    qs_permission_model = PermissionModel.objects.select_related("user", "group")
+    qs_collection_ds = DescriptorSchema.objects.select_related("contributor")
+    qs_collection = Collection.objects.select_related("contributor")
+    qs_collection = qs_collection.prefetch_related(
+        "data",
+        "entity_set",
+        Prefetch("descriptor_schema", queryset=qs_collection_ds),
+    )
+    qs_descriptor_schema = DescriptorSchema.objects.select_related("contributor")
+    queryset = Entity.objects.select_related("contributor")
+
+    def get_queryset(self):
+        """Get the queryset for the given request.
+
+        Prefetch only permissions for the given user, not all of them. This is
+        only possible with the request in the context.
+        """
+        user = get_user(self.request.user)
+        filters = Q(user=user) | Q(group__in=user.groups.all())
+        anonymous_user = get_anonymous_user()
+        if user != anonymous_user:
+            filters |= Q(user=anonymous_user)
+
+        qs_permission_model = self.qs_permission_model.filter(filters)
+
+        return self.queryset.prefetch_related(
+            "data",
+            Prefetch("permission_group__permissions", queryset=qs_permission_model),
+            Prefetch("collection", queryset=self.qs_collection),
+            Prefetch("descriptor_schema", queryset=self.qs_descriptor_schema),
+        )
+
+    def _get_entities(self, user, ids):
+        """Return entities queryset based on provided entity ids."""
+        queryset = Entity.objects.filter(id__in=ids).filter_for_user(user)
+        actual_ids = queryset.values_list("id", flat=True)
+        missing_ids = list(set(ids) - set(actual_ids))
+        if missing_ids:
+            raise exceptions.ParseError(
+                "Entities with the following ids not found: {}".format(
+                    ", ".join(map(str, missing_ids))
+                )
+            )
+
+        return queryset
+
+    @action(detail=False, methods=["post"])
+    def move_to_collection(self, request, *args, **kwargs):
+        """Move samples from source to destination collection."""
+        ids = self.get_ids(request.data)
+        dst_collection_id = self.get_id(request.data, "destination_collection")
+        dst_collection = get_collection_for_user(dst_collection_id, request.user)
+
+        entity_qs = self._get_entities(request.user, ids)
+        entity_qs.move_to_collection(dst_collection)
+
+        return Response()
+
+    # NOTE: This can be deleted when DRF will support select_for_update
+    #       on updates and ResolweUpdateModelMixin will use it.
+    #       https://github.com/encode/django-rest-framework/issues/4675
+    def update(self, request, *args, **kwargs):
+        """Update an entity.
+
+        Original queryset produces a temporary database table whose rows
+        cannot be selected for an update. As a workaround, we patch
+        get_queryset function to return only Entity objects without
+        additional data that is not needed for the update.
+        """
+        orig_get_queryset = self.get_queryset
+
+        def patched_get_queryset():
+            """Patched get_queryset method."""
+            entity_ids = orig_get_queryset().values_list("id", flat=True)
+            return Entity.objects.filter(id__in=entity_ids)
+
+        self.get_queryset = patched_get_queryset
+
+        resp = super().update(request, *args, **kwargs)
+
+        self.get_queryset = orig_get_queryset
+        return resp
+
+    @action(detail=False, methods=["post"])
+    def duplicate(self, request, *args, **kwargs):
+        """Duplicate (make copy of) ``Entity`` models."""
+        if not request.user.is_authenticated:
+            raise exceptions.NotFound
+
+        inherit_collection = request.data.get("inherit_collection", False)
+        ids = self.get_ids(request.data)
+        queryset = Entity.objects.filter(id__in=ids).filter_for_user(request.user)
+        actual_ids = queryset.values_list("id", flat=True)
+        missing_ids = list(set(ids) - set(actual_ids))
+        if missing_ids:
+            raise exceptions.ParseError(
+                "Entities with the following ids not found: {}".format(
+                    ", ".join(map(str, missing_ids))
+                )
+            )
+
+        duplicated = queryset.duplicate(
+            contributor=request.user, inherit_collection=inherit_collection
+        )
+
+        serializer = self.get_serializer(duplicated, many=True)
+        return Response(serializer.data)
---
+++ packages/resolwe/resolwe-29.0.0a5.tar.gz/resolwe-29.0.0a5/resolwe/permissions/shortcuts.py
@@ -0,0 +1,176 @@
+""".. Ignore pydocstyle D400.
+
+=====================
+Permissions shortcuts
+=====================
+
+.. autofunction:: _group_groups
+.. autofunction:: get_object_perms
+
+"""
+from typing import Dict, List, Optional, Tuple
+
+from django.contrib.auth.models import Group, User
+from django.db import models
+
+from resolwe.permissions.models import (
+    PermissionList,
+    PermissionModel,
+    UserOrGroup,
+    get_anonymous_user,
+)
+from resolwe.permissions.utils import get_identity, model_has_permissions
+
+
+def get_user_group_perms(
+    user_or_group: UserOrGroup, obj: models.Model
+) -> Tuple[PermissionList, List[Tuple[int, str, PermissionList]]]:
+    """Get permissions for the user/group on the given model.
+
+    :returns: the tuple cosisting of permissions for the given user and
+    a list of permissions for every group the user belongs to.
+    """
+    if not model_has_permissions(obj):
+        return [], []
+
+    entity, entity_name = get_identity(user_or_group)
+    user_perms = []
+    groups_perms = []
+    if entity_name == "user":
+        if not entity.is_active:
+            return [], []
+        user_perms = obj.get_permissions(entity)
+        groups = entity.groups.all()
+    else:
+        groups = [entity]
+
+    # Get group permissions, iterate through queryset to lower the numbers of
+    # produced queries.
+    for permission_model in PermissionModel.objects.filter(
+        group__in=groups, permission_group=obj.permission_group
+    ).order_by("group__pk"):
+        groups_perms.append(
+            (
+                permission_model.group_id,
+                permission_model.group.name,
+                permission_model.permissions,
+            )
+        )
+
+    return user_perms, groups_perms
+
+
+def get_users_with_perms(obj: models.Model) -> Dict[User, PermissionList]:
+    """Get users with permissions on the given object.
+
+    Only permissions attached to user directly are consider, permissions
+    attached to the user groups are ignored.
+    """
+    return {
+        permission_model.user: permission_model.permissions
+        for permission_model in PermissionModel.objects.filter(
+            permission_group=obj.permission_group, user__isnull=False
+        )
+    } or {}
+
+
+def get_groups_with_perms(obj: models.Model) -> Dict[Group, PermissionList]:
+    """Get groups with permissions on the given object."""
+    return {
+        permission_model.group: permission_model.permissions
+        for permission_model in PermissionModel.objects.filter(
+            permission_group=obj.permission_group, group__isnull=False
+        )
+    } or {}
+
+
+def get_object_perms(obj: models.Model, user: Optional[User] = None) -> Dict:
+    """Return permissions for given object in Resolwe specific format.
+
+    Function returns permissions for given object ``obj`` in following
+    format::
+
+       {
+           "type": "group"/"user"/"public",
+           "id": <group_or_user_id>,
+           "name": <group_or_user_name>,
+           "permissions": [<first_permission>, <second_permission>,...]
+       }
+
+    For ``public`` type ``id`` and ``name`` keys are omitted.
+
+    If ``user`` parameter is given, permissions are limited only to
+    given user, groups he belongs to and public permissions.
+
+    :param obj: Resolwe's DB model's instance
+    :type obj: a subclass of :class:`~resolwe.flow.models.base.BaseModel`
+    :param user: Django user
+    :type user: :class:`~django.contrib.auth.models.User` or :data:`None`
+    :return: list of permissions object in described format
+    :rtype: list
+
+    """
+    perms_list = []
+    public_permissions = []
+    anonymous_user = get_anonymous_user()
+
+    if user is not None:
+        if user.is_authenticated:
+            user_permissions, groups_permissions = get_user_group_perms(user, obj)
+        else:
+            user_permissions, groups_permissions = [], []
+
+        if user_permissions:
+            perms_list.append(
+                {
+                    "type": "user",
+                    "id": user.pk,
+                    "name": user.get_full_name() or user.username,
+                    "username": user.username,
+                    "permissions": [str(permission) for permission in user_permissions],
+                }
+            )
+
+        if groups_permissions:
+            for group_id, group_name, permissions in groups_permissions:
+                perms_list.append(
+                    {
+                        "type": "group",
+                        "id": group_id,
+                        "name": group_name,
+                        "permissions": [str(permission) for permission in permissions],
+                    }
+                )
+    else:
+        for user, permissions in get_users_with_perms(obj).items():
+            if user == anonymous_user:
+                # public user permissions are considered bellow
+                public_permissions = permissions
+                continue
+            perms_list.append(
+                {
+                    "type": "user",
+                    "id": user.pk,
+                    "name": user.get_full_name() or user.username,
+                    "username": user.username,
+                    "permissions": permissions,
+                }
+            )
+
+        for group, permissions in get_groups_with_perms(obj).items():
+            perms_list.append(
+                {
+                    "type": "group",
+                    "id": group.pk,
+                    "name": group.name,
+                    "permissions": permissions,
+                }
+            )
+    # Retrieve public permissions only if they are not set.
+    if not public_permissions:
+        public_permissions = get_user_group_perms(anonymous_user, obj)[0]
+
+    if public_permissions:
+        perms_list.append({"type": "public", "permissions": public_permissions})
+
+    return perms_list
