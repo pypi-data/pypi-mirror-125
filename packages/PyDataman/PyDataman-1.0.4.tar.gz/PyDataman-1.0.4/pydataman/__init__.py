e43e523c527ddf7455b4979c1d9245b759b4df43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package PyDataman/1.0.4/PyDataman-1.0.4.tar.gz

---
+++ packages/PyDataman/PyDataman-1.0.4.tar.gz/PyDataman-1.0.4/pydataman/__init__.py
@@ -0,0 +1,10 @@
+from .pydataman import InitializeDatabase
+from .pydataman import NewFile
+from .pydataman import CloudVariable
+from .pydataman import save
+from .pydataman import read
+from .pydataman import read_str
+from .pydataman import exists
+from .pydataman import get_all_as_dict
+from .pydataman import is_android
+
