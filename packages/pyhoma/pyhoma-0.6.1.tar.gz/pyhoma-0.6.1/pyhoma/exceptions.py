08b42c51c93a47fac099ea885a6511ee2c27c11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:04 +0000

    Add package pyhoma/0.6.1/pyhoma-0.6.1.tar.gz

---
+++ packages/pyhoma/pyhoma-0.6.1.tar.gz/pyhoma-0.6.1/pyhoma/exceptions.py
@@ -0,0 +1,48 @@
+class BadCredentialsException(Exception):
+    pass
+
+
+class InvalidCommandException(Exception):
+    pass
+
+
+class NotAuthenticatedException(Exception):
+    pass
+
+
+class TooManyExecutionsException(Exception):
+    pass
+
+
+class TooManyRequestsException(Exception):
+    pass
+
+
+class MaintenanceException(Exception):
+    pass
+
+
+class InvalidEventListenerIdException(Exception):
+    pass
+
+
+class NoRegisteredEventListenerException(Exception):
+    pass
+
+
+# Nexity
+class NexityBadCredentialsException(Exception):
+    pass
+
+
+class NexityServiceException(Exception):
+    pass
+
+
+# CozyTouch
+class CozyTouchBadCredentialsException(Exception):
+    pass
+
+
+class CozyTouchServiceException(Exception):
+    pass
