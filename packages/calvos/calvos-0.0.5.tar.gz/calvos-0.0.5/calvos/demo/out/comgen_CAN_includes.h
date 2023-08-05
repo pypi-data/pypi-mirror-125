f5989f9d6b83a5a8c7741a8a4e2119e897fb6487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:12 +0000

    Add package calvos/0.0.5/calvos-0.0.5.tar.gz

---
+++ packages/calvos/calvos-0.0.5.tar.gz/calvos-0.0.5/calvos/demo/out/comgen_CAN_includes.h
@@ -0,0 +1,63 @@
+/*============================================================================*/
+/*                           calvOS Project                                   */
+/*============================================================================*/
+/** \file		comgen_CAN_includes.h                                         */
+/** \brief     	Header file for including all the network headers.
+ *  \details   	Includes al headers related to the network to ease the
+ *  			integration of the network in the target project.
+ *  \author    	Carlos Calvillo
+ *  \version   	1.0
+ *  \date      	2021-10-08
+ *  \copyright 	2021 Carlos Calvillo.
+ */
+/*============================================================================*/
+/*  This file is part of calvOS project <https://github.com/calcore-io/calvos>.
+ *
+ *  calvOS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  calvOS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with calvOS.  If not, see <https://www.gnu.org/licenses/>. */
+/*============================================================================*/
+/*-----------------------------------------------------------------------------
+ * This file was generated on (yyyy.mm.dd::hh:mm:ss): 2021.10.26::10:22:46
+ * Generated from following source(s):
+ *     Network file: "G:\devproj\github\calvos_0_0_5\calvos\calvos-engine\..
+                      calvos\demo\usr_in\template - CAN Network Definition.ods"
+ *     Network name: "CAN-B"
+ *     Network id: "B"
+ *     Network date: "12/Mar/2021"
+ *     Network version: "2"
+ -----------------------------------------------------------------------------*/
+#ifndef COMGEN_CAN_INCLUDES_H
+#define COMGEN_CAN_INCLUDES_H
+
+#include "calvos.h"
+
+#include "comgen_CAN_common.h"
+#include "comgen_CAN_network.h"
+#include "comgen_CAN_DCM_DR_hal.h"
+#include "comgen_CAN_DCM_PS_hal.h"
+#include "comgen_CAN_DCM_RL_hal.h"
+#include "comgen_CAN_DCM_RR_hal.h"
+#include "comgen_CAN_DCM_DR_node_network.h"
+#include "comgen_CAN_DCM_PS_node_network.h"
+#include "comgen_CAN_DCM_RL_node_network.h"
+#include "comgen_CAN_DCM_RR_node_network.h"
+#include "comgen_CAN_DCM_DR_core.h"
+#include "comgen_CAN_DCM_PS_core.h"
+#include "comgen_CAN_DCM_RL_core.h"
+#include "comgen_CAN_DCM_RR_core.h"
+#include "comgen_CAN_DCM_DR_callbacks.h"
+#include "comgen_CAN_DCM_PS_callbacks.h"
+#include "comgen_CAN_DCM_RL_callbacks.h"
+#include "comgen_CAN_DCM_RR_callbacks.h"
+
+#endif /* COMGEN_CAN_INCLUDES_H */
