a46b9428a5c915d7aaea4ab86a5ce857b3fc3845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:31 +0000

    Add package VecDB/0.11.0/VecDB-0.11.0.tar.gz

---
+++ packages/VecDB/VecDB-0.11.0.tar.gz/VecDB-0.11.0/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+def read(rel_path):
+    """Read lines from given file"""
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    """Read __version__ from given file"""
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError(f"Unable to find a valid __version__ string in {rel_path}.")
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='VecDB',
+    version=get_version("vecdb/__init__.py"),
+    url='',
+    author='Relevance AI',
+    author_email='dev@vctr.ai',
+    description='No description',
+    packages=find_packages(),    
+    install_requires=required,
+)
+
