a3a3987909eac0cdb7a657dc85ffe6b618f40aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:27 +0000

    Add package iTude/0.1.4/iTude-0.1.4.tar.gz

---
+++ packages/iTude/iTude-0.1.4.tar.gz/iTude-0.1.4/iTude/__init__.py
@@ -0,0 +1,8 @@
+# This allows to use "from utils import conversion" in __main__.py
+# import os, sys
+
+__version__ = '0.1.4'
+__description__ = 'A TOOL for Attitude Longitude of Site'
+__author__ = 'Chaoqun Chen'
+__author_email__ = 'chen.chaoqun.ext@99cloud.net'
+__url__ = 'https://github.com/tsunchen/script'
