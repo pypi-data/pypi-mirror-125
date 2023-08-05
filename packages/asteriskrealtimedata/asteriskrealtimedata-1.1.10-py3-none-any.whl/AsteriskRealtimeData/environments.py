feab1abd2bd2483398257db610d99bc8a24f94f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:20 +0000

    Add package asteriskrealtimedata/1.1.10/asteriskrealtimedata-1.1.10-py3-none-any.whl

---
+++ packages/asteriskrealtimedata/asteriskrealtimedata-1.1.10-py3-none-any.whl/AsteriskRealtimeData/environments.py
@@ -0,0 +1,11 @@
+import os
+from antidote import Constants, const
+
+
+class Config(Constants):
+    HOST = const[str](os.environ.get("MONGODB_HOST", "127.0.0.1"))
+    # HOST = const[str](os.environ.get("MONGODB_HOST", "192.168.0.85"))
+    PORT = const[int](os.environ.get("MONGODB_PORT", 27017))
+    DATABASE = const[str](os.environ.get("MONGODB_DATABASE", "asterisk"))
+    USER = const[str]("")
+    PASSWORD = const[str]("")
