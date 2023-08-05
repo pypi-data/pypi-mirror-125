dffc64d5f20100cdea0522f6f8200f4d487107cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:23 +0000

    Add package atheris/2.0.7/atheris-2.0.7-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/atheris/atheris-2.0.7-cp39-cp39-macosx_11_0_x86_64.whl/atheris/__init__.py
@@ -0,0 +1,30 @@
+# Copyright 2021 Google Inc.
+# Copyright 2021 Fraunhofer FKIE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .native import Setup, Fuzz, FuzzedDataProvider, _trace_branch, _reserve_counters, _trace_cmp, _trace_regex_match, ALL_REMAINING
+from .import_hook import instrument_imports
+from .instrument_bytecode import patch_code, instrument_func, instrument_all
+from .utils import path
+from .function_hooks import enabled_hooks, gen_match
+
+# PyInstaller Support
+# PyInstaller doesn't automatically support lazy imports, which happens because
+# we dynamically decide whether to import the with/without_libfuzzer versions of
+# the core module. This function tells it where to look for a hook-atheris.py
+# file.
+
+def get_hook_dirs():
+  import os
+  return [os.path.dirname(__file__)]
