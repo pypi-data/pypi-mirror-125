044b784a7090564a355e40630ee706ce23a1ba64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:49 +0000

    Add package smartpay/0.1.6/smartpay-0.1.6.tar.gz

---
+++ packages/smartpay/smartpay-0.1.6.tar.gz/smartpay-0.1.6/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+
+VERSION = "0.1.6"
+
+setup(
+    name="smartpay",
+    version=VERSION,
+    author="Smartpay Co. Ltd.",
+    author_email="uxe@smartpay.co",
+    url="https://github.com/smartpay-co/sdk-python",
+    description="Smartpay SDK Python",
+    long_description=open(os.path.join(ROOT, "README.md")).read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["smartpay", "smartpay.schemas"],
+    package_data={'': ['*.json']},
+    install_requires=["requests==2.25.1",
+                      "jtd == 0.1.1"],
+)
