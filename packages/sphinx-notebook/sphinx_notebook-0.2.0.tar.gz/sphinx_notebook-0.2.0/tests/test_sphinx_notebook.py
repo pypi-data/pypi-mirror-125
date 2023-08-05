0371b77234c3a4d515cb240e271ed601a36d1de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:17 +0000

    Add package sphinx-notebook/0.2.0/sphinx_notebook-0.2.0.tar.gz

---
+++ packages/sphinx-notebook/sphinx_notebook-0.2.0.tar.gz/sphinx_notebook-0.2.0/tests/test_sphinx_notebook.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Tests for `sphinx_notebook` package."""
+# pylint: disable=redefined-outer-name
+
+# import pytest
+from click.testing import CliRunner
+
+from sphinx_notebook import cli
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'sphinx_notebook.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
