1cd4e77a06123f126a9235a2ce589e57bd0563ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:26 +0000

    Add package mb-tools/1.1.1.1/mb-tools-1.1.1.1.tar.gz

---
+++ packages/mb-tools/mb-tools-1.1.1.1.tar.gz/mb-tools-1.1.1.1/demo2/__init__.py
@@ -0,0 +1,5 @@
+def test2():
+    print("hello mieba!!!")
+
+if __name__ == "__main__":
+    test2()
---
+++ packages/mb-tools/mb-tools-1.1.1.1.tar.gz/mb-tools-1.1.1.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="mb-tools",
+    version="1.1.1.1",
+    author="mb-tools",
+    author_email="sufei@xiaoantech.com",
+    description="",
+    long_description="test Module",
+    long_description_content_type="text/markdown",
+    url="http://baidu.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
