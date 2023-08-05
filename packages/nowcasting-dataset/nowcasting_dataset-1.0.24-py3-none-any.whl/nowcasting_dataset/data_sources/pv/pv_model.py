4ce0049f8fac14a53741e7daa8feb7e4761c1766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:07 +0000

    Add package nowcasting-dataset/1.0.24/nowcasting_dataset-1.0.24-py3-none-any.whl

---
+++ packages/nowcasting-dataset/nowcasting_dataset-1.0.24-py3-none-any.whl/nowcasting_dataset/data_sources/pv/pv_model.py
@@ -0,0 +1,18 @@
+""" Model for output of PV data """
+from xarray.ufuncs import isnan, isinf
+from nowcasting_dataset.data_sources.datasource_output import DataSourceOutput
+
+
+class PV(DataSourceOutput):
+    """ Class to store PV data as a xr.Dataset with some validation """
+
+    __slots__ = ()
+    _expected_dimensions = ("time", "id")
+
+    @classmethod
+    def model_validation(cls, v):
+        """ Check that all values are not Nan, Infinite, or < 0."""
+        assert (~isnan(v.data)).all(), "Some pv data values are NaNs"
+        assert (~isinf(v.data)).all(), "Some pv data values are Infinite"
+        assert (v.data >= 0).all(), "Some pv data values are below 0"
+        return v
