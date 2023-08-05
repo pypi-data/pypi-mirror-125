6fafa1a20bbc05d280102efd208503145530a1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:07 +0000

    Add package snooze-server/1.0.16/snooze_server-1.0.16-py3-none-any.whl

---
+++ packages/snooze-server/snooze_server-1.0.16-py3-none-any.whl/snooze/utils/housekeeper.py
@@ -0,0 +1,86 @@
+#
+# Copyright 2018-2020 Florian Dematraz <florian.dematraz@snoozeweb.net>
+# Copyright 2018-2020 Guillaume Ludinard <guillaume.ludi@gmail.com>
+# Copyright 2020-2021 Japannext Co., Ltd. <https://www.japannext.co.jp/>
+# SPDX-License-Identifier: AFL-3.0
+#
+
+#!/usr/bin/python3.6
+
+import logging
+import time
+import datetime
+import threading
+
+from logging import getLogger
+from snooze.utils import config
+log = getLogger('snooze.housekeeping')
+
+class Housekeeper():
+    def __init__(self, core):
+        log.debug('Init Housekeeper')
+        self.core = core
+        self.conf = None
+        self.thread = None
+        self.interval = None
+        self.snooze_expired = None
+        self.reload()
+        self.thread = HousekeeperThread(self)
+        self.thread.start()
+
+    def reload(self):
+        self.conf = config('housekeeping')
+        self.interval = self.conf.get('cleanup_interval', 60)
+        self.snooze_expired = self.conf.get('cleanup_snooze', 86400)
+        log.debug("Reloading Housekeeper with conf {}".format(self.conf))
+
+class HousekeeperThread(threading.Thread):
+
+    def __init__(self, housekeeper):
+        super().__init__()
+        self.housekeeper = housekeeper
+        self.main_thread = threading.main_thread()
+
+    def run(self):
+        timer = (1 - self.housekeeper.conf.get('trigger_on_startup', True)) * time.time()
+        last_day = -1
+        while True:
+            if not self.main_thread.is_alive():
+                break
+            if self.housekeeper.interval > 0 and time.time() - timer >= self.housekeeper.interval:
+                timer = time.time()
+                self.housekeeper.core.db.cleanup_timeout('record')
+                self.housekeeper.core.db.cleanup_orphans('comment', 'record_uid', 'record', 'uid')
+            day = datetime.datetime.now().day
+            if day != last_day:
+                last_day = day
+                self.cleanup_expired_snooze()
+            time.sleep(1)
+
+    def cleanup_expired_snooze(self):
+        if self.housekeeper.snooze_expired > 0:
+            log.debug("Starting to cleanup expired snooze filters")
+            now = datetime.datetime.now().astimezone()
+            date = now.astimezone().strftime("%Y-%m-%dT%H:%M")
+            hour = now.astimezone().strftime("%H:%M")
+            weekday = now.day
+            date_delta = (now - datetime.timedelta(seconds=self.housekeeper.snooze_expired)).astimezone().strftime("%Y-%m-%dT%H:%M")
+            match = ['AND',
+                ['OR', ['NOT', ['EXISTS', 'time_constraints.weekdays']], ['IN', weekday, 'time_constraints.weekdays.weekdays']],
+                ['AND',
+                    ['OR', ['NOT', ['EXISTS', 'time_constraints.datetime']], ['<=', 'time_constraints.datetime.from', date]],
+                    ['AND',
+                        ['OR', ['NOT', ['EXISTS', 'time_constraints.datetime']], ['>=', 'time_constraints.datetime.until', date]],
+                        ['AND',
+                            ['OR', ['NOT', ['EXISTS', 'time_constraints.time']], ['<=', 'time_constraints.time.from', hour]],
+                            ['OR', ['NOT', ['EXISTS', 'time_constraints.time']], ['>=', 'time_constraints.time.until', hour]]
+                        ]
+                    ]
+                ]
+            ]
+            expired_query = ['AND', ['NOT', match], ['AND', ['EXISTS', 'time_constraints.datetime'], ['NOT', ['>=', 'time_constraints.datetime.until', date_delta]]]]
+            expired_results = self.housekeeper.core.db.search('snooze', expired_query)
+            if expired_results['count'] > 0:
+                log.debug("List of expired snooze filters to cleanup: {}".format(expired_results))
+                deleted_results = self.housekeeper.core.db.delete('snooze', expired_query)
+                log.debug("Deleted {} snooze filters".format(deleted_results['count']))
---
+++ packages/snooze-server/snooze_server-1.0.16-py3-none-any.whl/snooze/utils/modification.py
@@ -0,0 +1,64 @@
+#
+# Copyright 2018-2020 Florian Dematraz <florian.dematraz@snoozeweb.net>
+# Copyright 2018-2020 Guillaume Ludinard <guillaume.ludi@gmail.com>
+# Copyright 2020-2021 Japannext Co., Ltd. <https://www.japannext.co.jp/>
+# SPDX-License-Identifier: AFL-3.0
+#
+
+#!/usr/bin/python3.6
+
+class ModificationException(Exception): pass
+
+from jinja2 import Template
+
+from logging import getLogger
+log = getLogger('snooze.utils.modification')
+
+class Modification():
+    def __init__(self, operation, key, value=None):
+        self.operation = operation
+        self.key = key
+        self.value = value
+    def modify(self, record):
+        """
+        Modify the record inplace.
+        Args:
+            record (dict): Record to modify
+        Returns:
+            bool: True if the record was modified
+
+            The Boolean returned is just used for control
+            (pretty logs, better verbose information)
+        Examples:
+            >>> modification = Modification('SET', 'key', 'value')
+            >>> modification.modify({})
+            True
+            >>> record
+            {'key': 'value'}
+        """
+        return_code = False
+        log.debug("Starting modification [{}, {}, {}]".format(self.operation, self.key, self.value))
+        templated_key = self.key
+        templated_value = self.value
+        if self.key and type(self.key) is str:
+            templated_key = Template(self.key).render(record)
+        if self.value and type(self.value) is str:
+            templated_value = Template(self.value).render(record)
+        if self.operation == 'SET':
+            return_code = bool(templated_value and record.get(templated_key) != templated_value)
+            record[templated_key] = templated_value
+        elif self.operation == 'ARRAY_APPEND':
+            array = record.get(templated_key)
+            if array and type(array) == list:
+                record[templated_key] += templated_value
+                return_code = True
+        elif self.operation == 'ARRAY_DELETE':
+            array = record.get(templated_key)
+            if array and type(array) == list and templated_value in array:
+                array.remove(templated_value)
+                return_code = True
+        elif self.operation == 'DELETE':
+            if templated_key in record:
+                del record[templated_key]
+                return_code = True
+        return return_code
