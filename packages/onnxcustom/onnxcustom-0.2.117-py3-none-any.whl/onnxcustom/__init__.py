892899f350886684182bc83f1d22a9d23d940a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:18 +0000

    Add package onnxcustom/0.2.117/onnxcustom-0.2.117-py3-none-any.whl

---
+++ packages/onnxcustom/onnxcustom-0.2.117-py3-none-any.whl/onnxcustom/__init__.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+"""
+@file
+@brief Experimentation with ONNX, examples.
+"""
+
+__version__ = "0.2.117"
+__author__ = "Xavier Dupré, ..."
+__max_supported_opset__ = 15  # Converters are tested up to this version.
+
+
+def check(verbose=1):
+    """
+    Runs a couple of functions to check the module is working.
+
+    :param verbose: 0 to hide the standout output
+    :return: list of dictionaries, result of each test
+    """
+    return []
