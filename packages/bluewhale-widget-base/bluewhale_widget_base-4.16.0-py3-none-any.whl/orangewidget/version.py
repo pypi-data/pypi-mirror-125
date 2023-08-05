15a1ed82a4b8a16852f7b8869c9174e26801b7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:38 +0000

    Add package bluewhale-widget-base/4.16.0/bluewhale_widget_base-4.16.0-py3-none-any.whl

---
+++ packages/bluewhale-widget-base/bluewhale_widget_base-4.16.0-py3-none-any.whl/orangewidget/version.py
@@ -0,0 +1,10 @@
+# THIS FILE IS GENERATED FROM BLUEWHALE-WIDGET-BASE SETUP.PY
+short_version = '4.16.0'
+version = '4.16.0'
+full_version = '4.16.0.dev0+cec075c'
+git_revision = 'cec075c3b0d1877e4670d063183ee11487ebd931'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
