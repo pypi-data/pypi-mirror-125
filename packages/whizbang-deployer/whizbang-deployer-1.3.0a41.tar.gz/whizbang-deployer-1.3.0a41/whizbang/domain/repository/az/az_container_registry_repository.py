eba2bdfa146268b91f2025283e7caef335e1c248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:06 +0000

    Add package whizbang-deployer/1.3.0a41/whizbang-deployer-1.3.0a41.tar.gz

---
+++ packages/whizbang-deployer/whizbang-deployer-1.3.0a41.tar.gz/whizbang-deployer-1.3.0a41/whizbang/domain/repository/az/az_container_registry_repository.py
@@ -0,0 +1,16 @@
+from whizbang.data.az_cli_context import AzCliContext
+from whizbang.domain.repository.az.az_repository_base import AzRepositoryBase
+
+
+class AzContainerRegistryRepository(AzRepositoryBase):
+    def __init__(self, context: AzCliContext):
+        AzRepositoryBase.__init__(self, context)
+
+    @property
+    def _resource_provider(self) -> str:
+        return 'acr'
+
+    def get_tags(self, registry_name, repository_name):
+        return self._execute(f'repository show-tags'
+                             f' --name {registry_name}'
+                             f' --repository {repository_name}')
