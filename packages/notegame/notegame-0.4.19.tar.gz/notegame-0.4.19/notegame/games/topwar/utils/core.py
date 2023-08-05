31b3390511c908df5c2f6d1012c9d3f4b9163f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:56 +0000

    Add package notegame/0.4.19/notegame-0.4.19.tar.gz

---
+++ packages/notegame/notegame-0.4.19.tar.gz/notegame-0.4.19/notegame/games/topwar/utils/core.py
@@ -0,0 +1,35 @@
+
+
+field_list = {
+    "id": "id",
+    "pid": "pid",
+    "nickname": "nick_name",
+    "username": "user_name",
+    "nationalflag": "national_flag",
+    "usergender": "user_gender",
+    "gender": "gender",
+    "avatarurl": "avatar_url",
+    "headimgurl": "head_img_url",
+    "power": "power",
+    "level": "player_level",
+    "shieldTime": "shield_time",
+    "fireTime": "fire_time",
+    "province": "province",
+    "x": "x",
+    "y": "y",
+    "k": "k",
+    "aid": "aid",
+    "a_tag": "a_tag",
+    "pointType": "point_type",
+    "ownerId": "owner_id",
+    "itemId": "item_id",
+    "expireTime": "expire_time"
+}
+
+
+def merge_info(update_dict: dict, properties: dict = None):
+    properties = properties or {}
+    for key, value in field_list.items():
+        if key in update_dict.keys():
+            properties[value] = update_dict[key]
+    return properties
