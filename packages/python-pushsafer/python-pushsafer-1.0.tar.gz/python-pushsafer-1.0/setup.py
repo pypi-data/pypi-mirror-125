f387828b04b75612c32ca6d12983c8d9155a1751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:05 +0000

    Add package python-pushsafer/1.0/python-pushsafer-1.0.tar.gz

---
+++ packages/python-pushsafer/python-pushsafer-1.0.tar.gz/python-pushsafer-1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="python-pushsafer",
+    version="1.0",
+    description="Comprehensive bindings for the " "Pushsafer.com notification service",
+    long_description=open("README.rst").read()
+    + "\n"
+    + open("AUTHORS.rst").read()
+    + "\n"
+    + open("CHANGES.rst").read(),
+    url="https://github.com/appzer/python-pushsafer",
+    author="Kevin Siml",
+    author_email="info@appzer.de",
+    py_modules=["pushsafer"],
+    install_requires=["requests>=1.0"],
+    use_2to3=True,
+    license="GNU GPLv3",
+)
