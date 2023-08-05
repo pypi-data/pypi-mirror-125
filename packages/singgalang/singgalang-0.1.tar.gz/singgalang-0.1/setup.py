14371867f9a4e7e5c4775bce9d2011b046d14375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:54 +0000

    Add package singgalang/0.1/singgalang-0.1.tar.gz

---
+++ packages/singgalang/singgalang-0.1.tar.gz/singgalang-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+name="singgalang",
+version="0.1",
+description="Python Script dengan tempo yang sesingkat-singkatnya",
+author="Muhammad Al Fajri",
+url="https://github.com/aN4ksaL4y/hariansinggalang",
+install_requires=["requests","bs4", "urwid"],
+scripts=["bin/singgalang"],
+packages=["singgalang"],
+zip_safe=False
+)
