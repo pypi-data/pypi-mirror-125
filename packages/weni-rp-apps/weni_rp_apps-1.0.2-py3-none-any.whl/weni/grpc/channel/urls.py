29826833f689e347f032254c2848487cd58fb530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:28 +0000

    Add package weni-rp-apps/1.0.2/weni_rp_apps-1.0.2-py3-none-any.whl

---
+++ packages/weni-rp-apps/weni_rp_apps-1.0.2-py3-none-any.whl/weni/grpc/channel/urls.py
@@ -0,0 +1,6 @@
+from weni.protobuf.flows import channel_pb2_grpc
+from weni.grpc.channel import services
+
+
+def grpc_handlers(server):
+    channel_pb2_grpc.add_WeniWebChatControllerServicer_to_server(services.WeniWebChatService.as_servicer(), server)
