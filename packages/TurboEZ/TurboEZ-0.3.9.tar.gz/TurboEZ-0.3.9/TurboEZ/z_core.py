67772587f16c6fe793769d3be3dd908c7b34ec85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:36 +0000

    Add package TurboEZ/0.3.9/TurboEZ-0.3.9.tar.gz

---
+++ packages/TurboEZ/TurboEZ-0.3.9.tar.gz/TurboEZ-0.3.9/TurboEZ/z_core.py
@@ -0,0 +1,21 @@
+﻿#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import platform
+from TurboEZ.vtg_ezztp_inertia_register_tbm_ import *
+
+def get_platform():
+    return platform.platform()
+
+
+def get_node():
+    return platform.node()
+
+
+def turboeasy_appztp():
+    tbm = Application()
+    tbm.create_widgets()
+
+
+if __name__ == '__main__':
+    pass
