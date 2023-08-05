485d6b33952a8f003f5246295ac8a72df042eead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:52 +0000

    Add package dashborg-python-sdk/0.4.0.dev4/dashborg-python-sdk-0.4.0.dev4.tar.gz

---
+++ packages/dashborg-python-sdk/dashborg-python-sdk-0.4.0.dev4.tar.gz/dashborg-python-sdk-0.4.0.dev4/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dashborg-python-sdk",
+    version="0.4.0.dev4",
+    author="Michael Sawka",
+    author_email="mike@dashborg.net",
+    description="Dashborg Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sawka/dashborg-python-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'aiofiles',
+        'cffi',
+        'cryptography',
+        'ecdsa',
+        'grpcio',
+        'grpcio-tools',
+        'protobuf',
+        'pycparser',
+        'six',
+        'pyjwt',
+        'watchdog',
+        'aiohttp',
+    ],
+    python_requires='>=3.6',
+)
