9b0cbd16ba1be262753db69710ab8ee50309e280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:16 +0000

    Add package hideandseek/0.1.1.1/hideandseek-0.1.1.1-py3-none-any.whl

---
+++ packages/hideandseek/hideandseek-0.1.1.1-py3-none-any.whl/hideandseek/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.1.1"
+from . import eval as E
+from . import node as N
+from . import validation as V
+
+# TODO: Change V.EarlyStopping funcionality into node. patience & primary scorer etc should be managed by node, not validation.
+# TODO: Change naming: cross validation -> validation
