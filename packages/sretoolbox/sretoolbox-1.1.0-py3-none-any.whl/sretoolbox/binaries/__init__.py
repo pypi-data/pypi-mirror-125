c283f6e8920448d91d489c26fc4d156b7986deaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:35 +0000

    Add package sretoolbox/1.1.0/sretoolbox-1.1.0-py3-none-any.whl

---
+++ packages/sretoolbox/sretoolbox-1.1.0-py3-none-any.whl/sretoolbox/binaries/__init__.py
@@ -0,0 +1,28 @@
+# Copyright 2021 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Exposes the binary libs for easy access.
+"""
+
+from sretoolbox.binaries.oc import Oc
+from sretoolbox.binaries.opm import Opm
+from sretoolbox.binaries.operator_sdk import OperatorSDK
+
+
+__all__ = [
+    'Oc',
+    'Opm',
+    'OperatorSDK',
+]
