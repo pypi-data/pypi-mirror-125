560a14156e3b577eb66ce21140ca0a4d732289ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:00 +0000

    Add package pygon/2.0.0a0/pygon-2.0.0a0.tar.gz

---
+++ packages/pygon/pygon-2.0.0a0.tar.gz/pygon-2.0.0a0/pygon/types/__init__.py
@@ -0,0 +1,10 @@
+"""
+discord.types
+~~~~~~~~~~~~~~
+
+Typings for the Discord API
+
+:copyright: (c) 2015-present Code-ParagonX
+:license: MIT, see LICENSE for more details.
+
+"""
---
+++ packages/pygon/pygon-2.0.0a0.tar.gz/pygon-2.0.0a0/pygon/ui/__init__.py
@@ -0,0 +1,15 @@
+"""
+discord.ui
+~~~~~~~~~~~
+
+Bot UI Kit helper for the Discord API
+
+:copyright: (c) 2015-present Code-ParagonX
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from .view import *
+from .item import *
+from .button import *
+from .select import *
