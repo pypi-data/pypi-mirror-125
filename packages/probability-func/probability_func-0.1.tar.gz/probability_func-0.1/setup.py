f97ead37535be73af5cf84e32afa663e19c704b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:29 +0000

    Add package probability-func/0.1/probability_func-0.1.tar.gz

---
+++ packages/probability-func/probability_func-0.1.tar.gz/probability_func-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_func',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_func'],
+      author = 'Dimobi Stephanie',
+      author_email = 'stephaniedimobi@gmail.com',
+      zip_safe=False)
