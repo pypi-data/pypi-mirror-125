4312c91819868286080ea77be45770905dcad0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:59 +0000

    Add package pygon/2.0.0a0/pygon-2.0.0a0-py2.py3-none-any.whl

---
+++ packages/pygon/pygon-2.0.0a0-py2.py3-none-any.whl/pygon/webhook/__init__.py
@@ -0,0 +1,13 @@
+"""
+discord.webhook
+~~~~~~~~~~~~~~
+
+Webhook support
+
+:copyright: (c) 2015-present Code-ParagonX
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from .async_ import *
+from .sync import *
