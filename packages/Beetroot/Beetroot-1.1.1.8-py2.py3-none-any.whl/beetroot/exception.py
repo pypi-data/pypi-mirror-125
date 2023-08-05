0c348d4338ad2fd3b1238ba95d176baed005567a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:05 +0000

    Add package Beetroot/1.1.1.8/Beetroot-1.1.1.8-py2.py3-none-any.whl

---
+++ packages/Beetroot/Beetroot-1.1.1.8-py2.py3-none-any.whl/beetroot/exception.py
@@ -0,0 +1,27 @@
+#Exception classes
+class VersionError(Exception):
+    pass
+
+class FunctionError(Exception):
+    pass
+
+class LanguageError(Exception):
+    pass
+
+class ModuleError(Exception):
+    pass
+
+class InvalidVoiceError(Exception):
+    pass
+
+class InvalidTypeError(Exception):
+    pass
+
+class InvalidHashSecurityValue(Exception):
+    pass
+
+class UnknownError(Exception):
+    pass
+
+class InvalidPPBool(Exception):
+    pass
