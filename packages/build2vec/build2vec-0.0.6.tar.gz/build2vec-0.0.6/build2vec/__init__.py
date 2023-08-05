83054f852f6c6d986f8993315c661b85ecdb46a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:31 +0000

    Add package build2vec/0.0.6/build2vec-0.0.6.tar.gz

---
+++ packages/build2vec/build2vec-0.0.6.tar.gz/build2vec-0.0.6/build2vec/__init__.py
@@ -0,0 +1,5 @@
+from .build2vec import Build2Vec
+from .ifc2graph import Ifc2Graph
+from importlib import metadata
+
+__version__ = metadata.version('build2vec')
---
+++ packages/build2vec/build2vec-0.0.6.tar.gz/build2vec-0.0.6/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Python package for building data embeddings'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name = 'build2vec',         # How you named your package folder (MyLib)
+    version=VERSION,
+    author="Mahmoud Abdelrahman",
+    author_email="<arch.mahmoud.ouf111@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[            # I get to this in a second
+          'gensim',
+          'networkx',
+          'geopandas',
+          'tqdm',
+      ],
+    keywords=['graph', 'network', 'building', 'spatial', 'spatiotemporal', 'bim', 'gis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
