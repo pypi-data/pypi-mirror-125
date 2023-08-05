e2038bdd9136a5a5900c69e3d6caf9c628b49bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:36 +0000

    Add package datacenter-datasdk/0.2.1/datacenter_datasdk-0.2.1-py3-none-any.whl

---
+++ packages/datacenter-datasdk/datacenter_datasdk-0.2.1-py3-none-any.whl/datacenter_datasdk/__init__.py
@@ -0,0 +1,5 @@
+# -*-coding:utf-8 -*-
+from .settings import auth # 鉴权
+from .restful_api import get_price # 行情接口
+from .pg_data import get_trade_days # 交易日历
+from .pg_data import get_security_info # 标的详情
