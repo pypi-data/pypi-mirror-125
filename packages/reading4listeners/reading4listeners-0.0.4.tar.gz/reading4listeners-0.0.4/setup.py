803d8883c801db6b82d1c98953b6cf3040a5c685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:31 +0000

    Add package reading4listeners/0.0.4/reading4listeners-0.0.4.tar.gz

---
+++ packages/reading4listeners/reading4listeners-0.0.4.tar.gz/reading4listeners-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    reqs = f.read()
+# with open('LICENSE', 'r') as f:
+#    legal = f.read()
+with open('README.md', 'r', encoding="utf-8") as f:
+    readme = f.read()
+setup(
+    name='reading4listeners',
+    version='0.0.4',
+    packages=['r4l'],
+    url='https://github.com/CypherousSkies/reading-for-listeners',
+    project_urls={
+        "Bug Tracker": "https://github.com/CypherousSkies/reading-for-listeners/issues",
+    },
+    # license=legal,
+    license='AGPL-3',
+    author='CypherousSkies',
+    author_email="5472563+CypherousSkies@users.noreply.github.com",
+    description='A deep-learning powered application which turns pdfs into audio files. Featuring ocr improvement and tts with inflection!',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=reqs,
+    entry_points={"console_scripts": ["r4l = r4l.bin.cli:main"], }
+)
