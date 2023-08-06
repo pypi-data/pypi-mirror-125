647d8b6da0b0ca5f4aba7799ade46c0cf1d27c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:38 +0000

    Add package diamond-miner/0.1.15/diamond_miner-0.1.15-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/diamond-miner/diamond_miner-0.1.15-cp39-cp39-macosx_10_15_x86_64.whl/diamond_miner/rounds/mda.py
@@ -0,0 +1,75 @@
+from typing import Iterable, Iterator
+
+from diamond_miner.defaults import (
+    DEFAULT_PROBE_DST_PORT,
+    DEFAULT_PROBE_SRC_PORT,
+    PROTOCOLS,
+    UNIVERSE_SUBSET,
+)
+from diamond_miner.logging import logger
+from diamond_miner.queries import GetNextRound
+from diamond_miner.typing import FlowMapper, IPNetwork, Probe
+
+
+def mda_probes(
+    url: str,
+    measurement_id: str,
+    round_: int,
+    mapper_v4: FlowMapper,
+    mapper_v6: FlowMapper,
+    probe_src_port: int = DEFAULT_PROBE_SRC_PORT,
+    probe_dst_port: int = DEFAULT_PROBE_DST_PORT,
+    adaptive_eps: bool = False,
+    n_initial_flows: int = 6,
+    target_epsilon: float = 0.05,
+    subsets: Iterable[IPNetwork] = (UNIVERSE_SUBSET,),
+) -> Iterator[Probe]:
+    """
+    Compute the probes to send given the previously discovered links.
+    """
+    # TODO: filter_partial is temporary.
+    # TODO: filter_inter_round?
+    query = GetNextRound(
+        adaptive_eps=adaptive_eps,
+        round_leq=round_,
+        filter_partial=True,
+        filter_virtual=True,
+        filter_inter_round=True,
+        target_epsilon=target_epsilon,
+        n_initial_flows=n_initial_flows,
+    )
+    rows = query.execute_iter(url, measurement_id, subsets)
+    for row in rows:
+        for probe in row_to_probes(
+            GetNextRound.Row(*row), mapper_v4, mapper_v6, probe_src_port, probe_dst_port
+        ):
+            # TEMP: Log prefixes that overflows the port number.
+            if probe[1] > (2 ** 16 - 1):
+                logger.warning("Port overflow for %s", GetNextRound.Row(*row))
+                break
+            yield probe
+
+
+def row_to_probes(
+    row: GetNextRound.Row,
+    mapper_v4: FlowMapper,
+    mapper_v6: FlowMapper,
+    probe_src_port: int,
+    probe_dst_port: int,
+) -> Iterator[Probe]:
+    dst_prefix_int = int(row.dst_prefix)
+    mapper = mapper_v4 if row.dst_prefix.ipv4_mapped else mapper_v6
+    protocol_str = PROTOCOLS[row.protocol]
+
+    for ttl, to_send, already_sent in zip(row.ttls, row.to_send, row.already_sent):
+        for flow_id in range(already_sent, already_sent + to_send):
+            addr_offset, port_offset = mapper.offset(
+                flow_id=flow_id, prefix=dst_prefix_int
+            )
+            yield (
+                dst_prefix_int + addr_offset,
+                probe_src_port + port_offset,
+                probe_dst_port,
+                ttl,
+                protocol_str,
+            )
---
+++ packages/diamond-miner/diamond_miner-0.1.15-cp39-cp39-macosx_10_15_x86_64.whl/diamond_miner/test.py
@@ -0,0 +1,14 @@
+from ipaddress import IPv6Address
+
+url = "clickhouse://localhost"
+
+
+def addr_to_string(addr: IPv6Address) -> str:
+    """
+    >>> from ipaddress import ip_address
+    >>> addr_to_string(ip_address('::dead:beef'))
+    '::dead:beef'
+    >>> addr_to_string(ip_address('::ffff:8.8.8.8'))
+    '8.8.8.8'
+    """
+    return str(addr.ipv4_mapped or addr)
