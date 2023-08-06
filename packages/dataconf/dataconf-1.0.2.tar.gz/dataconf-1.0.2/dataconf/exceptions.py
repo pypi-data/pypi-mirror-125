d2386a84be547b75a59091daa7138e5312ecb656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:09 +0000

    Add package dataconf/1.0.2/dataconf-1.0.2.tar.gz

---
+++ packages/dataconf/dataconf-1.0.2.tar.gz/dataconf-1.0.2/dataconf/exceptions.py
@@ -0,0 +1,18 @@
+class TypeConfigException(Exception):
+    pass
+
+
+class MissingTypeException(Exception):
+    pass
+
+
+class MalformedConfigException(Exception):
+    pass
+
+
+class UnexpectedKeysException(Exception):
+    pass
+
+
+class EnvListOrderException(Exception):
+    pass
