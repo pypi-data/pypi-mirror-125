13422b624567b82ad77208c51a18327e2aedfe00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:38 +0000

    Add package mat_discover/1.3.2/mat_discover-1.3.2.tar.gz

---
+++ packages/mat_discover/mat_discover-1.3.2.tar.gz/mat_discover-1.3.2/fixup_grayskull.py
@@ -0,0 +1,16 @@
+"""Touch up the conda recipe from grayskull using conda-souschef."""
+from souschef.recipe import Recipe
+from os.path import join
+
+fpath = join("mat_discover", "meta.yaml")
+fpath2 = join("scratch", "meta.yaml")
+my_recipe = Recipe(load_file=fpath)
+my_recipe["requirements"]["host"].append("flit")
+my_recipe["build"].append("noarch: python")
+del my_recipe["requirements"]["build"]
+my_recipe["requirements"]["run"].remove("kaleido")
+my_recipe["requirements"]["run"].append("python-kaleido")
+my_recipe["requirements"]["run"].append("pytorch >=1.9.0")
+my_recipe["requirements"]["run"].append("cudatoolkit ==11.3.1")
+my_recipe.save(fpath)
+my_recipe.save(fpath2)
