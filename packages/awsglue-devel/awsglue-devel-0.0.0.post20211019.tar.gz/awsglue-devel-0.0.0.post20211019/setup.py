14cf9d79bfc81a28c044058f227ad7ad010efd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:11 +0000

    Add package awsglue-devel/0.0.0.post20211019/awsglue-devel-0.0.0.post20211019.tar.gz

---
+++ packages/awsglue-devel/awsglue-devel-0.0.0.post20211019.tar.gz/awsglue-devel-0.0.0.post20211019/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='awsglue-devel',
+    version='0.0.0.post20211019',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
