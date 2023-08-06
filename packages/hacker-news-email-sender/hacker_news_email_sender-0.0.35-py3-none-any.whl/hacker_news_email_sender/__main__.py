e29e55090e51f79da7e51f1111088156907a2349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:02 +0000

    Add package hacker-news-email-sender/0.0.35/hacker_news_email_sender-0.0.35-py3-none-any.whl

---
+++ packages/hacker-news-email-sender/hacker_news_email_sender-0.0.35-py3-none-any.whl/hacker_news_email_sender/__main__.py
@@ -0,0 +1,27 @@
+from . import newsExctractor
+
+
+def main() -> None:  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m hacker_news_email_sender` and `$ hacker_news_email_sender `.
+
+    This is your program's entry point.
+
+    You can change this function to do whatever you want.
+    Examples:
+        * Run a test suite
+        * Run a server
+        * Do some other stuff
+        * Run a command line application (Click, Typer, ArgParse)
+        * List all available tasks
+        * Run an application (Flask, FastAPI, Django, etc.)
+    """
+
+    extractor = newsExctractor.News_extractor()
+    extractor.extract_news()
+    extractor.send_email()
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
