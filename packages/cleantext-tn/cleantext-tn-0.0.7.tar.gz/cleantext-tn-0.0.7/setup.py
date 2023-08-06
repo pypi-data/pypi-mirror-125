d60c38e1b9cdd2885bae74cb4f478013e03369a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:34 +0000

    Add package cleantext-tn/0.0.7/cleantext-tn-0.0.7.tar.gz

---
+++ packages/cleantext-tn/cleantext-tn-0.0.7.tar.gz/cleantext-tn-0.0.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cleantext-tn",
+    version="0.0.7",
+    author="Thu Nguyen",
+    author_email="minhthu6521@gmail.com",
+    description="Clean text package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minhthu6521/clean-text",
+    project_urls={
+        "Bug Tracker": "https://github.com/minhthu6521/clean-text/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "nltk==3.6.5",
+    ],
+    python_requires=">=3.6",
+)
