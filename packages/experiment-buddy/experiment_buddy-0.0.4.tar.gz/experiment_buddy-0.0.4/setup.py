5821115cf5908a4314623e9a9ce5c20fb5c7e807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:56 +0000

    Add package experiment-buddy/0.0.4/experiment_buddy-0.0.4.tar.gz

---
+++ packages/experiment-buddy/experiment_buddy-0.0.4.tar.gz/experiment_buddy-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+install_requires = [
+    'GitPython',
+    'tensorboardX',
+    'matplotlib',
+    'wandb',
+    'fabric',
+    'cloudpickle',
+    'PyYaml',
+    'paramiko',
+    'tqdm',
+    'aiohttp',
+    'funcy',
+]
+
+setup(
+    name='experiment_buddy',
+    version='0.0.4',
+    packages=["experiment_buddy", "scripts"],
+    package_data={'scripts': ['*/*.sh']},
+    url='https://github.com/ministry-of-silly-code/experiment_buddy/',
+    license='',
+    author='Manuel Del Verme, Ionelia Buzatu',
+    maintainer='Ionelia Buzatu',
+    author_email='',
+    description='',
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["buddy-init=buddy_init.init_project:sys_main"]}
+)
