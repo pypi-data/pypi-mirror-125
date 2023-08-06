eb41373556c1ba176c14029c40594ca986b3f54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:54 +0000

    Add package DXR/0.1.2/DXR-0.1.2.tar.gz

---
+++ packages/DXR/DXR-0.1.2.tar.gz/DXR-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DXR',
+    version='0.1.2',
+    packages=['Dxr_mqtt', 'Dxr_log'],
+    install_requires=['paho-mqtt'],
+    author='luzhipeng',
+    author_email='402087139@qq.com',
+    license='MIT',
+    url='http://pycn.me'
+)
