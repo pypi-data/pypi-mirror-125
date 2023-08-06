a3490a4976854227d8ae6e0f554dcc4a071ff960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:22 +0000

    Add package py-ar/1.0.0/py-ar-1.0.0.tar.gz

---
+++ packages/py-ar/py-ar-1.0.0.tar.gz/py-ar-1.0.0/README.md
@@ -0,0 +1,6 @@
+# trprogram Package To print "Hello World"
+* pip install trakos
+## By :
+* Trakos 
+### Telegram Channel
+* https://t.me/trprogram
---
+++ packages/py-ar/py-ar-1.0.0.tar.gz/py-ar-1.0.0/src/py-ar/my_class.py
@@ -0,0 +1,7 @@
+from user_agent import generate_user_agent
+class iq:
+    def run():
+        print = اطبع
+class fake:
+        def get():
+            return (generate_user_agent())
