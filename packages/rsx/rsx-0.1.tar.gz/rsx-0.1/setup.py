73ede08d43feaed4708b96b6e58f69d112934db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:42 +0000

    Add package rsx/0.1/rsx-0.1.tar.gz

---
+++ packages/rsx/rsx-0.1.tar.gz/rsx-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rsx',
+    version='0.1',
+    description='',
+    long_description='',
+    url='https://github.com/gstamatelat/rsx',
+    author='Giorgos Stamatelatos',
+    author_email='gstamatelat@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
