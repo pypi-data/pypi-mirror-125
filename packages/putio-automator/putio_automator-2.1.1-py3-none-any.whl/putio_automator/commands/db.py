4119463933f4c932755d68794d5b46da44fef0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:06 +0000

    Add package putio-automator/2.1.1/putio_automator-2.1.1-py3-none-any.whl

---
+++ packages/putio-automator/putio_automator-2.1.1-py3-none-any.whl/putio_automator/commands/db.py
@@ -0,0 +1,27 @@
+"""
+Commands to manage the download database
+"""
+import click
+
+from putio_automator import echo, logger
+from putio_automator.cli import cli
+from putio_automator.db import with_db
+
+
+@cli.group()
+def db():
+    pass
+
+@db.command()
+@click.argument('name')
+def forget(name):
+    "Delete records of previous downloads in the database"
+
+    def func(connection):
+        "Do the above"
+        logger.debug('Delete downloads with name like %s' % name)
+        conn = connection.cursor()
+        conn.execute('delete from downloads where name like ?', ('%%%s%%' % name,))
+        echo('info', 'Deleted %d rows with name like %s' % (conn.rowcount, name))
+
+    with_db(func)
