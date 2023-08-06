23933043ca889358093f46bac9f237e936d5b14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:00 +0000

    Add package jl-logger/0.1.0/jl_logger-0.1.0-py3-none-any.whl

---
+++ packages/jl-logger/jl_logger-0.1.0-py3-none-any.whl/jl_logger-0.1.0.data/data/share/jupyter/labextensions/jl-logger/static/style_index_js.1ddf700d425d0d0d5c82.js
@@ -0,0 +1,73 @@
+"use strict";
+(self["webpackChunkjl_logger"] = self["webpackChunkjl_logger"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, ".activated-jl-logger-btn{\n    background-color: var(--jp-layout-color2);\n}\n\n.jl-logger-btn .jp-ToolbarButtonComponent-label{\n    font-weight: 640;\n}\n\n.jl-logger-tools input[type=\"text\"]{\n    margin: 0px 4px;\n    background-color: #EFEFEF;\n    border: solid #EFEFEF;\n    border-radius: 4px;\n    width: 80px;\n    height: 24px;\n    box-sizing: border-box;\n}\n.jl-logger-tools .ext-txt{\n    color: #CFCFCF;\n    font-weight: bolder;\n    font-size: 32%;\n    vertical-align: bottom;\n}\n.jl-logger-tools label{\n    margin-left: 4px;\n    font-size: var(--jp-ui-font-size1);\n    vertical-align: middle;\n}\n.jl-logger-tools input[type=\"checkbox\"]{\n    vertical-align: middle;\n}", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;IACI,yCAAyC;AAC7C;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,eAAe;IACf,yBAAyB;IACzB,qBAAqB;IACrB,kBAAkB;IAClB,WAAW;IACX,YAAY;IACZ,sBAAsB;AAC1B;AACA;IACI,cAAc;IACd,mBAAmB;IACnB,cAAc;IACd,sBAAsB;AAC1B;AACA;IACI,gBAAgB;IAChB,kCAAkC;IAClC,sBAAsB;AAC1B;AACA;IACI,sBAAsB;AAC1B","sourcesContent":[".activated-jl-logger-btn{\n    background-color: var(--jp-layout-color2);\n}\n\n.jl-logger-btn .jp-ToolbarButtonComponent-label{\n    font-weight: 640;\n}\n\n.jl-logger-tools input[type=\"text\"]{\n    margin: 0px 4px;\n    background-color: #EFEFEF;\n    border: solid #EFEFEF;\n    border-radius: 4px;\n    width: 80px;\n    height: 24px;\n    box-sizing: border-box;\n}\n.jl-logger-tools .ext-txt{\n    color: #CFCFCF;\n    font-weight: bolder;\n    font-size: 32%;\n    vertical-align: bottom;\n}\n.jl-logger-tools label{\n    margin-left: 4px;\n    font-size: var(--jp-ui-font-size1);\n    vertical-align: middle;\n}\n.jl-logger-tools input[type=\"checkbox\"]{\n    vertical-align: middle;\n}"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.1ddf700d425d0d0d5c82.js.map
