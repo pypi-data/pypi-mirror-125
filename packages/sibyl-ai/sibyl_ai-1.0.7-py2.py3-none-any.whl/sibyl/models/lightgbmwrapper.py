367b13ffa4f30e28ef756f0d9cbe44accc8aba93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:37 +0000

    Add package sibyl-ai/1.0.7/sibyl_ai-1.0.7-py2.py3-none-any.whl

---
+++ packages/sibyl-ai/sibyl_ai-1.0.7-py2.py3-none-any.whl/sibyl/models/lightgbmwrapper.py
@@ -0,0 +1,25 @@
+"""
+Light GBM wrapper to simplify early stopping
+
+@author: Francesco Baldisserri
+@creation date: 24/9/2021
+"""
+
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+
+
+class LGBMRegressorWrapper(lgb.LGBMRegressor):
+    def fit(self, x, y):
+        rounds = int(self.n_estimators ** 0.5)
+        x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
+        return super().fit(x_train, y_train, eval_set=[(x_val, y_val)],
+                           callbacks=[lgb.early_stopping(10, verbose=rounds)])
+
+
+class LGBMClassifierWrapper(lgb.LGBMClassifier):
+    def fit(self, x, y):
+        rounds = int(self.n_estimators ** 0.5)
+        x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
+        return super().fit(x_train, y_train, eval_set=[(x_val, y_val)],
+                           callbacks=[lgb.early_stopping(10, verbose=rounds)])
