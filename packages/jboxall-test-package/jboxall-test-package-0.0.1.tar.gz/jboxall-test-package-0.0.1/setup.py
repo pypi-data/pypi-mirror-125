4ad9ed4d4786dd24afdb26c23ed6dc3275007e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:24:54 +0000

    Add package jboxall-test-package/0.0.1/jboxall-test-package-0.0.1.tar.gz

---
+++ packages/jboxall-test-package/jboxall-test-package-0.0.1.tar.gz/jboxall-test-package-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jboxall-test-package",
+    version="0.0.1",
+    description="Test Package",
+    author="John Boxall",
+    author_email="jboxall@salesforce.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True
+)
