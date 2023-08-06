6fa66a6cb39c9e97c3094ee7d0bd6d02702499ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:29:29 +0000

    Add package bw2data/4.0.dev11/bw2data-4.0.dev11.tar.gz

---
+++ packages/bw2data/bw2data-4.0.dev11.tar.gz/bw2data-4.0.dev11/tests/compatibility.py
@@ -0,0 +1,32 @@
+from bw2data import *
+from bw2data.tests import BW2DataTest, bw2test
+
+from .fixtures import biosphere, food
+
+
+@bw2test
+def test_repr_str_unicode():
+    objects = (
+        geomapping,
+        databases,
+        methods,
+        normalizations,
+        weightings,
+        Database("foo"),
+        DataStore("foo"),
+        projects,
+    )
+    for obj in objects:
+        assert repr(obj)
+        assert str(obj)
+        print(obj)
+
+
+@bw2test
+def test_registered_database_repr():
+    d = Database("biosphere")
+    d.write(biosphere)
+    assert repr(d)
+    assert str(d)
+    # Make sure can be printed - not for debugging
+    print(d)
