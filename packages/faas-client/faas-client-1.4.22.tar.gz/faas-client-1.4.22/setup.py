cae1d518436a7d13fa48160fd2b3ef9d56689273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:50 +0000

    Add package faas-client/1.4.22/faas-client-1.4.22.tar.gz

---
+++ packages/faas-client/faas-client-1.4.22.tar.gz/faas-client-1.4.22/setup.py
@@ -0,0 +1,35 @@
+import os
+
+import setuptools
+
+install_requires = [
+    "sh"
+]
+
+test_requires = [
+    "pytest",
+    "pytest-xdist"
+]
+
+package_version = os.environ.get("PACKAGE_VERSION", "0.0.0")
+package_name = os.environ.get("PACKAGE_NAME")
+
+setuptools.setup(
+    name=package_name,
+    version=package_version,
+    author="Laurent Michel",
+    author_email="softozor@gmail.com",
+    description=f"A client library for faas-cli",
+    url="https://gitlab.hidora.com/softozor/packages",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        "test": test_requires,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
