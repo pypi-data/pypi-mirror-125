63903ea57661f00a7139ce267f65dcbee6c49c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:01 +0000

    Add package nian/0.1.81/nian-0.1.81.tar.gz

---
+++ packages/nian/nian-0.1.81.tar.gz/nian-0.1.81/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name="nian", # Replace with your own username  #自定义封装模块名与文件夹名相同
+    version="0.1.81",
+    author="初慕苏流年", #作者
+    author_email="1274210585@qq.com", #邮箱
+    description="集合工具等", #描述
+    long_description='白嫖(QQ，酷狗，酷我，网易)音乐，快速排序，冒泡排序，目录暴力遍历', #描述
+    long_description_content_type="text/markdown", #markdown
+    url="http://0.wlwmz.top:2/", #github地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", #License
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',  #支持python版本
+)
