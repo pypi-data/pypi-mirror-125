e8f8ae2fb89389ffccc5c2373029704d49dd23eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:41 +0000

    Add package my-calc/0.0.1/my_calc-0.0.1.tar.gz

---
+++ packages/my-calc/my_calc-0.0.1.tar.gz/my_calc-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name="my_calc",
+    version="0.0.1",
+    author="Vadym",
+    author_email="v@mail.ru",
+    description="Calculator created with hillel group of 13.09.2021",
+    packages=[
+        '.'
+    ],
+    python_requires='>=3.7',
+)
