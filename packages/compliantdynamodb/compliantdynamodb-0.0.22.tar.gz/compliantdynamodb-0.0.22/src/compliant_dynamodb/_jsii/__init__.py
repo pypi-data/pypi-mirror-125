25b0124f2026cf0dde2bc8a7e6978551bc3c3229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:48 +0000

    Add package compliantdynamodb/0.0.22/compliantdynamodb-0.0.22.tar.gz

---
+++ packages/compliantdynamodb/compliantdynamodb-0.0.22.tar.gz/compliantdynamodb-0.0.22/src/compliant_dynamodb/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_backup._jsii
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_kinesis._jsii
+import aws_cdk.aws_kms._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "compliantdynamodb", "0.0.22", __name__[0:-6], "compliantdynamodb@0.0.22.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
