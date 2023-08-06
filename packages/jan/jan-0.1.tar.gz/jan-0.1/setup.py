97ff5bedc583dad6df55dcce8fe47a74936e4e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:53 +0000

    Add package jan/0.1/jan-0.1.tar.gz

---
+++ packages/jan/jan-0.1.tar.gz/jan-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="jan",
+    version="0.1",
+    description="fly.",
+    url="http://github.com/neilyio/figur",
+    author="neilyio",
+    author_email="neil.hansen.31@gmail.com",
+    license="MIT",
+    packages=["lili"],
+    zip_safe=False,
+)
