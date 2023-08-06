0eb1f779cafb93fa3a8b13f00a378da1c109e381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:14 +0000

    Add package isbnlib-kb/0.0.1/isbnlib_kb-0.0.1-py2.py3-none-any.whl

---
+++ packages/isbnlib-kb/isbnlib_kb-0.0.1-py2.py3-none-any.whl/isbnlib_kb/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+from ._kb import query
+
+__version__ = '0.0.1'
+
+__plugin_author__ = 'willemjan'
+__plugin_description__ = 'Metadata for books from the National Library of the Netherlands'
+__plugin_homepage__ = 'github.com/kbNLresearch/isbnlib-kb'
+__plugin_service_language__ = 'nl'
+__plugin_service_name__ = 'kb'
+__plugin_type__ = 'metadata'
