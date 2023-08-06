df54e26b29151a24e954edc7807ef3a614288572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:41 +0000

    Add package askdata/1.0.63/askdata-1.0.63-py3-none-any.whl

---
+++ packages/askdata/askdata-1.0.63-py3-none-any.whl/askdata/NLG.py
@@ -0,0 +1,87 @@
+import operator
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+import random
+import logging
+import requests
+
+ops = {
+    '<': operator.lt,
+    '<=': operator.le,
+    '==': operator.eq,
+    '!=': operator.ne,
+    '>=': operator.ge,
+    '>': operator.gt
+}
+
+
+def get_conditional_phrases(conditions, phrase1, phrase2):
+    bool_array = []
+    for condition in conditions:
+        for op in ops:
+            if op in condition:
+                splitted = condition.split(' ' + op + ' ')
+                operator = ops[op]
+                bool_array.append(operator(splitted[0], splitted[1]))
+        # bool_array.append(eval(condition))
+    if False in bool_array:
+        return phrase2
+    else:
+        return phrase1
+
+
+def get_random_synonymous(synonyms):
+    random.seed()
+    nKeys = len(synonyms.keys())
+    pickedItem = random.randint(0, nKeys - 1)
+    synPicked = synonyms[pickedItem]
+    return synPicked
+
+
+def words_to_digits(phrase):
+    words = phrase.split(" ")
+    num = []
+    for word in words:
+        if "." in word:
+            word = word.replace(".", "")
+        if "," in word:
+            word = word.replace(",", "")
+        if word.isdigit():
+            num.append(int(word))
+    return num
+
+
+def add_random_synonymous_to_sentence(phrase, placeholder, synonyms):
+    random.seed()
+    nKeys = len(synonyms.keys())
+    pickedItem = random.randint(0, nKeys - 1)
+    synPicked = synonyms[pickedItem]
+    phrase = phrase.replace(placeholder, synPicked)
+    return phrase
+
+
+def nlg(df, examples, example_sentences, max_tokens=64):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "df": df,
+        "examples": examples,
+        "examples_sentences": example_sentences,
+        "max_tokens": max_tokens
+    }
+
+    s = requests.Session()
+    s.keep_alive = False
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[502, 503, 504])
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+
+    url = "https://api-dev.askdata.com/cfc/nlg"
+
+    r = s.post(url=url, headers=headers, json=data)
+    r.raise_for_status()
+
+    try:
+        return r.json()['nlp_list']
+    except:
+        logging.error(r.json())
