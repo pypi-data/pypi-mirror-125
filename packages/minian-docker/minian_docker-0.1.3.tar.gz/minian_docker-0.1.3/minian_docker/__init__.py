9736249be8a42bfcb80442101c01dd84078b35b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:05 +0000

    Add package minian-docker/0.1.3/minian_docker-0.1.3.tar.gz

---
+++ packages/minian-docker/minian_docker-0.1.3.tar.gz/minian_docker-0.1.3/minian_docker/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+
+from version import __version__
+from bin import *
