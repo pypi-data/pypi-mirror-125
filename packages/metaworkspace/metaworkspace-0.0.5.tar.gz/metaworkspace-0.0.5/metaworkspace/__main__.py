4fed7822125139bb723ffc2db43e228f3cdbf49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:02 +0000

    Add package metaworkspace/0.0.5/metaworkspace-0.0.5.tar.gz

---
+++ packages/metaworkspace/metaworkspace-0.0.5.tar.gz/metaworkspace-0.0.5/metaworkspace/__main__.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+from argparse import ArgumentParser
+
+import metaworkspace.filesystem as fs
+from metaworkspace.workspace import MetacityWorkspace
+
+
+def install(workspace_path: str):
+    print(f"Creating workspace directory...")
+    fs.recreate_workspace(workspace_path)
+
+
+def run(mws: MetacityWorkspace):
+    os.environ["METACITYWS"] = mws.path
+    mws.clear_logs()
+
+    server_log = open(mws.server_log, 'w+')
+    jobs_log = open(mws.jobs_log, 'w+')
+    
+    proc = subprocess.Popen(["uvicorn", "metaworkspace.runtime.api.api:app", "--workers", "4", "--host", "0.0.0.0", "--port", "5000"], 
+                            stdout=server_log,
+                            stderr=server_log)
+
+    jobs = subprocess.Popen(["python", "-m", "metaworkspace.runtime.processing"], 
+                            stdout=jobs_log,
+                            stderr=jobs_log)
+                 
+    return_code = proc.wait()
+    return_code = jobs.wait()
+
+
+usage = ("Sets up Metacity Workspace."
+         "Downloads all Metacity modules, sets up local installation of Java,"
+         "GMLTools")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=usage)
+    parser.add_argument('--install', help='Run the installation process', action="store_true")
+    parser.add_argument('--run', help='Run the application', action="store_true")
+    parser.add_argument('workspace_dir', type=str, help='Path to newly created Metacity workspace')
+    args = parser.parse_args()    
+    path = args.workspace_dir
+    mws = MetacityWorkspace(path)
+
+    if args.install:
+        install(mws.path)
+    
+    if args.run:
+        run(mws)
---
+++ packages/metaworkspace/metaworkspace-0.0.5.tar.gz/metaworkspace-0.0.5/metaworkspace/runtime/processing/loader.py
@@ -0,0 +1,36 @@
+from typing import Dict, Type
+from metaworkspace.runtime.processing.jobs.job import Job
+from metaworkspace.runtime.processing.jobs.loaddataset import JobLoadDataset
+import json
+import os
+import uuid
+
+
+jobs: Dict[str, Type[Job]] = {
+    JobLoadDataset.TYPE: JobLoadDataset
+}
+
+
+def job_object_hook(data):
+    if 'type' not in data:
+        raise Exception(f"Job {data} missing job type")
+    
+    t = data['type']
+    if t not in jobs:
+        raise Exception(f"Uknown job type: {t}")
+
+    job = jobs[t]()
+    job.deserialize(data)
+    return job
+    
+     
+def load_job(job_dir):
+    jobfile = os.path.join(job_dir, 'job.json')
+    with open(jobfile, 'r') as job_file:
+        job: Job = json.load(job_file, object_hook=job_object_hook)
+    job.job_dir = job_dir
+    job.setup_log(str(uuid.uuid1()))
+    return job
+
+
+
