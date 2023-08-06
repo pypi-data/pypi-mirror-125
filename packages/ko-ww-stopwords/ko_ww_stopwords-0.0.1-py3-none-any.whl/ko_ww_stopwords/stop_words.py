940fd545017f745523f16bad9192bf3871f7a226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:24 +0000

    Add package ko-ww-stopwords/0.0.1/ko_ww_stopwords-0.0.1-py3-none-any.whl

---
+++ packages/ko-ww-stopwords/ko_ww_stopwords-0.0.1-py3-none-any.whl/ko_ww_stopwords/stop_words.py
@@ -0,0 +1,22 @@
+'''
+Copyright 2021 Rairye
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+ko_ww_stop_words = set(["그", "또", "및", "사", "삼", "셋", "야", "어", "왜", "육", "응", "일", "자", "저", "제", "좀", "참", "즉", "칠", "툭", "퉤", "하나", "허", "헉", "휴", "흥", "이것", "이곳", "그것", "이것들", "그것들",
+                        "함께", "같이", "나", "만큼", "하나", "둘", "넷", "다섯", "여섯", "일곱", "여덟", "아홉", "열", "이", "삼", "사", "오", "육", "칠", "팔", "구", "십", "스물", "이런", "그런", "저런", "이까짓",
+                        "그까짓", "저까짓", "따라", "무엇", "뭘", "뭐", "못", "안", "수", "아무", "적이", "하나도", "겸", "듯이", "채로", "대로", "정도로", "차라리", "때", "때문에", "달라고", "얼마나", "몇", "번", "주라고",
+                        "위해서", "테니까", "거니까", "아직", "그렇지만", "또", "만일", "바로", "실로", "어느", "어디", "어때", "어떠한", "어떤", "어떤것", "어떤것들", "어이", "언제", "언젠가", "얼마", "이상",
+                        "얼마나", "얼마만큼", "여기", "여부", "예컨대", "예하면", "오로지", "오자마자", "오히려", "왜", "왜냐하면", "외에도", "요컨대", "우리", "우선", "이때", "이쪽", "자기", "자신", "잠깐", "잠시", "저것", "저기",
+                        "저쪽", "전부", "전자", "혹시", "혼자", "혼자서", "좀", "제", "제각기", "조금", "즉", "즉시", "진짜로", "차라리", "타인", "첫번째로", "통하여", "틈타", "할뿐", "마터면", "한다", "된다", "했다", "됐다", "향하여",
+                        "향해서", "훨씬"]) 
