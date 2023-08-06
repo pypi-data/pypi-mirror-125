321ccdfbb0b14791f8b6e07727cabce426797f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:01 +0000

    Add package seven-cloudapp-frame/1.0.7/seven_cloudapp_frame-1.0.7.tar.gz

---
+++ packages/seven-cloudapp-frame/seven_cloudapp_frame-1.0.7.tar.gz/seven_cloudapp_frame-1.0.7/seven_cloudapp_frame/models/db_models/user/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: HuangJianYi
+@Date: 2021-07-15 17:17:08
+@LastEditTime: 2021-08-12 13:58:21
+@LastEditors: HuangJianYi
+@Description: 
+"""
+__all__=["user_asset_model","user_account_model","user_black_model","user_info_model",]
