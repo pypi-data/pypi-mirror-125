78bef20652c6371c91fd7ab4f97c1ee15793cd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:16 +0000

    Add package hbtn-cli/2.0.0/hbtn-cli-2.0.0.tar.gz

---
+++ packages/hbtn-cli/hbtn-cli-2.0.0.tar.gz/hbtn-cli-2.0.0/README.md
@@ -0,0 +1,36 @@
+# HBTN-CLI
+
+Automates the Creation of  all necessary files of a project. by supplying one or more URL.
+
+<img src="https://github.com/maleksal/hbtn-cli/blob/main/demo.gif"  style="zoom:150%;" />
+
+## Install
+
+```bash
+$ python3 -m pip install hbtn-cli
+```
+
+
+
+## Usage
+
+```console
+$ hbtn setup
+
+$ hbtn fetch <project URL>
+
+$ hbtn --help
+Usage: hbtn [OPTIONS] COMMAND [ARGS]...
+
+  Create all necessary files of a project. by supplying one or more URL.
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  fetch  Fetch project files and creates them.
+  setup  Create settings.ini.
+
+
+```
+
---
+++ packages/hbtn-cli/hbtn-cli-2.0.0.tar.gz/hbtn-cli-2.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hbtn-cli',
+    version='2.0.0',
+    author='Malek Salem',
+    author_email='1419@holbertonschool.com',
+    license="MIT License",
+    description='Automates Holberton School projects creation.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/maleksal/hbtn-cli',
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4==4.10.0",
+        "Click==7.1",
+        "requests",
+        "lxml",
+        "wheel",
+    ],
+    python_requires='>=3.5',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix"
+    ],
+    entry_points='''
+        [console_scripts]
+        hbtn=core.interface:main
+    '''
+)
