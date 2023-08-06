f226f83db1e2abea4516da2e4d4f426c456b52c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:35 +0000

    Add package vault/1.3.0/vault-1.3.0.tar.gz

---
+++ packages/vault/vault-1.3.0.tar.gz/vault-1.3.0/vault/static/vault/js/swift_cloud/report.js
@@ -0,0 +1,83 @@
+// const reportData = Vue.observable({});
+
+const ReportApp = {
+  el: "#report-app",
+  data: {
+    projects: PROJECTS,
+    statusUrl: STATUS_URL,
+    migrateUrl: MIGRATE_URL,
+  },
+  methods: {
+    async getProjectStatus(project) {
+      const data = await fetch(`${this.statusUrl}?project_id=${project.id}`);
+      const result = await data.json();
+      project.status = result.status;
+    },
+    async migrateProject(project) {
+      const csrfToken = Base.Cookie.read("csrftoken");
+      const res = await fetch(this.migrateUrl, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-CSRFToken": csrfToken,
+        },
+        body: JSON.stringify({
+          project_id: project.id,
+          project_name: project.name,
+          environment: project.environment,
+        }),
+      });
+      const result = await res.json();
+
+      if (!res.ok) {
+        Base.Messages.setMessage({
+          type: "error",
+          description: result.error,
+        });
+        return;
+      }
+
+      Base.Messages.setMessage({
+        type: "success",
+        description: result.message,
+      });
+
+      this.getProjectStatus(project);
+    },
+  },
+  template: `
+  <div class="box-table">
+    <table class="table">
+      <thead>
+        <tr>
+          <th width="28%">ID</th>
+          <th>Project Name</th>
+          <th>Status</th>
+          <th width="10%"></th>
+          <th width="10%"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.name">
+          <td>{{ project.id }}</td>
+          <td>{{ project.name }}</td>
+          <td>{{ project.status }}</td>
+          <td class="text-end">
+            <button class="btn btn-sm btn-light" @click="getProjectStatus(project)">
+              Update Status
+            </button>
+          </td>
+          <td class="text-end">
+            <button class="btn btn-sm btn-primary" @click="migrateProject(project)">
+              Migrate
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  `,
+};
+
+new Vue(ReportApp);
