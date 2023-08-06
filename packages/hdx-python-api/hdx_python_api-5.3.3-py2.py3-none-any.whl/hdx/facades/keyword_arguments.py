2f254d01b48388b1d33c509fb1867c9f9883d3a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:58 +0000

    Add package hdx-python-api/5.3.3/hdx_python_api-5.3.3-py2.py3-none-any.whl

---
+++ packages/hdx-python-api/hdx_python_api-5.3.3-py2.py3-none-any.whl/hdx/facades/keyword_arguments.py
@@ -0,0 +1,36 @@
+"""Facade to simplify project setup that calls project main function with kwargs"""
+import logging
+from typing import Any, Callable
+
+from hdx.facades import logging_kwargs
+from hdx.hdx_configuration import Configuration
+from hdx.utilities.easy_logging import setup_logging
+from hdx.utilities.useragent import UserAgent
+
+logger = logging.getLogger(__name__)
+setup_logging(**logging_kwargs)
+
+
+def facade(projectmainfn: Callable[[Any], None], **kwargs: Any):
+    """Facade to simplify project setup that calls project main function
+
+    Args:
+        projectmainfn ((Any) -> None): main function of project
+        **kwargs: configuration parameters to pass to HDX Configuration & other parameters to pass to main function
+
+    Returns:
+        None
+    """
+
+    #
+    # Setting up configuration
+    #
+    site_url = Configuration._create(**kwargs)
+
+    logger.info("--------------------------------------------------")
+    logger.info(f"> Using HDX Python API Library {Configuration.apiversion}")
+    logger.info(f"> HDX Site: {site_url}")
+
+    UserAgent.user_agent = Configuration.read().user_agent
+
+    projectmainfn(**kwargs)
---
+++ packages/hdx-python-api/hdx_python_api-5.3.3-py2.py3-none-any.whl/hdx/facades/simple.py
@@ -0,0 +1,36 @@
+"""Facade to simplify project setup that calls project main function"""
+import logging
+from typing import Any, Callable
+
+from hdx.facades import logging_kwargs
+from hdx.hdx_configuration import Configuration
+from hdx.utilities.easy_logging import setup_logging
+from hdx.utilities.useragent import UserAgent
+
+logger = logging.getLogger(__name__)
+setup_logging(**logging_kwargs)
+
+
+def facade(projectmainfn: Callable[[None], None], **kwargs: Any):
+    """Facade to simplify project setup that calls project main function
+
+    Args:
+        projectmainfn ((None) -> None): main function of project
+        **kwargs: configuration parameters to pass to HDX Configuration class
+
+    Returns:
+        None
+    """
+
+    #
+    # Setting up configuration
+    #
+    site_url = Configuration._create(**kwargs)
+
+    logger.info("--------------------------------------------------")
+    logger.info(f"> Using HDX Python API Library {Configuration.apiversion}")
+    logger.info(f"> HDX Site: {site_url}")
+
+    UserAgent.user_agent = Configuration.read().user_agent
+
+    projectmainfn()
