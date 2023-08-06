5092e44ed7942aa2b209916a4288fb68de9ab5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:50 +0000

    Add package stere/0.31.0/stere-0.31.0-py3-none-any.whl

---
+++ packages/stere/stere-0.31.0-py3-none-any.whl/stere/browser_spy/xhr_script.py
@@ -0,0 +1,39 @@
+# Javascript intended to be opened as a string and injected into the browser.
+js_script = """
+// Prevent patch from stacking if applied multiple times.
+if (!window.oldSend) {
+    // Number of requests that are active.
+    document.activeXHRrequests = 0;
+
+    // Number of requests that have been completed.
+    document.totalXHRrequests = 0;
+
+    window.oldSend = XMLHttpRequest.prototype.send;
+
+    // Patch a request counter onto XMLHttpRequest.send()
+    XMLHttpRequest.prototype.send = function() {
+        countRequests(this);
+        // Call the native send()
+        window.oldSend.apply(this, arguments);
+    }
+
+    // Increment a counter every time a request starts,
+    //   and add an event to decrement it when the request is complete.
+    var countRequests = function(xhr) {
+        document.activeXHRrequests += 1;
+        document.totalXHRrequests += 1;
+
+        xhr.addEventListener(
+            'loadend', () => {
+                // Decrement counter
+                document.activeXHRrequests -= 1;
+
+                // For live debugging
+                var remaining_msg = `remaining: ${document.activeXHRrequests}`;
+                var total_msg = `total: ${document.totalXHRrequests}`;
+                console.log('XHR loadend,', remaining_msg, total_msg);
+            }
+        )
+    }
+}
+"""
---
+++ packages/stere/stere-0.31.0-py3-none-any.whl/stere/fields/appium/input.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from ..decorators import stere_performer, use_after, use_before
+from ..field import Field
+
+
+@stere_performer('send_keys', consumes_arg=True)
+class Input(Field):
+    """Convenience Class on top of Field.
+
+    Uses Appium's send_keys method.
+
+    Arguments:
+        default_value (str): When Input.send_keys() is called with no
+            arguments, this value will be used instead.
+    """
+
+    def __init__(self, *args, default_value: Optional[str] = None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.default_value = default_value
+
+    @use_after
+    @use_before
+    def send_keys(self, value: Optional[str] = None) -> None:
+        """Use Appium's fill method.
+
+        Arguments:
+            value (str): The text to enter into the input.
+
+        Example:
+
+        >>> first_name = Input('id', 'fillme')
+        >>> first_name.send_keys('Joseph')
+
+        """
+        if value is None and self.default_value:
+            value = self.default_value
+        self.find().send_keys(value)
