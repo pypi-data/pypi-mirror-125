ba5535f69aab3c4966d9dc232dd6863a0fe33044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:55 +0000

    Add package quakeio/0.0.7/quakeio-0.0.7.tar.gz

---
+++ packages/quakeio/quakeio-0.0.7.tar.gz/quakeio-0.0.7/src/quakeio/basic_formats.py
@@ -0,0 +1,56 @@
+import json
+from copy import copy
+from pathlib import Path
+
+from quakeio.core import GroundMotionComponent, GroundMotionEvent
+from quakeio.utils.parseutils import open_quake
+
+
+class Unused:
+    pass
+
+
+class QuakeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "tolist"):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+def read_json(read_file, **kwds: Unused):
+
+    return json.load(read_file)
+
+
+def write_json(write_file, ground_motion, indent=4, summarize=False, **kwds):
+    ground_motion = ground_motion.serialize(serialize_data=not summarize)
+    try:
+        with open_quake(write_file, "w") as f:
+            json.dump(ground_motion, f, indent=indent, cls=QuakeEncoder)
+    except BrokenPipeError:
+        pass
+
+
+def read_yaml(read_file, **k):
+    import yaml
+
+    return yaml.load(read_file, Loader=yaml.Loader)
+
+
+def write_yaml(write_file, ground_motion, summarize=False, **kwds):
+    import yaml
+
+    ground_motion = ground_motion.serialize(serialize_data=not summarize)
+    # if not summarize:
+    #     ground_motion = ground_motion.serialize()
+
+    with open_quake(write_file, "w") as f:
+        yaml.dump(ground_motion, f)
+
+
+FILE_TYPES = {
+    "json": {"read": read_json, "write": write_json},
+    "yaml": {"read": read_yaml, "write": write_yaml},
+    "quake.json": {"read": read_json, "write": write_json},
+    "quake.yaml": {"read": read_yaml, "write": write_yaml},
+}
---
+++ packages/quakeio/quakeio-0.0.7.tar.gz/quakeio-0.0.7/src/quakeio/eqsig.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from quakeio.core import GroundMotionSeries, GroundMotionComponent
+from quakeio.utils.parseutils import open_quake
+
+
+def read(read_file, **kwds):
+    with open_quake(read_file, "r") as f:
+        time_step = next(next(f))
+        accel = np.genfromtxt(f, skip_header=1, delimiter=",", usecols=0)
+    return GroundMotionComponent(
+        GroundMotionSeries(accel), meta={"accel.time_step": time_step}
+    )
+
+
+def write(write_file, motion: GroundMotionSeries, label="eqsig data", **kwds):
+    print(dir(motion.accel))
+    print(dir(motion))
+    time_step = motion.accel.time_step
+    data = [label, f"{len(motion.accel)} {time_step}", *map(str, motion.accel)]
+    with open_quake(write_file, "w") as f:
+        f.write("\n".join(data))
+
+
+FILE_TYPES = {"eqsig": {"read": read, "write": write}}
