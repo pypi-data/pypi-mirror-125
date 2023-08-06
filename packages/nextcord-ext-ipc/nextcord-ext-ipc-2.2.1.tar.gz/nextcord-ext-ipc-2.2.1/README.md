27d5d530c5f663421e09d27102c59668e3012f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:03 +0000

    Add package nextcord-ext-ipc/2.2.1/nextcord-ext-ipc-2.2.1.tar.gz

---
+++ packages/nextcord-ext-ipc/nextcord-ext-ipc-2.2.1.tar.gz/nextcord-ext-ipc-2.2.1/README.md
@@ -0,0 +1,38 @@
+# nextcord-ext-ipc
+
+[![Discord server invite](https://discord.com/api/guilds/881118111967883295/embed.png)](https://discord.gg/ZebatWssCB)
+[![PyPI version info](https://img.shields.io/pypi/v/nextcord-ext-ipc.svg)](https://pypi.python.org/pypi/nextcord-ext-ipc)
+[![Analyze Status](https://github.com/nextcord/nextcord-ext-ipc/workflows/Analyze/badge.svg?event=push)](https://github.com/nextcord/nextcord-ext-ipc/actions?query=workflow%3AAnalyze+event%3Apush)
+[![Build Status](https://github.com/nextcord/nextcord-ext-ipc/workflows/Build/badge.svg?event=push)](https://github.com/nextcord/nextcord-ext-ipc/actions?query=workflow%3ABuild+event%3Apush)
+[![Lint Status](https://github.com/nextcord/nextcord-ext-ipc/workflows/Lint/badge.svg?event=push)](https://github.com/Ext-Creators/nextcord-ext-ipc/actions?query=workflow%3ALint+event%3Apush)
+
+## About
+
+A Nextcord extension for inter-process communication.
+
+## Installation
+
+**Python 3.8 or higher is required**
+
+It is necessary to first install [Nextcord](https://github.com/nextcord/nextcord)
+
+Then install the extension by running one of the following commands:
+
+```py 
+# Linux/macOS
+python3 -m pip install -U nextcord-ext-ipc
+
+# Windows
+py -3 -m pip install -U nextcord-ext-ipc
+```
+
+## Links 
+
+- [Documentation](https://nextcord-ext-ipc.readthedocs.io)
+- [Usage Examples](https://github.com/nextcord/nextcord-ext-ipc/tree/master/examples)
+- [Support Discord Server](https://discord.gg/ZebatWssCB)
+
+## License
+
+Copyright (c) 2020 Ext-Creators  
+Copyright (c) 2021 Nextcord Developers
---
+++ packages/nextcord-ext-ipc/nextcord-ext-ipc-2.2.1.tar.gz/nextcord-ext-ipc-2.2.1/setup.py
@@ -0,0 +1,92 @@
+import re
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Framework :: AsyncIO",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Communications",
+    "Topic :: Documentation",
+    "Topic :: Documentation :: Sphinx",
+    "Topic :: Internet",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+extras_require = {
+    "docs": [
+        "sphinx",
+        "sphinxcontrib_trio",
+        "sphinx-rtd-theme",
+    ],
+}
+
+with open("requirements.txt") as stream:
+    install_requires = stream.read().splitlines()
+
+packages = [
+    "nextcord.ext.ipc",
+]
+
+project_urls = {
+    "Documentation": "https://nextcord-ext-ipc.readthedocs.io",
+    "Issue Tracker": "https://github.com/nextcord/nextcord-ext-ipc/issues",
+    "Source": "https://github.com/nextcord/nextcord-ext-ipc",
+}
+
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("nextcord/ext/ipc/__init__.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+version = match.group(2)
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+
+setuptools.setup(
+    author="Nextcord Developers",
+    classifiers=classifiers,
+    description="A Nextcord extension for inter-process communication.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    extras_require=extras_require,
+    install_requires=install_requires,
+    license="Apache Software License",
+    name="nextcord-ext-ipc",
+    packages=packages,
+    project_urls=project_urls,
+    python_requires=">=3.6.0",
+    url="https://github.com/nextcord/nextcord-ext-ipc",
+    version=version,
+)
