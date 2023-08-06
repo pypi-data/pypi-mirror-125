f7ba9312802d163d48399c9b800c52c2644037ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:32 +0000

    Add package crocodile/3.0/crocodile-3.0-py3-none-any.whl

---
+++ packages/crocodile/crocodile-3.0-py3-none-any.whl/crocodile/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "3.0"
+docs = r"https://crocodile.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/crocodile"
+release_notes = """
+
+Fixed Debugging tools.
+"""
