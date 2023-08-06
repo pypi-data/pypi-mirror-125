e18d1051b3c9069dd3251f192d5eeade19b2008e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:10 +0000

    Add package clevr/0.1/clevr-0.1.tar.gz

---
+++ packages/clevr/clevr-0.1.tar.gz/clevr-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='clevr',
+    version='0.1',
+    description='Official Python SDK for Clevr.',
+    py_modules=['clevr'],
+    install_requires=[],
+    license='MIT',
+    package_dir={'': 'clevr'}
+)
