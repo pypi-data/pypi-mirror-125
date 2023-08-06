04ffaa1c132310cbcd6bd7e83fed7822a4c4be76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:45 +0000

    Add package AnonimTest/1.0/AnonimTest-1.0.tar.gz

---
+++ packages/AnonimTest/AnonimTest-1.0.tar.gz/AnonimTest-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='AnonimTest',
+	version='1.0',
+	author='Anonim777',
+	author_email='falis65155@ingfix.com',
+	description='',
+	packages=['AnonimTest'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
