eb9e3f0cc012c076e016ef1aeee6bedb86215d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:52 +0000

    Add package calibrade/0.1.0/CalibrADE-0.1.0.tar.gz

---
+++ packages/calibrade/CalibrADE-0.1.0.tar.gz/CalibrADE-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['calibrade']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.4.3,<4.0.0',
+ 'numpy>=1.21.3,<2.0.0',
+ 'opencv-python>=4.5.4,<5.0.0']
+
+setup_kwargs = {
+    'name': 'calibrade',
+    'version': '0.1.0',
+    'description': 'A toolbox for end-to-end camera calibration with minimal pain.',
+    'long_description': None,
+    'author': 'David Russell',
+    'author_email': 'davidrus@andrew.cmu.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
