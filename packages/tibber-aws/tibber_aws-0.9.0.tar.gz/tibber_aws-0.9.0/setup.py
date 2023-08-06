fe4dd9213e65f6f977d2750022b8ec4196b1152a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:22 +0000

    Add package tibber-aws/0.9.0/tibber_aws-0.9.0.tar.gz

---
+++ packages/tibber-aws/tibber_aws-0.9.0.tar.gz/tibber_aws-0.9.0/setup.py
@@ -0,0 +1,32 @@
+import sys
+
+from setuptools import setup
+
+install_requires = [
+    "aioboto3",
+    "aiohttp",
+    "async_timeout"
+]
+
+if sys.version_info < (3, 7):
+    install_requires.append("async_exit_stack")
+
+setup(
+    name="tibber_aws",
+    packages=["tibber_aws"],
+    install_requires=install_requires,
+    version="0.9.0",
+    description="A python3 library to communicate with Aws",
+    python_requires=">=3.5.3",
+    author="Tibber",
+    author_email="drdaniel@tibber.com",
+    url="https://github.com/tibbercom/tibber-pyAws",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
