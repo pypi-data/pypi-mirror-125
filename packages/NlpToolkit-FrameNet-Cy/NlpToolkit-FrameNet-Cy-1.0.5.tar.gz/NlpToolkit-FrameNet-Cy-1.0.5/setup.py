6a6ba1a832e8968631ebb98bff5d08e690c2f9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:59 +0000

    Add package NlpToolkit-FrameNet-Cy/1.0.5/NlpToolkit-FrameNet-Cy-1.0.5.tar.gz

---
+++ packages/NlpToolkit-FrameNet-Cy/NlpToolkit-FrameNet-Cy-1.0.5.tar.gz/NlpToolkit-FrameNet-Cy-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(["FrameNet/*.pyx"],
+                          compiler_directives={'language_level': "3"}),
+    name='NlpToolkit-FrameNet-Cy',
+    version='1.0.5',
+    packages=['FrameNet'],
+    package_data={'FrameNet': ['*.pxd', '*.pyx', '*.c', '*.py']},
+    url='https://github.com/StarlangSoftware/TurkishFrameNet-Cy',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='FrameNet library',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
