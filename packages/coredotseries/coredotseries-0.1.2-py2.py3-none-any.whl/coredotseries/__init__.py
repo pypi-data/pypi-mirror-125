2fcf45ae5cc5e169548bd76bad79ec93e771dbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:56 +0000

    Add package coredotseries/0.1.2/coredotseries-0.1.2-py2.py3-none-any.whl

---
+++ packages/coredotseries/coredotseries-0.1.2-py2.py3-none-any.whl/coredotseries/__init__.py
@@ -0,0 +1,9 @@
+import os
+from .series import *
+from .version import __version__
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+"""Top-level package for Core.Series."""
+
+__author__ = """CoreDotToday"""
+__email__ = 'engine@core.today'
