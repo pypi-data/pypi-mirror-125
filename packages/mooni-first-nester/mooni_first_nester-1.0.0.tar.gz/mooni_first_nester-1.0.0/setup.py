ddc76434525440430f7da20e84a250d61bf3bf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:22 +0000

    Add package mooni-first-nester/1.0.0/mooni_first_nester-1.0.0.tar.gz

---
+++ packages/mooni-first-nester/mooni_first_nester-1.0.0.tar.gz/mooni_first_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name            = 'mooni_first_nester',
+        version         = '1.0.0',
+        author          = 'orwellmoon',
+        author_email    = 'orwellmoon@gmail.com',
+        url = 'https://blog.naver.com/mooni3013',
+        description     = 'A simple printer of nested lists'
+     )
