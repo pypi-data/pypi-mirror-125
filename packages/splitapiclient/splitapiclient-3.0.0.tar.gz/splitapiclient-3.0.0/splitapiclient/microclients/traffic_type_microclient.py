393cdcb5954dca0098d83998e0d9e788a827d493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:26 +0000

    Add package splitapiclient/3.0.0/splitapiclient-3.0.0.tar.gz

---
+++ packages/splitapiclient/splitapiclient-3.0.0.tar.gz/splitapiclient-3.0.0/splitapiclient/microclients/traffic_type_microclient.py
@@ -0,0 +1,41 @@
+from splitapiclient.util.exceptions import HTTPResponseError, \
+    UnknownApiClientError
+from splitapiclient.util.logger import LOGGER
+from splitapiclient.resources import TrafficType
+
+
+class TrafficTypeMicroClient:
+    '''
+    '''
+    _endpoint = {
+        'all_items': {
+            'method': 'GET',
+            'url_template': 'trafficTypes/ws/{workspaceId}',
+            'headers': [{
+                'name': 'Authorization',
+                'template': 'Bearer {value}',
+                'required': True,
+            }],
+            'query_string': [],
+            'response': True,
+        },
+    }
+
+    def __init__(self, http_client):
+        '''
+        Constructor
+        '''
+        self._http_client = http_client
+
+    def list(self, workspace_id):
+        '''
+        Returns a list of TrafficType objects.
+
+        :returns: List of TrafficType objects
+        :rtype: list(TrafficType)
+        '''
+        response = self._http_client.make_request(
+            self._endpoint['all_items'],
+            workspaceId = workspace_id
+        )
+        return [TrafficType(item, self._http_client) for item in response]
