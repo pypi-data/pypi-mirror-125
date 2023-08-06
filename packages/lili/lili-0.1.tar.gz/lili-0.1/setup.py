0ab1530e95faa3482277462b872b339d5447bacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:52 +0000

    Add package lili/0.1/lili-0.1.tar.gz

---
+++ packages/lili/lili-0.1.tar.gz/lili-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="lili",
+    version="0.1",
+    description="fly.",
+    url="http://github.com/neilyio/figur",
+    author="neilyio",
+    author_email="neil.hansen.31@gmail.com",
+    license="MIT",
+    packages=["lili"],
+    zip_safe=False,
+)
