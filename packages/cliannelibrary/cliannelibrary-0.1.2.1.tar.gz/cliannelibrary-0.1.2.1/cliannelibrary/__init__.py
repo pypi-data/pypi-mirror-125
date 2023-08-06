bee105512ca0a01c3eb8cff4d25b5c93de051f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:28 +0000

    Add package cliannelibrary/0.1.2.1/cliannelibrary-0.1.2.1.tar.gz

---
+++ packages/cliannelibrary/cliannelibrary-0.1.2.1.tar.gz/cliannelibrary-0.1.2.1/cliannelibrary/__init__.py
@@ -0,0 +1,12 @@
+"""
+:author: Dmitry Ostroushko
+:copyright: (c) 2021 Dmitry Ostroushko
+"""
+
+from .filelibrary import (FileLibrary)
+from .exceptions import (FileAlreadyExistsException)
+import .parselibrary
+
+__author__ = 'Dmitry Ostroushko'
+__version__ = '0.1.2.1'
+__email__ = 'dmitrylianne@gmail.com'
