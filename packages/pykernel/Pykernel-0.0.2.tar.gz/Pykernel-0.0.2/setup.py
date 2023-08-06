c9ccd0ccc2b0adaadd8b33d557385e90f1adc11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:10 +0000

    Add package pykernel/0.0.2/Pykernel-0.0.2.tar.gz

---
+++ packages/pykernel/Pykernel-0.0.2.tar.gz/Pykernel-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="Pykernel",
+    version="0.0.2",
+    description="Python & text Windows CMD Editior written with only native libaries",
+    py_modules=["pykernel"],
+    package_dir={"":"code"},
+    url="https://github.com/coolnicecool/Pykernel",
+    author="Raleigh Priour"
+)
