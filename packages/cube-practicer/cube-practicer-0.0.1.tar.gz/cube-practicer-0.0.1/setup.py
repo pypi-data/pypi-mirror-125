7f568c69d7daf4ba69e821af1ba37bd58dbc5569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:03 +0000

    Add package cube-practicer/0.0.1/cube-practicer-0.0.1.tar.gz

---
+++ packages/cube-practicer/cube-practicer-0.0.1.tar.gz/cube-practicer-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cube_practicer']
+
+package_data = \
+{'': ['*'], 'cube_practicer': ['resources/*']}
+
+install_requires = \
+['pygame>=2.0.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cube-practicer',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'ylq',
+    'author_email': 'jamesylq@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
