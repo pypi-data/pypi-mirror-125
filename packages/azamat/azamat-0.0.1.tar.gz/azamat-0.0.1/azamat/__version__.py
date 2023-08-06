45bcc88a6325abdef852b8beb3ca9f7f2d95d3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:35 +0000

    Add package azamat/0.0.1/azamat-0.0.1.tar.gz

---
+++ packages/azamat/azamat-0.0.1.tar.gz/azamat-0.0.1/azamat/__version__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#                            _                _____
+#      /\                   | |       /\     |_   _|
+#     /  \     ____   __ _  | |_     /  \      | |
+#    / /\ \   |_  /  / _` | | __|   / /\ \     | |
+#   / ____ \   / /  | (_| | | |_   / ____ \   _| |_
+#  /_/    \_\ /___|  \__,_|  \__| /_/    \_\ |_____|
+#
+#
+
+__title__ = "azamat"
+__description__ = "Azamat Test Python Package"
+__url__ = "https://azamat.com/pkg"
+__version__ = "0.0.1"
+__author__ = "Boranbayev Azamat"
+__author_email__ = "azaza7087@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 Azat Artificial Intelligence, LLP (AzatAI)"
