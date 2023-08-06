f5e31f034ef0202e2f1fdd59fd55e5be4c835985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:24 +0000

    Add package fugue/0.6.4/fugue-0.6.4.tar.gz

---
+++ packages/fugue/fugue-0.6.4.tar.gz/fugue-0.6.4/fugue_spark/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from fugue_version import __version__
+
+from fugue_spark.dataframe import SparkDataFrame
+from fugue_spark.execution_engine import SparkExecutionEngine
+from fugue_spark.registry import register
+
+register()
