ea59aa3da70c107f756512aa9cf778d760100441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:14 +0000

    Add package shiboken6/6.2.1/shiboken6-6.2.1-6.2.1-cp36.cp37.cp38.cp39.cp310-none-win_amd64.whl

---
+++ packages/shiboken6/shiboken6-6.2.1-6.2.1-cp36.cp37.cp38.cp39.cp310-none-win_amd64.whl/shiboken6/_config.py
@@ -0,0 +1,11 @@
+shiboken_library_soversion = str(6.2)
+
+version = "6.2.1"
+version_info = (6, 2, 1, "", "")
+
+__build_date__ = '2021-10-25T10:31:56+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.2.1'
