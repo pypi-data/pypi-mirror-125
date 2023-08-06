3d3eb48a7db2fa634d0d39ae6abf477ca904397d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:50 +0000

    Add package flask-replit-auth/0.0.6/flask_replit_auth-0.0.6.tar.gz

---
+++ packages/flask-replit-auth/flask_replit_auth-0.0.6.tar.gz/flask_replit_auth-0.0.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fhandle:
+    long_description = fhandle.read()
+
+setuptools.setup(
+    name="flask_replit_auth",
+    version="0.0.6",
+    author="EpicCodeWizard",
+    author_email="epiccodewizard@gmail.com",
+    description="Brings replit auth to flask.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://replit.com/@EpicCodeWizard/flaskreplitauth",
+    install_requires=[
+        'requests',
+        'flask'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
