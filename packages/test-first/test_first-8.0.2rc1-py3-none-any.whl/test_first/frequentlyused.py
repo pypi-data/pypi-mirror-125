c9328da428e0aeeb18356e3c8818798039e95580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:10 +0000

    Add package test-first/8.0.2rc1/test_first-8.0.2rc1-py3-none-any.whl

---
+++ packages/test-first/test_first-8.0.2rc1-py3-none-any.whl/test_first/frequentlyused.py
@@ -0,0 +1,6 @@
+from test_first.fake import Fake
+from test_first.fakeobject import FakeObject
+from test_first.argumentexpectations import *
+from test_first.scenario import *
+from test_first.hook import Hook
+from test_first.DSL import *
