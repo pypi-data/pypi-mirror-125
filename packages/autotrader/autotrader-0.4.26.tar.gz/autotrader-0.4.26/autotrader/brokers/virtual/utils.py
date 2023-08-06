dfcb43b92263655b5cf799244ef762309851e7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:45 +0000

    Add package autotrader/0.4.26/autotrader-0.4.26.tar.gz

---
+++ packages/autotrader/autotrader-0.4.26.tar.gz/autotrader-0.4.26/autotrader/brokers/virtual/utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Utilities for virtual broker.
+
+"""
+
+from autotrader.brokers.broker_utils import BrokerUtils
+
+class Utils(BrokerUtils):
+    def __init__(self):
+        return
