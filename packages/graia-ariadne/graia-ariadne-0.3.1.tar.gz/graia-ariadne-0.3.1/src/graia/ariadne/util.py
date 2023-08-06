a03986a5d68754aae8b4374cc47ee4156b11791e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:45 +0000

    Add package graia-ariadne/0.3.1/graia-ariadne-0.3.1.tar.gz

---
+++ packages/graia-ariadne/graia-ariadne-0.3.1.tar.gz/graia-ariadne-0.3.1/src/graia/ariadne/util.py
@@ -0,0 +1,88 @@
+import functools
+import sys
+import traceback
+from typing import Callable, ContextManager, TypeVar, Union
+
+from graia.broadcast.entities.dispatcher import BaseDispatcher
+from graia.broadcast.interfaces.dispatcher import DispatcherInterface
+from loguru import logger
+from typing_extensions import ParamSpec
+
+from .context import enter_context
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+from .exception import (
+    AccountMuted,
+    AccountNotFound,
+    InvalidArgument,
+    InvalidSession,
+    InvalidVerifyKey,
+    MessageTooLong,
+    UnknownTarget,
+    UnVerifyedSession,
+)
+
+code_exceptions_mapping = {
+    1: InvalidVerifyKey,
+    2: AccountNotFound,
+    3: InvalidSession,
+    4: UnVerifyedSession,
+    5: UnknownTarget,
+    6: FileNotFoundError,
+    10: PermissionError,
+    20: AccountMuted,
+    30: MessageTooLong,
+    400: InvalidArgument,
+}
+
+
+def validate_response(code: Union[dict, int]):
+    if isinstance(code, dict):
+        code = code.get("code")
+        exception_code = code_exceptions_mapping.get(code)
+        if exception_code:
+            raise exception_code
+    elif isinstance(code, int):
+        exception_code = code_exceptions_mapping.get(code)
+        if exception_code:
+            raise exception_code
+
+
+def loguru_tb(limit=None, file=None, chain=True):
+    logger.exception(sys.exc_info()[1])
+
+
+def inject_loguru_traceback():
+    traceback.print_exc = loguru_tb
+
+
+class ApplicationMiddlewareDispatcher(BaseDispatcher):
+    always = True
+    context: ContextManager
+
+    def __init__(self, app) -> None:
+        self.app = app
+
+    def beforeExecution(self, interface: "DispatcherInterface"):
+        self.context = enter_context(self.app, interface.event)
+        self.context.__enter__()
+
+    def afterExecution(self, interface: "DispatcherInterface", exception, tb):
+        self.context.__exit__(exception.__class__ if exception else None, exception, tb)
+
+    async def catch(self, interface: "DispatcherInterface"):
+        from .app import Ariadne
+
+        if interface.annotation is Ariadne:
+            return self.app
+
+
+def app_ctx_manager(func: Callable[P, R]) -> Callable[P, R]:
+    @functools.wraps(func)
+    async def wrapper(self, *args: P.args, **kwargs: P.kwargs):
+        with enter_context(app=self):
+            return await func(self, *args, **kwargs)
+
+    return wrapper
