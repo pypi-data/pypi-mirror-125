1a77eadf5510fd900c663e53325a12f97e0a7e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:44 +0000

    Add package git-phoenix/1.0.4/git-phoenix-1.0.4.tar.gz

---
+++ packages/git-phoenix/git-phoenix-1.0.4.tar.gz/git-phoenix-1.0.4/src/core/actions/tag.py
@@ -0,0 +1,15 @@
+from src.core.actions.executable import Executable
+from src.core.logger import Logger
+
+
+class Tag(Executable):
+
+    def __init__(self, execution, action_execution):
+        super().__init__(execution, action_execution)
+        self.is_implemented = False
+
+    def execute(self):
+        Logger.warn(cls=Tag, msg="Not implemented yet!")
+
+    def _parse(self):
+        pass
