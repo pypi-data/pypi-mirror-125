15d085ccc28379174940fc343f31c38105634ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:39 +0000

    Add package hydrolib-core/0.1.4/hydrolib_core-0.1.4-py3-none-any.whl

---
+++ packages/hydrolib-core/hydrolib_core-0.1.4-py3-none-any.whl/hydrolib/core/basemodel.py
@@ -0,0 +1,277 @@
+"""
+Here we define our Pydantic `BaseModel` with custom settings,
+as well as a `FileModel` that inherits from a `BaseModel` but
+also represents a file on disk.
+
+"""
+import logging
+from abc import ABC, abstractclassmethod
+from contextvars import ContextVar
+from pathlib import Path
+from typing import Any, Callable, Dict, Optional, Type
+from warnings import warn
+from weakref import WeakValueDictionary
+
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic.error_wrappers import ErrorWrapper, ValidationError
+
+from hydrolib.core.io.base import DummmyParser, DummySerializer
+from hydrolib.core.utils import to_key
+
+logger = logging.getLogger(__name__)
+
+# We use ContextVars to keep a reference to the folder
+# we're currently parsing files in. In the future
+# we could move to https://github.com/samuelcolvin/pydantic/issues/1549
+context_dir: ContextVar[Path] = ContextVar("folder")
+
+
+def _reset_context_dir(token):
+    # Once the model has been completely initialized
+    # reset the context
+    if token:
+        logger.info("Reset context.")
+        context_dir.reset(token)
+
+
+class BaseModel(PydanticBaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+        validate_assignment = True
+        use_enum_values = True
+        extra = "forbid"  # will throw errors so we can fix our models
+        allow_population_by_field_name = True
+        alias_generator = to_key
+
+    def __init__(self, **data: Any) -> None:
+        """Initializes a BaseModel with the provided data.
+
+        Raises:
+            ValidationError: A validation error when the data is invalid.
+        """
+        try:
+            super().__init__(**data)
+        except ValidationError as e:
+            id = self._get_identifier(data)
+            if id is None:
+                raise e
+            else:
+                # If there is an identifier, include this in the ValidationError messages.
+                raise ValidationError([ErrorWrapper(e, loc=id)], self.__class__)
+
+    def is_file_link(self) -> bool:
+        """Generic attribute for models backed by a file."""
+        return False
+
+    def is_intermediate_link(self) -> bool:
+        """Generic attribute for models that have children fields that could contain files."""
+        return self.is_file_link()
+
+    def show_tree(self, indent=0):
+        """Recursive print function for showing a tree of a model."""
+        angle = "∟" if indent > 0 else ""
+
+        # Only print if we're backed by a file
+        if self.is_file_link():
+            print(" " * indent * 2, angle, self)
+
+        # Otherwise we recurse through the fields of a model
+        for _, value in self:
+            # Handle lists of items
+            if not isinstance(value, list):
+                value = [value]
+            for v in value:
+                if hasattr(v, "is_intermediate_link") and v.is_intermediate_link():
+                    # If the field is only an intermediate, print the name only
+                    if not v.is_file_link():
+                        print(" " * (indent * 2 + 2), angle, v.__class__.__name__)
+                    v.show_tree(indent + 1)
+
+    def _apply_recurse(self, f, *args, **kwargs):
+        # TODO Could we use this function for `show_tree`?
+        for _, value in self:
+            # Handle lists of items
+            if not isinstance(value, list):
+                value = [value]
+            for v in value:
+                if hasattr(v, "is_intermediate_link") and v.is_intermediate_link():
+                    v._apply_recurse(f, *args, **kwargs)
+
+        # Run self as last, so we can make use of the nested updates
+        if self.is_file_link():
+            getattr(self, f)(*args, **kwargs)
+
+    def _get_identifier(self, data: dict) -> str:
+        """Gets the identifier for this model.
+
+        Args:
+            data (dict): The data from which to retrieve the identifier
+
+        Returns:
+            str: The identifier or None.
+        """
+        return None
+
+
+class FileModel(BaseModel, ABC):
+    """Base class to represent models with a file representation.
+
+    It therefore always has a `filepath` and if it is given on
+    initilization, it will parse that file.
+
+    This class extends the `validate` option of Pydantic,
+    so when when a Path is given to a field with type `FileModel`,
+    it doesn't error, but actually initializes the `FileModel`.
+    """
+
+    __slots__ = ["__weakref__"]
+    # Use WeakValueDictionary to keep track of file paths with their respective parsed file models.
+    _file_models_cache: WeakValueDictionary = WeakValueDictionary()
+    filepath: Optional[Path] = None
+
+    def __new__(cls, filepath: Optional[Path] = None, *args, **kwargs):
+        """Creates a new model.
+        If the file at the provided file path was already parsed, this instance is returned.
+
+        Args:
+            filepath (Optional[Path], optional): The absolute file path to the file. Defaults to None.
+
+        Returns:
+            FileModel: A file model.
+        """
+
+        if filepath:
+            filepath = Path(filepath)
+
+            if filepath in FileModel._file_models_cache:
+                filemodel = FileModel._file_models_cache[filepath]
+                logger.info(
+                    f"Returning existing {type(filemodel).__name__} from cache, because {filepath} was already parsed."
+                )
+                return filemodel
+
+        return super().__new__(cls)
+
+    def __init__(self, filepath: Optional[Path] = None, *args, **kwargs):
+        """Initialize a model.
+
+        The model is empty (with defaults) if no `filepath` is given,
+        otherwise the file at `filepath` will be parsed."""
+        # Parse the file if path is given
+        context_dir_reset_token = None
+        if filepath:
+            filepath = Path(filepath)  # so we also accept strings
+
+            if filepath in FileModel._file_models_cache:
+                return None
+
+            # If not set, this is the root file path
+            if not context_dir.get(None):
+                logger.info(f"Set context to {filepath.parent}")
+                context_dir_reset_token = context_dir.set(filepath.parent)
+
+            FileModel._file_models_cache[filepath] = self
+
+            logger.info(f"Loading data from {filepath}")
+            data = self._load(filepath)
+            data["filepath"] = filepath
+            kwargs.update(data)
+
+        try:
+            super().__init__(*args, **kwargs)
+        finally:
+            _reset_context_dir(context_dir_reset_token)
+
+    def is_file_link(self) -> bool:
+        return True
+
+    @classmethod
+    def validate(cls: Type["FileModel"], value: Any):
+        # Enable initialization with a Path.
+        if isinstance(value, (Path, str)):
+            filepath = Path(value)
+
+            # Use the context if needed to resolve the absolute file path
+            if not filepath.is_absolute():
+                # The context_dir has been set within the initializer of the root FileModel
+                folder = context_dir.get()
+                logger.info(f"Used context to get {folder} for {filepath}")
+                filepath = folder / filepath
+
+            # Pydantic Model init requires a dict
+            value = {"filepath": filepath}
+        return super().validate(value)
+
+    def _load(self, filepath: Path) -> Dict:
+        # TODO Make this lazy in some cases
+        # so it doesn't become slow
+        if filepath.is_file():
+            return self._parse(filepath)
+        else:
+            warn(f"File: `{filepath}` not found, skipped parsing.")
+            return {}
+
+    def save(self, folder: Optional[Path] = None) -> Path:
+        """Save model and child models to their set filepaths.
+
+        If a folder is given, for models with an unset filepath,
+        we generate one based on the given `folder` and a default name.
+        Otherwise we override the folder part of already set filepaths.
+        This can thus be used to copy complete models.
+
+        Args:
+            folder: path to the folder where this FileModel will be stored
+        """
+        if not self.filepath and not folder:
+            raise ValueError(
+                "Either set the `filepath` on the model or pass a `folder` when saving."
+            )
+
+        if not folder:
+            folder = self.filepath.absolute().parent
+
+        self._apply_recurse("_save", folder)
+        return self.filepath.absolute()
+
+    def _save(self, folder):
+        filename = Path(self.filepath.name) if self.filepath else self._generate_name()
+        self.filepath = folder / filename
+
+        self._serialize(self.dict())
+
+    def _serialize(self, data: dict) -> None:
+        self._get_serializer()(self.filepath, data)
+
+    @classmethod
+    def _parse(cls, path: Path) -> Dict:
+        return cls._get_parser()(path)
+
+    @classmethod
+    def _generate_name(cls) -> Path:
+        name, ext = cls._filename(), cls._ext()
+        return Path(f"{name}{ext}")
+
+    @abstractclassmethod
+    def _filename(cls):
+        return "test"
+
+    @abstractclassmethod
+    def _ext(cls):
+        return ".test"
+
+    @abstractclassmethod
+    def _get_serializer(cls) -> Callable:
+        return DummySerializer.serialize
+
+    @abstractclassmethod
+    def _get_parser(cls) -> Callable:
+        return DummmyParser.parse
+
+    def __str__(self) -> str:
+        return str(self.filepath if self.filepath else "")
+
+    def _get_identifier(self, data: dict) -> str:
+        filepath = data.get("filepath")
+        if filepath:
+            return filepath.name
+        return None
---
+++ packages/hydrolib-core/hydrolib_core-0.1.4-py3-none-any.whl/hydrolib/core/io/ini/models.py
@@ -0,0 +1,275 @@
+import logging
+from abc import ABC
+from functools import reduce
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Literal,
+    Optional,
+    Sequence,
+    Set,
+    Tuple,
+    Type,
+    Union,
+)
+
+from pydantic import Extra, Field, root_validator
+from pydantic.class_validators import validator
+
+from hydrolib.core import __version__ as version
+from hydrolib.core.basemodel import BaseModel, FileModel
+from hydrolib.core.io.base import DummySerializer
+
+from .io_models import (
+    CommentBlock,
+    ContentElement,
+    Datablock,
+    DatablockRow,
+    Document,
+    Property,
+    Section,
+)
+from .parser import Parser
+from .serializer import Serializer, SerializerConfig, write_ini
+from .util import make_list_validator
+
+logger = logging.getLogger(__name__)
+
+
+class INIBasedModel(BaseModel, ABC):
+    """INIBasedModel defines the base model for ini models
+
+    INIBasedModel instances can be created from Section instances
+    obtained through parsing ini documents. It further supports
+    adding arbitrary fields to it, which will be written to file.
+    Lastly, no arbitrary types are allowed for the defined fields.
+
+    Attributes:
+        comments (Optional[Comments]):
+            Optional Comments if defined by the user.
+    """
+
+    _header: str
+
+    class Config:
+        extra = Extra.allow
+        arbitrary_types_allowed = False
+
+    @classmethod
+    def _supports_comments(cls):
+        return True
+
+    @classmethod
+    def _duplicate_keys_as_list(cls):
+        return False
+
+    @classmethod
+    def list_delimiter(cls) -> str:
+        return ";"
+
+    class Comments(BaseModel, ABC):
+        """Comments defines the comments of an INIBasedModel"""
+
+        class Config:
+            extra = Extra.allow
+            arbitrary_types_allowed = False
+
+    comments: Optional[Comments] = Comments()
+
+    @root_validator(pre=True)
+    def _skip_nones_and_set_header(cls, values):
+        """Drop None fields for known fields."""
+        dropkeys = []
+        for k, v in values.items():
+            if v is None and k in cls.__fields__.keys():
+                dropkeys.append(k)
+
+        logger.info(f"Dropped unset keys: {dropkeys}")
+        for k in dropkeys:
+            values.pop(k)
+
+        if "_header" in values:
+            values["_header"] = cls._header
+
+        return values
+
+    @validator("comments", always=True, allow_reuse=True)
+    def comments_matches_has_comments(cls, v):
+        if not cls._supports_comments() and v is not None:
+            logging.warning(f"Dropped unsupported comments from {cls.__name__} init.")
+            v = None
+        return v
+
+    @classmethod
+    def validate(cls: Type["INIBasedModel"], value: Any) -> "INIBasedModel":
+        if isinstance(value, Section):
+            value = value.flatten(
+                cls._duplicate_keys_as_list(), cls._supports_comments()
+            )
+
+        return super().validate(value)
+
+    @classmethod
+    def _exclude_fields(cls) -> Set:
+        return {"comments", "datablock", "_header"}
+
+    @staticmethod
+    def _convert_value(v: Any) -> str:
+        if isinstance(v, bool):
+            return str(int(v))
+        elif isinstance(v, list):
+            return ";".join([str(x) for x in v])
+        elif v is None:
+            return ""
+        else:
+            return str(v)
+
+    def _to_section(self) -> Section:
+        props = []
+        for key, value in self:
+            if key in self._exclude_fields():
+                continue
+
+            if key in self.__fields__:
+                key = self.__fields__[key].alias
+
+            prop = Property(
+                key=key,
+                value=INIBasedModel._convert_value(value),
+                comment=getattr(self.comments, key, None),
+            )
+            props.append(prop)
+        return Section(header=self._header, content=props)
+
+
+class DataBlockINIBasedModel(INIBasedModel):
+    """DataBlockINIBasedModel defines the base model for ini models with datablocks."""
+
+    datablock: List[List[float]] = []
+
+    _make_lists = make_list_validator("datablock")
+
+    def _to_section(self) -> Section:
+        section = super()._to_section()
+        section.datablock = self.datablock
+        return section
+
+
+class INIGeneral(INIBasedModel):
+    _header: Literal["General"] = "General"
+    fileversion: str = Field("3.00", alias="fileVersion")
+    filetype: str = Field(alias="fileType")
+
+    @classmethod
+    def _supports_comments(cls):
+        return True
+
+
+class FrictGeneral(INIGeneral):
+    fileversion: str = Field("3.00", alias="fileVersion")
+    filetype: Literal["roughness"] = Field("roughness", alias="fileType")
+
+
+class CrossdefGeneral(INIGeneral):
+    fileversion: str = Field("3.00", alias="fileVersion")
+    filetype: Literal["crossDef"] = Field("crossDef", alias="fileType")
+
+
+class CrosslockGeneral(INIGeneral):
+    fileversion: str = Field("3.00", alias="fileVersion")
+    filetype: Literal["crossLoc"] = Field("crossLoc", alias="fileType")
+
+
+class INIModel(FileModel):
+    """INI Model representation."""
+
+    general: INIGeneral
+
+    @classmethod
+    def _ext(cls) -> str:
+        return ".ini"
+
+    @classmethod
+    def _filename(cls) -> str:
+        return "fm"
+
+    @classmethod
+    def _get_serializer(cls):
+        pass  # unused in favor of direct _serialize
+
+    @classmethod
+    def _get_parser(cls) -> Callable:
+        return Parser.parse
+
+    def _to_document(self) -> Document:
+        header = CommentBlock(lines=[f"written by HYDROLIB-core {version}"])
+        sections = []
+        for _, value in self:
+            if _ == "filepath" or value is None:
+                continue
+            if isinstance(value, list):
+                for v in value:
+                    sections.append(v._to_section())
+            else:
+                sections.append(value._to_section())
+        return Document(header_comment=[header], sections=sections)
+
+    def _serialize(self, _: dict) -> None:
+        # We skip the passed dict for a better one.
+        config = SerializerConfig(section_indent=0, property_indent=4)
+        write_ini(self.filepath, self._to_document(), config=config)
+
+
+class Definition(INIBasedModel):
+    _header: Literal["Definition"] = "Definition"
+    id: str = Field(alias="id")
+    type: str = Field(alias="type")
+
+    @classmethod
+    def _duplicate_keys_as_list(cls):
+        return True
+
+
+class CrossDefModel(INIModel):
+    general: CrossdefGeneral = CrossdefGeneral()
+    definition: List[Definition] = []
+
+    _make_list = make_list_validator("definition")
+
+    @classmethod
+    def _filename(cls) -> str:
+        return "crsdef"
+
+
+class CrossSection(INIBasedModel):
+    _header: Literal["CrossSection"] = "CrossSection"
+    id: str = Field(alias="id")
+    branchid: str = Field(alias="branchId")
+
+
+class CrossLocModel(INIModel):
+    general: CrosslockGeneral = CrosslockGeneral()
+    crosssection: List[CrossSection] = []
+
+    @classmethod
+    def _filename(cls) -> str:
+        return "crsloc"
+
+
+class Global(INIBasedModel):
+    _header: Literal["Global"] = "Global"
+    frictionid: str = Field(alias="frictionId")
+    frictiontype: str = Field(alias="frictionType")
+    frictionvalue: float = Field(alias="frictionValue")
+
+
+class FrictionModel(INIModel):
+    general: FrictGeneral = FrictGeneral()
+    global_: List[Global] = Field([], alias="global")  # to circumvent built-in kw
+
+    _split_to_list = make_list_validator(
+        "global_",
+    )
