52ec08a2646c58019f5b472a0160a254721f05ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package phannh-hello-world-package/0.1/phannh_hello_world_package-0.1.tar.gz

---
+++ packages/phannh-hello-world-package/phannh_hello_world_package-0.1.tar.gz/phannh_hello_world_package-0.1/phannh_hello_world_package/__init__.py
@@ -0,0 +1,2 @@
+def hello_world():
+    print('Phannh Hello World')
---
+++ packages/phannh-hello-world-package/phannh_hello_world_package-0.1.tar.gz/phannh_hello_world_package-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'phannh_hello_world_package',
+  packages = ['phannh_hello_world_package'],
+  version = '0.1',
+  license='MIT',
+  description = 'phannh_hello_world_package',
+  author = 'Phan Nghiem Hai',
+  author_email = 'phannh@sea.com',
+  url = 'https://git.insea.io/phannh/phannh_hello_world_package',
+  download_url = 'https://git.insea.io/phannh/phannh_hello_world_package/archive/v_01.tar.gz',
+  keywords = ['phannh', 'hello_world'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
