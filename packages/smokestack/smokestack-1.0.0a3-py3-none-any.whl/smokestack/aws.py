369da9402070a6a4e484698c67be2556523da6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:46 +0000

    Add package smokestack/1.0.0a3/smokestack-1.0.0a3-py3-none-any.whl

---
+++ packages/smokestack/smokestack-1.0.0a3-py3-none-any.whl/smokestack/aws.py
@@ -0,0 +1,25 @@
+from time import sleep
+from typing import Callable, Optional
+
+from botocore.exceptions import ClientError
+
+
+def endeavor(
+    func: Callable[[], None], on_exception: Optional[Callable[[], None]] = None
+) -> None:
+    attempt = 0
+    max_attempts = 20
+
+    while attempt < max_attempts:
+        try:
+            func()
+            return
+        except ClientError as ex:
+            if "throttl" not in str(ex).lower():
+                if on_exception:
+                    on_exception()
+                    return
+                raise
+
+            sleep(8)
+            attempt += 1
