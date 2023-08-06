d3545ef58a5863504e23d3c7783666aaf9c3f05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:13 +0000

    Add package bm-cli/0.0.2/bm-cli-0.0.2.tar.gz

---
+++ packages/bm-cli/bm-cli-0.0.2.tar.gz/bm-cli-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bm-cli",
+    version="0.0.2",
+    author="coyote963",
+    author_email="coyoteandbird@gmail.com",
+    description="Command line interface for boring man servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/coyote963/bm-server-tools/",
+    project_urls={
+        "Bug Tracker": "https://github.com/coyote963/bm-server-tools/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "bm_cli"},
+    packages=setuptools.find_packages(where="bm_cli"),
+    python_requires=">=3.6",
+    entry_points = {
+        'console_scripts': [
+            'bmcli = bm_cli.cli:main',
+        ]
+    }
+)
