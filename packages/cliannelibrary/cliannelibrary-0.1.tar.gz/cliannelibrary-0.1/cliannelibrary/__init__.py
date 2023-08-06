00d497544c20808ec45763ff3fc4dd3736b7e8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:40 +0000

    Add package cliannelibrary/0.1/cliannelibrary-0.1.tar.gz

---
+++ packages/cliannelibrary/cliannelibrary-0.1.tar.gz/cliannelibrary-0.1/cliannelibrary/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: DmitryLianne
+
+:copyright: (c) 2021 DmitryLianne
+"""
+
+from .cliannelibrary import (FileLibrary)
+from .exceptions import (FileAlreadyExistsException)
+
+__author__ = 'DmitryLianne'
+__version__ = '0.1'
+__email__ = 'dmitrylianne@gmail.com'
