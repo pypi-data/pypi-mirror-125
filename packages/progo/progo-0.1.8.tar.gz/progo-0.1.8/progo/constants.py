5909b6d8f67c06fac28ead12aea3824f43891d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:39 +0000

    Add package progo/0.1.8/progo-0.1.8.tar.gz

---
+++ packages/progo/progo-0.1.8.tar.gz/progo-0.1.8/progo/constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+OPEN_GOPRO_BASE_URL = "http://10.5.5.9:8080"
+
+THUMBNAIL_DIR = Path("thumbnail/")
+SCREENNAIL_DIR = Path("screennail/")
+GPMF_DIR = Path("gpmf/")
+MEDIA_INFO_DIR = Path("media_info/")
+TELEMETRY_DIR = Path("telemetry/")
+DCIM_DIR = Path("dcim/")
