1b1d06754a13c3f8eae2827763cc802548312ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:08 +0000

    Add package disgames/1.0.2/disgames-1.0.2.tar.gz

---
+++ packages/disgames/disgames-1.0.2.tar.gz/disgames-1.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open("README.md") as f:
+        r = f.read()
+    return r
+
+setup(
+    name="disgames",
+    version="1.0.2",
+    description="A games module that can be used to instantly add games to your discord bot",
+    long_description=readme(),
+    author="andrew",
+    maintainer="Marcus",
+    url="https://github.com/andrewthederp/Disgames",
+    license="Apache",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=["discord.py","aiohttp"],
+    python_requires='>=3.6',
+    packages=find_packages(include=['disgames', 'disgames.*']),
+)
