13ac578cc29609d0c22fe24e9b0b4f9ec2f1c91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:58 +0000

    Add package torrent-organizer/0.1.0/torrent-organizer-0.1.0.tar.gz

---
+++ packages/torrent-organizer/torrent-organizer-0.1.0.tar.gz/torrent-organizer-0.1.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['torrent_organizer', 'torrent_organizer.console']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0',
+ 'parse-torrent-title>=2.3,<3.0',
+ 'pydantic>=1.8.2,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['torrent_organizer = torrent_organizer.console.run:cli']}
+
+setup_kwargs = {
+    'name': 'torrent-organizer',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Juan Olvera',
+    'author_email': 'juan@jolvera.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
