9ac8f9686ae073213b9c6f8cc3f1d370316fe2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:14 +0000

    Add package mmds/0.0.1.dev20211030214613/mmds-0.0.1.dev20211030214613.tar.gz

---
+++ packages/mmds/mmds-0.0.1.dev20211030214613.tar.gz/mmds-0.0.1.dev20211030214613/mmds/modalities/mel.py
@@ -0,0 +1,26 @@
+import attr
+
+from ..utils.spectrogram import LogMelSpectrogram, torch
+from .ts import TimeSeriesModality
+from .traits import CalculableModalityTrait
+
+
+@attr.define
+class MelModality(CalculableModalityTrait, TimeSeriesModality):
+    mel_fn: LogMelSpectrogram
+    sample_rate: float = attr.field(init=False)
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.sample_rate = self.mel_fn.rate
+
+    def calculate(self):
+        wav = self.base_modality.fetch()
+        with torch.no_grad():
+            wav = torch.from_numpy(wav)  # (t c)
+            mel = self.mel_fn(wav, dim=0).numpy()  # (t c d)
+        return mel
+
+    @property
+    def checksum(self):
+        return str(self.mel_fn)
