60231f4662f7b6ea6ed07dd06f664fb0a4cba0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:32 +0000

    Add package distributions-am/0.1/distributions_am-0.1.tar.gz

---
+++ packages/distributions-am/distributions_am-0.1.tar.gz/distributions_am-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_am',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_am'],
+      zip_safe=False)
