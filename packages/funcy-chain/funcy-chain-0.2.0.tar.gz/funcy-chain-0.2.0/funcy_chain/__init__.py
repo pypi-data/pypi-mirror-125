ce2a07cf8ff6eb635b5e9faa49df3a27dd846587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:00 +0000

    Add package funcy-chain/0.2.0/funcy-chain-0.2.0.tar.gz

---
+++ packages/funcy-chain/funcy-chain-0.2.0.tar.gz/funcy-chain-0.2.0/funcy_chain/__init__.py
@@ -0,0 +1,12 @@
+"""Method chaining with funcy functions."""
+
+__version__ = "0.2.0"
+
+
+from .chain import Chain
+from .iterchain import IterChain
+
+__all__ = [
+    "Chain",
+    "IterChain",
+]
---
+++ packages/funcy-chain/funcy-chain-0.2.0.tar.gz/funcy-chain-0.2.0/test/test_heapq.py
@@ -0,0 +1,14 @@
+def test_nlargest(Chain):
+    assert Chain([]).nlargest(0).value == []
+    assert Chain([]).nlargest(1).value == []
+    assert Chain([1, 3, 2, 4, 3]).nlargest(2).value == [4, 3]
+    assert Chain([1, 3, 2, 4, 3]).nlargest(3).value == [4, 3, 3]
+    assert Chain([1, 3, 2, 4, 1]).nlargest(0).value == []
+
+
+def test_nsmallest(Chain):
+    assert Chain([]).nsmallest(0).value == []
+    assert Chain([]).nsmallest(1).value == []
+    assert Chain([1, 3, 2, 4, 1]).nsmallest(2).value == [1, 1]
+    assert Chain([1, 3, 2, 4, 1]).nsmallest(3).value == [1, 1, 2]
+    assert Chain([1, 3, 2, 4, 1]).nsmallest(0).value == []
