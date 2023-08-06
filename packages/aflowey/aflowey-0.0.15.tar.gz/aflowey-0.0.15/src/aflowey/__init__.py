6abd2240d26fd1cc87d702401e54a4b34b8212f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:57 +0000

    Add package aflowey/0.0.15/aflowey-0.0.15.tar.gz

---
+++ packages/aflowey/aflowey-0.0.15.tar.gz/aflowey-0.0.15/src/aflowey/__init__.py
@@ -0,0 +1,74 @@
+from aflowey.f import (
+    F,
+)
+from aflowey.functions import (
+    async_wrap,
+    ensure_callable,
+    side_effect,
+    may_fail,
+    spread,
+    spread_args,
+    spread_kw,
+    spread_kwargs,
+    f0,
+    F0,
+    f1,
+    F1,
+    erratic,
+    breaker,
+    lift,
+    imp,
+    impure,
+    apartial,
+    partial,
+    identity,
+    log,
+    flog,
+)
+from aflowey.async_flow import (
+    AsyncFlow,
+    aflow,
+    async_flow,
+)
+from aflowey.executor import (
+    AsyncFlowExecutor,
+    async_exec,
+    aexec,
+)
+
+from aflowey.single_executor import (
+    CANCEL_FLOW,
+)
+
+__all__ = [
+    "F",
+    "async_wrap",
+    "ensure_callable",
+    "side_effect",
+    "f0",
+    "F0",
+    "f1",
+    "F1",
+    "erratic",
+    "breaker",
+    "may_fail",
+    "lift",
+    "imp",
+    "impure",
+    "AsyncFlow",
+    "aflow",
+    "async_flow",
+    "AsyncFlowExecutor",
+    "async_exec",
+    "aexec",
+    "apartial",
+    "partial",
+    "spread",
+    "spread_args",
+    "spread_kw",
+    "spread_kwargs",
+    "identity",
+    "CANCEL_FLOW",
+    "flog",
+    "log",
+]
