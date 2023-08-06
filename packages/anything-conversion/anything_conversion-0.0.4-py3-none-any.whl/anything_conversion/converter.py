42a8c78ee82bc261a32a984472984a30e2c6573c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:30 +0000

    Add package anything-conversion/0.0.4/anything_conversion-0.0.4-py3-none-any.whl

---
+++ packages/anything-conversion/anything_conversion-0.0.4-py3-none-any.whl/anything_conversion/converter.py
@@ -0,0 +1,595 @@
+from __future__ import division
+import math
+class AreaConverter():
+    def squareyard_squarefeet_or_foot(sy):
+        """
+        here f
+        1 square yard = 9 suare feet
+        
+        """
+        res=9*sy
+        return res
+    def acre_squarefeet_or_foot(acr):
+        """
+        here f
+        1 acre = 43560 suare feet
+        
+        """
+        res=acr*43560
+        return res
+
+class WeightConverter():
+    def kilogram_gram(kg):
+        """
+        here f
+        1 kilogram = 1000 grams 
+        
+        """
+        res=kg*1000
+        return res
+    def metricton_kilogram(me):
+        """
+        here f
+        1 metricton = 1000 kg
+        
+        """
+        res=me*1000
+        return res
+    def kilogram_pound(kg):
+        """
+        here
+        1 kilogram  = 2.2046226218488 pound
+        
+        """
+        res=kg*2.2046226218488
+        return res
+    def kilogram_ounce(kg):
+        """
+        here
+        1 kilogram = 35.27396194958 ounce
+        
+        """
+        res=kg*35.27396194958
+        return res
+    def kilogram_carat(kg):
+        """
+        here
+        1 kilogram = 5000 carat
+        
+        """
+        res=kg*5000
+        return res
+    def kilogram_short_ton_US(kg):
+        """
+        here
+        1 kilogram = 0.0011023113109244 short_ton_US
+        
+        """
+        res=kg*0.0011023113109244
+        return res
+    def kilogram_long_ton_UK(kg):
+        """
+        here
+        1 kilogram = 0.00098420652761106 long_ton_UK
+        
+        """
+        res=kg*0.00098420652761106
+        return res
+    def kilogram_metric_ton(kg):
+        """
+        here
+        1 kilogram = 0.001 metric ton
+        
+        """
+        res=kg*0.001
+        return res
+    def kilogram_grain(kg):
+        """
+        here
+        1 kilogram = 15432.358352941 grain
+        
+        """
+        res=kg*15432.358352941
+        return res
+    def gram_kilogram(gr):
+        """
+        here
+        1 gram = 0.001 kilogram
+        
+        """
+        res=gr*0.001
+        return res
+    def gram_pound(gr):
+        """
+        here
+        1 gram = 0.0022046226218488 0.0022046226218488 pound
+        
+        """
+        res=gr*0.0022046226218488
+        return res
+    def gram_ounce(gr):
+        """
+        here
+        1 gram = 0.03527396194958 ounce
+        
+        """
+        res=gr*0.03527396194958
+        return res
+    def gram_carat(gr):
+        """
+        here
+        1 gram = 5 carat
+        
+        """
+        res=gr*5
+        return res
+    def gram_short_ton_US(gr):
+        """
+        here
+        1 gram = 0.0000011023113109244 short ton us
+        
+        """
+        res=gr*0.0000011023113109244
+        return res
+    def gram_long_ton_UK(gr):
+        """
+        here
+        1 gram = 0.00000098420652761106 long ton UK
+        
+        """
+        res=gr*0.00000098420652761106
+        return res
+    def gram_metric_ton(gr):
+        """
+        here
+        1 gram = 0.000001 metric ton
+        
+        """
+        res=gr*0.000001
+        return res
+    def gram_grain(gr):
+        """
+        here
+        1 gram = 15.432358352941
+        
+        """
+        res=gr*15.432358352941
+        return res
+    def pound_gram(po):
+        """
+        here
+        1 pound = 453.59237 gram
+        
+        """
+        res=po*453.59237
+        return res
+    def pound_kilogram(po):
+        """
+        here
+        1 pound = 0.45359237 kilogram
+        
+        """
+        res=po*0.45359237
+        return res
+    def pound_ounce(po):
+        """
+        here
+        1 pound = 16 ounce
+        
+        """
+        res=po*16
+        return res
+    def pound_carat(po):
+        """
+        here
+        1 pound = 2267.96185 carat
+        
+        """
+        res=po*2267.96185
+        return res
+    def pound_short_ton_US(po):
+        """
+        here
+        1 pound = 0.0005 short ton us
+        
+        """
+        res=po*0.0005
+        return res
+    def pound_long_ton_UK(po):
+        """
+        here
+        1 pound = 0.00044642857142857  long ton UK
+        
+        """
+        res=po*0.00044642857142857
+        return res
+    def pound_metric_ton(po):
+        """
+        here
+        1 pound =  0.00045359237 metric ton
+        
+        """
+        res=po*0.00045359237
+        return res
+    def pound_grain(po):
+        """
+        here
+        1 pound = 7000 grain 
+        
+        """
+        res=po*7000
+        return res
+    def ounce_gram(ou):
+        """
+        here
+        1 ounce = 28.349523125 gram
+        
+        """
+        res=ou*28.349523125
+        return res
+    def ounce_kilogram(ou):
+        """
+        here
+        1 ounce = 0.028349523125 kilogram 
+        """
+        res=ou*0.028349523125
+        return res
+    def ounce_pound(ou):
+        """
+        here
+        1 ounce = 0.0625 pound
+        
+        """
+        res=ou*0.0625
+        return res
+    def ounce_carat(ou):
+        """
+        here
+        1 ounce = 141.747615625 carat
+        
+        """
+        res=ou*141.747615625
+        return res
+    def ounce_short_ton_US(ou):
+        """
+        here
+        1 ounce = 0.00003125 short ton US
+        
+        """
+        res=ou*0.00003125
+        return res
+    def ounce_long_ton_UK(ou):
+        """
+        here
+        1 ounce = 0.000027901785714286 long ton UK
+        
+        """
+        res=ou*0.000027901785714286
+        return res
+    def ounce_metric_ton(ou):
+        """
+        here
+        1 ounce = 0.000028349523125 metric ton
+        
+        """
+        res=ou*0.000028349523125
+        return res
+    def ounce_grain(ou):
+        """
+        here
+        1 ounce = 437.5 grain 
+        
+        """
+        res=ou*437.5
+        return res
+    def carat_gram(ca):
+        """
+        here
+        1 carat = 0.2 gram
+        
+        """
+        res=ca*0.2
+        return res
+    def carat_kilogram(ca):
+        """
+        here
+        1 carat = 0.0002 kilogram
+        
+        """
+        res=ca*0.0002
+        return res
+    def carat_pound(ca):
+        """
+        here
+        1 carat = 0.00044092452436976 pound
+        
+        """
+        res=ca*0.00044092452436976
+        return res
+    def carat_ounce(ca):
+        """
+        here
+        1 carat = 0.0070547923899161 ounce
+        
+        """
+        res=ca*0.0070547923899161
+        return res
+    def carat_short_ton_US(ca):
+        """
+        here
+        1 carat = 0.00000022046226218488 short ton US
+        
+        """
+        res=ca*0.00000022046226218488
+        return res
+    def carat_long_ton_UK(ca):
+        """
+        here
+        1 carat = 0.00000019684130552221 long ton UK
+        
+        """
+        res=ca*0.00000019684130552221
+        return res
+    def carat_metric_ton(ca):
+        """
+        here
+        1 carat = 0.0000002 metric ton
+        
+        """
+        res=ca*0.0000002
+        return res
+    def carat_grain(ca):
+        """
+        here
+        1 carat = 3.0864716705883 grain 
+        
+        """
+        res=ca*3.0864716705883
+        return res
+    def short_ton_US_gram(sh):
+        """
+        here
+        1 short_ton_US = 907184.74 gram
+        
+        """
+        res=sh*907184.74
+        return res
+    def short_ton_US_kilogram(sh):
+        """
+        here
+        1 short_ton_US = 907.18474 kilogram
+        
+        """
+        res=sh*907.18474
+        return res
+    def short_ton_US_pound(sh):
+        """
+        here
+        1 short_ton_US = 2000 pound
+        
+        """
+        res=sh*2000
+        return res
+    def short_ton_US_ounce(sh):
+        """
+        here
+        1 short_ton_US = 32000 ounce
+        
+        """
+        res=sh*32000
+        return res
+    def short_ton_US_carat(sh):
+        """
+        here
+        1 short_ton_US = 4535923.7 carat
+        
+        """
+        res=sh*4535923.7
+        return res
+    def short_ton_US_long_ton_UK(sh):
+        """
+        here
+        1 short_ton_US = 0.89285714285714 long ton UK
+        
+        """
+        res=sh*0.89285714285714
+        return res
+    def short_ton_US_metric_ton(sh):
+        """
+        here
+        1 short_ton_US = 0.90718474 metric ton
+        
+        """
+        res=sh*0.90718474
+        return res
+    def short_ton_US_grain(sh):
+        """
+        here
+        1 short_ton_US = 14000000 grain
+        
+        """
+        res=sh*14000000
+        return res
+    def long_ton_UK_gram(lo):
+        """
+        here
+        1 long_ton_UK = 1016046.9088 gram
+        
+        """
+        res=lo*1016046.9088
+        return res
+    def long_ton_UK_kilogram(lo):
+        """
+        here
+        1 long_ton_UK = 1016.0469088 kilogram
+        
+        """
+        res=lo*1016.0469088
+        return res
+    def long_ton_UK_pound(lo):
+        """
+        here
+        1 long_ton_UK = 2240 pound
+        
+        """
+        res=lo*2240
+        return res
+    def long_ton_UK_ounce(lo):
+        """
+        here
+        1 long_ton_UK = 35840 ounce
+        
+        """
+        res=lo*35840
+        return res
+    def long_ton_UK_carat(lo):
+        """
+        here
+        1 long_ton_UK = 5080234.544 carat
+        
+        """
+        res=lo*5080234.544
+        return res
+    def long_ton_UK_short_ton_US(lo):
+        """
+        here
+        1 long_ton_UK = 1.12 short ton US
+        
+        """
+        res=lo*1.12
+        return res
+    def long_ton_UK_metric_ton(lo):
+        """
+        here
+        1 long_ton_UK = 1.0160469088 metric_ton
+        
+        """
+        res=lo*1.0160469088
+        return res
+    def long_ton_UK_grain(lo):
+        """
+        here
+        1  long_ton_UK = 15680000 grain
+        
+        """
+        res=lo*15680000
+        return res
+    def metric_ton_gram(me):
+        """
+        here
+        1 metric_ton = 1000000 gram
+        
+        """
+        res=me*1000000
+        return res
+    def metric_ton_pound(me):
+        """
+        here
+        1 metric_ton = 2204.6226218488 pound
+        
+        """
+        res=me*2204.6226218488
+        return res
+    def metric_ton_ounce(me):
+        """
+        here
+        1 metric_ton = 35273.96194958 ounce
+        
+        """
+        res=me*35273.96194958
+        return res
+    def metric_ton_carat(me):
+        """
+        here
+        1 metric_ton = 5000000 carat
+        
+        """
+        res=me*5000000
+        return res
+    def metric_ton_short_ton_US(me):
+        """
+        here
+        1 metric_ton = 1.1023113109244 short ton US
+        
+        """
+        res=me*1.1023113109244
+        return res
+    def metric_ton_long_ton_UK(me):
+        """
+        here
+        1 metric_ton = 0.984206527611061106 long ton UK
+        
+        """
+        res=me*0.984206527611061106
+        return res
+    def metric_ton_grain(me):
+        """
+        here
+        1 metric_ton = 15432358.352941 grain
+        
+        """
+        res=me*15432358.352941
+        return res
+    def grain_gram(gn):
+        """
+        here
+        1 grain = 0.06479891 grain
+        
+        """
+        res=gn*0.06479891
+        return res
+    def grain_kilogram(gn):
+        """
+        here
+        1 grain = 0.00006479891 kilogram
+        
+        """
+        res=gn*0.00006479891
+        return res
+    def grain_pound(gn):
+        """
+        here
+        1 grain = 0.00014285714285714 pound
+        """
+        res=gn*0.00014285714285714
+        return res
+    def grain_ounce(gn):
+        """
+        here
+        1 grain = 0.0022857142857143 ounce
+        
+        """
+        res=gn*0.0022857142857143
+        return res
+    def grain_carat(gn):
+        """
+        here
+        1 grain = 0.32399455 carat
+        
+        """
+        res=gn*0.32399455
+        return res
+    def grain_short_ton_US(gn):
+        """
+        here
+        1 grain = 0.000000071428571428571 short ton US
+        
+        """
+        res=gn*0.000000071428571428571
+        return res
+    def grain_long_ton_UK(gn):
+        """
+        here
+        1 grain = 0.000000063775510204082 long ton UK
+        
+        """
+        res=gn*0.000000063775510204082
+        return res
+    def grain_metric_ton(gn):
+        """
+        here
+        1 grain = 0.00000006479891 metric_ton
+        
+        """
+        res=gn*0.00000006479891
+        return res
