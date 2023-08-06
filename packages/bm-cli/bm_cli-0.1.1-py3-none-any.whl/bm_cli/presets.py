d7559343070247f0420c613adb563b9af3f1d6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:29 +0000

    Add package bm-cli/0.1.1/bm_cli-0.1.1-py3-none-any.whl

---
+++ packages/bm-cli/bm_cli-0.1.1-py3-none-any.whl/bm_cli/presets.py
@@ -0,0 +1,25 @@
+preset_settings = {
+    'tdm': {
+        'Server': {
+            "Bots" : "0",
+            "Gamemode": "2",
+            "MapSelection": "1",
+            "Hazards": "0",
+            "Powerups": "0",
+            "Name": "Team Deathmatch"
+        },
+        'ServerMutators': {
+            "CaptEnable": "1",
+        }
+    },
+    'dm': {
+        'Server': {
+            "Bots" : "3",
+            "Gamemode": "0",
+            "MapSelection": "1",
+            "Hazards": "0",
+            "Powerups": "1",
+            "Name": "Deathmatch"
+        }
+    },
+}
