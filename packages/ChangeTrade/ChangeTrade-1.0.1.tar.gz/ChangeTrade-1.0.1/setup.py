38b6ebf5699852731be26439884ef2ff71c1c3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:33 +0000

    Add package ChangeTrade/1.0.1/ChangeTrade-1.0.1.tar.gz

---
+++ packages/ChangeTrade/ChangeTrade-1.0.1.tar.gz/ChangeTrade-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+setup(
+	name="ChangeTrade",
+	version="1.0.1",
+	packages=find_packages(), # permet de récupérer tout les fichiers 
+	description="My usefull functions trading",
+	url="https://None.come",
+	author="Charles Hajjar",
+	license="WTFPL",
+	python_requires=">=3.9"
+	)
