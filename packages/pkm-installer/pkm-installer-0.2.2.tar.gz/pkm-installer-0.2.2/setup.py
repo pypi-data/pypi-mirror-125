5b70b2049073612c794e02fbfc120930088a2616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package pkm-installer/0.2.2/pkm-installer-0.2.2.tar.gz

---
+++ packages/pkm-installer/pkm-installer-0.2.2.tar.gz/pkm-installer-0.2.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pkm_installer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['importlib-metadata>=1.7.0', 'virtualenv>=20.9.0,<21.0.0']
+
+setup_kwargs = {
+    'name': 'pkm-installer',
+    'version': '0.2.2',
+    'description': 'lightweight installer for pkm',
+    'long_description': '',
+    'author': 'bennyl',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
