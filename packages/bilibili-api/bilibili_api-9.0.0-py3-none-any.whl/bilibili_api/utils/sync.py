dbfbb1bd7d0ed4f6d7a03a6df25e0882b1f226f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:15 +0000

    Add package bilibili-api/9.0.0/bilibili_api-9.0.0-py3-none-any.whl

---
+++ packages/bilibili-api/bilibili_api-9.0.0-py3-none-any.whl/bilibili_api/utils/sync.py
@@ -0,0 +1,17 @@
+from typing import Coroutine
+import asyncio
+
+
+def sync(coroutine: Coroutine):
+    """
+    同步执行异步函数，使用可参考 [同步执行异步代码](https://www.moyu.moe/bilibili-api/#/sync-executor)
+
+    Args:
+        coroutine (Coroutine): 异步函数
+
+    Returns:
+        该异步函数的返回值
+    """
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(coroutine)
+
