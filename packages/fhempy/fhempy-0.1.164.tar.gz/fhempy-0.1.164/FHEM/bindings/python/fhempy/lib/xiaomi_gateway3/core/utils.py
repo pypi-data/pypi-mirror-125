4849728d73bbf7a1b36e8cf54dbaef813164e3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:15 +0000

    Add package fhempy/0.1.164/fhempy-0.1.164.tar.gz

---
+++ packages/fhempy/fhempy-0.1.164.tar.gz/fhempy-0.1.164/FHEM/bindings/python/fhempy/lib/xiaomi_gateway3/core/utils.py
@@ -0,0 +1,173 @@
+import json
+import logging
+import random
+import re
+import string
+import time
+import functools
+from typing import List, Optional
+
+from fhempy.lib import utils
+
+import requests
+
+from .mini_miio import SyncmiIO
+from .shell import TelnetShell
+from .xiaomi_cloud import MiCloud
+
+DOMAIN = "xiaomi_gateway3"
+
+_LOGGER = logging.getLogger(__name__)
+
+
+# new miio adds colors to logs
+RE_JSON1 = re.compile(b"msg:(.+) length:([0-9]+) bytes")
+RE_JSON2 = re.compile(b"{.+}")
+
+
+def extract_jsons(raw) -> List[bytes]:
+    """There can be multiple concatenated json on one line. And sometimes the
+    length does not match the message."""
+    m = RE_JSON1.search(raw)
+    if m:
+        length = int(m[2])
+        raw = m[1][:length]
+    else:
+        m = RE_JSON2.search(raw)
+        raw = m[0]
+    return raw.replace(b"}{", b"}\n{").split(b"\n")
+
+
+def migrate_options(data):
+    data = dict(data)
+    options = {k: data.pop(k) for k in ("ble", "zha") if k in data}
+    return {"data": data, "options": options}
+
+
+def check_mgl03(host: str, token: str, telnet_cmd: Optional[str]) -> Optional[str]:
+    try:
+        # 1. try connect with telnet (custom firmware)?
+        shell = TelnetShell(host)
+        # 1.1. check token with telnet
+        return None if shell.get_token() == token else "wrong_token"
+    except:
+        if not telnet_cmd:
+            return "cant_connect"
+
+    # 2. try connect with miio
+    miio = SyncmiIO(host, token)
+    info = miio.info()
+    # fw 1.4.6_0012 without cloud will respond with a blank string reply
+    if info is None:
+        # if device_id not None - device works but not answer on commands
+        return "wrong_token" if miio.device_id else "cant_connect"
+
+    # 3. check if right model
+    if info and info["model"] != "lumi.gateway.mgl03":
+        return "wrong_model"
+
+    raw = json.loads(telnet_cmd)
+    # fw 1.4.6_0043+ won't answer on cmd without cloud, so don't check answer
+    miio.send(raw["method"], raw.get("params"))
+
+    # waiting for telnet to start
+    time.sleep(1)
+
+    try:
+        # 4. check if telnet command helps
+        TelnetShell(host)
+    except:
+        return "wrong_telnet"
+
+
+def get_lan_key(host: str, token: str):
+    device = SyncmiIO(host, token)
+    resp = device.send("get_lumi_dpf_aes_key")
+    if resp is None:
+        return "Can't connect to gateway"
+    if len(resp[0]) == 16:
+        return resp[0]
+    key = "".join(
+        random.choice(string.ascii_lowercase + string.digits) for _ in range(16)
+    )
+    resp = device.send("set_lumi_dpf_aes_key", [key])
+    if resp[0] == "ok":
+        return key
+    return "Can't update gateway key"
+
+
+async def get_room_mapping(cloud: MiCloud, host: str, token: str):
+    try:
+        device = SyncmiIO(host, token)
+        local_rooms = device.send("get_room_mapping")
+        cloud_rooms = await cloud.get_rooms()
+        result = ""
+        for local_id, cloud_id in local_rooms:
+            cloud_name = next(
+                (p["name"] for p in cloud_rooms if p["id"] == cloud_id), "-"
+            )
+            result += f"\n- {local_id}: {cloud_name}"
+        return result
+
+    except Exception:
+        return "Can't get from cloud"
+
+
+async def get_bindkey(cloud: MiCloud, did: str):
+    bindkey = await cloud.get_bindkey(did)
+    if bindkey is None:
+        return "Can't get from cloud"
+    if bindkey.endswith("FFFFFFFF"):
+        return "Not needed"
+    return bindkey
+
+
+def reverse_mac(s: str):
+    return f"{s[10:]}{s[8:10]}{s[6:8]}{s[4:6]}{s[2:4]}{s[:2]}"
+
+
+EZSP_URLS = {
+    7: "https://master.dl.sourceforge.net/project/mgl03/zigbee/"
+    "ncp-uart-sw_mgl03_6_6_2_stock.gbl?viasf=1",
+    8: "https://master.dl.sourceforge.net/project/mgl03/zigbee/"
+    "ncp-uart-sw_mgl03_6_7_8_z2m.gbl?viasf=1",
+}
+
+
+def _update_zigbee_firmware(host: str, ezsp_version: int):
+    shell = TelnetShell(host)
+
+    # stop all utilities without checking if they are running
+    shell.stop_lumi_zigbee()
+    shell.stop_zigbee_tcp()
+    # flash on another port because running ZHA or z2m can breake process
+    shell.run_zigbee_tcp(port=8889)
+    time.sleep(0.5)
+
+    _LOGGER.info(f"Try update EZSP to version {ezsp_version}")
+
+    from .elelabs_ezsp_utility import ElelabsUtilities
+
+    config = type(
+        "", (), {"port": (host, 8889), "baudrate": 115200, "dlevel": _LOGGER.level}
+    )
+    utils = ElelabsUtilities(config, _LOGGER)
+
+    # check current ezsp version
+    resp = utils.probe()
+    _LOGGER.info(f"EZSP before flash: {resp}")
+    if resp[0] == 0 and resp[1] == ezsp_version:
+        return True
+
+    url = EZSP_URLS[ezsp_version]
+    r = requests.get(url)
+
+    resp = utils.flash(r.content)
+    _LOGGER.info(f"EZSP after flash: {resp}")
+    return resp[0] == 0 and resp[1] == ezsp_version
+
+
+async def update_zigbee_firmware(host: str, ezsp_version: int):
+    return await utils.run_blocking(
+        functools.partial(_update_zigbee_firmware, host, ezsp_version)
+    )
---
+++ packages/fhempy/fhempy-0.1.164.tar.gz/fhempy-0.1.164/FHEM/bindings/python/fhempy/lib/xiaomi_gateway3/xiaomi_gateway3.py
@@ -0,0 +1,185 @@
+# THANKS to https://github.com/AlexxIT/XiaomiGateway3 for the HASS implementation
+# which I used as a base for the FHEM implementation
+
+import asyncio
+import functools
+import logging
+
+from .. import fhem, utils
+from .. import generic
+from .core.gateway3 import GatewayEntry
+from .core.utils import update_zigbee_firmware
+
+DOMAINS = [
+    "binary_sensor",
+    "climate",
+    "cover",
+    "light",
+    "remote",
+    "sensor",
+    "switch",
+    "alarm_control_panel",
+    "device_tracker",
+]
+
+
+class xiaomi_gateway3(generic.FhemModule):
+    def __init__(self, logger):
+        super().__init__(logger)
+        self.gw = None
+        self.devices = {}
+        self.fhempy_devices = {}
+
+    @property
+    def gateway3(self):
+        return self.gw.gateway3
+
+    # FHEM FUNCTION
+    async def Define(self, hash, args, argsh):
+        await super().Define(hash, args, argsh)
+        self.hash = hash
+
+        if len(args) < 5:
+            return "Usage: define devname fhempy xiaomi_gateway3 <IP> <TOKEN>"
+
+        if await fhem.AttrVal(self.hash["NAME"], "icon", "") == "":
+            await fhem.CommandAttr(self.hash, self.hash["NAME"] + " icon mqtt")
+        await fhem.readingsSingleUpdateIfChanged(hash, "state", "disconnected", 1)
+
+        logging.getLogger("fhempy.lib.xiaomi_gateway3.core.gateway3").setLevel(
+            logging.ERROR
+        )
+
+        hash["HOST"] = args[3]
+        hash["TOKEN"] = args[4]
+
+        self.host = args[3]
+        self.token = args[4]
+
+        set_conf = {
+            "activate_zigbee2mqtt": {
+                "help": (
+                    "You need to install zigbee2mqtt aftewards<br>"
+                    "=> https://www.zigbee2mqtt.io/getting_started/running_zigbee2mqtt.html#2-installing<br>"
+                    "Set following lines in configuration.yaml<br>"
+                    " serial:<br>"
+                    "   port: tcp://GATEWAY_IP_ADDRESS:8888<br>"
+                    "   adapter: ezsp<br>"
+                    " mqtt:<br>"
+                    "   client_id: zigbee_pi<br>"
+                )
+            },
+            "deactivate_zigbee2mqtt": {},
+        }
+        self.set_set_config(set_conf)
+
+        self.create_async_task(self.connect_gw())
+
+        return ""
+
+    async def set_activate_zigbee2mqtt(self, hash, params):
+        ezsp_version = 8
+        self.create_async_task(update_zigbee_firmware(self.host, ezsp_version))
+        await fhem.readingsSingleUpdateIfChanged(hash, "zigbee2mqtt", "on", 1)
+
+    async def set_deactivate_zigbee2mqtt(self, hash, params):
+        ezsp_version = 7
+        self.create_async_task(update_zigbee_firmware(self.host, ezsp_version))
+        await fhem.readingsSingleUpdateIfChanged(hash, "zigbee2mqtt", "off", 1)
+
+    async def register_device(self, fhempy_device, handler):
+        did = fhempy_device.did
+        self.fhempy_devices[did] = fhempy_device
+        # wait till gateway is ready
+        while self.gw is None:
+            await asyncio.sleep(3)
+        # if did == "lumi.0":
+        #    self.gw.add_stats(did, handler)
+        await asyncio.sleep(3)
+        if did in self.devices and "init" in self.devices[did]:
+            await fhempy_device.initialize(self.devices[did])
+
+    async def connect_gw(self):
+        await asyncio.sleep(0)
+        config = {"devices": {}}
+        z2m = await fhem.ReadingsVal(self.hash["NAME"], "zigbee2mqtt", "off")
+        if z2m == "on":
+            config["zha"] = True
+        self.gw = FhempyGateway(self.logger)
+        await self.gw.create_gateway(self.hash, self.host, self.token, config)
+        # prepare domains
+        for domain in DOMAINS:
+            self.gw.add_setup(domain, self.create_device)
+        # start check task
+        self.create_async_task(self.is_connected())
+        # run gateway
+        self.gw.start()
+
+    async def is_connected(self):
+        while True:
+            if await self.gw.is_connected():
+                await fhem.readingsSingleUpdateIfChanged(
+                    self.hash, "state", "connected", 1
+                )
+            else:
+                await fhem.readingsSingleUpdateIfChanged(
+                    self.hash, "state", "disconnected", 1
+                )
+            await asyncio.sleep(60)
+
+    async def create_device(self, gw, device, type):
+        self.logger.debug(f"Check if device {device['did']} exists")
+        did = device["did"]
+        self.devices[did] = device
+        if not await fhem.checkIfDeviceExists(
+            self.hash, "PYTHONTYPE", "xiaomi_gateway3_device", "DID", did
+        ):
+            devname = (
+                "".join(filter(str.isalnum, device["model"])) + "_" + device["mac"]
+            )
+            await fhem.CommandDefine(
+                self.hash,
+                devname
+                + " PythonModule xiaomi_gateway3_device "
+                + self.hash["NAME"]
+                + " "
+                + did,
+            )
+            # wait for fhem
+            await asyncio.sleep(1)
+        else:
+            if "init" in device and did in self.fhempy_devices:
+                if did == "lumi." + device["mac"]:
+                    device["version"] = self.gw.version()
+                await self.fhempy_devices[did].initialize(device)
+
+
+class FhempyGateway:
+    def __init__(self, logger):
+        self.loop = asyncio.get_event_loop()
+
+    async def create_gateway(self, hash, host, token, config):
+        self.gw = await utils.run_blocking(
+            functools.partial(GatewayEntry, host=host, token=token, options=config)
+        )
+
+    @property
+    def gateway3(self):
+        return self.gw
+
+    def add_setup(self, domain, handler):
+        def setup(gw, device, attr):
+            asyncio.run_coroutine_threadsafe(
+                handler(gw, device, attr), self.loop
+            ).result()
+
+        self.gw.add_setup(domain, setup)
+
+    def version(self):
+        return self.gw.ver
+
+    def start(self):
+        utils.run_blocking_task(functools.partial(self.gw.start))
+
+    async def is_connected(self):
+        return await utils.run_blocking(functools.partial(self.gw._check_port, 23))
