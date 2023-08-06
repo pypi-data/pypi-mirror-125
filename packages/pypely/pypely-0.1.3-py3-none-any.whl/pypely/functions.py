9a5ec0077273c119ab7b781303f947e73f9840cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:21 +0000

    Add package pypely/0.1.3/pypely-0.1.3-py3-none-any.whl

---
+++ packages/pypely/pypely-0.1.3-py3-none-any.whl/pypely/functions.py
@@ -0,0 +1,39 @@
+from functools import reduce
+from typing import Callable, Tuple, TypeVar
+from .helpers import flatten
+from .types import PypelyTuple
+
+T = TypeVar("T")
+
+def pipeline(*funcs: Callable) -> Callable:
+    def _reducer(func1, func2):
+        return lambda *x: func2(func1(*x))
+
+    return reduce(_reducer, funcs)
+
+
+def fork(*funcs: Callable) -> Callable[..., PypelyTuple]:
+    return lambda *x: PypelyTuple(*[func(*x) for func in funcs])
+
+
+def to(obj: T, *set_fields: str) -> Callable[[PypelyTuple], T]:
+    def _inner(vals: PypelyTuple) -> T:
+        vals_flattened = flatten(vals)
+        if not set_fields == ():
+            assert len(vals_flattened) == len(set_fields)
+            fields_named = {field_name: val for field_name, val in zip(set_fields, vals_flattened)}
+            return obj(**fields_named)
+        else:
+            return obj(*vals_flattened)
+    
+    return _inner
+
+
+def merge(func: Callable[..., T]) -> Callable[[PypelyTuple], T]:
+    return lambda branches: func(*flatten(branches))
+
+
+def identity(*x):
+    if len(x) == 1:
+        return x[0]
+    return x
