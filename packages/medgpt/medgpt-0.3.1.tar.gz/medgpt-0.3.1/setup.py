a34dc419213d28c39b4c8ce8ed7fdcbc8efedbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:19 +0000

    Add package medgpt/0.3.1/medgpt-0.3.1.tar.gz

---
+++ packages/medgpt/medgpt-0.3.1.tar.gz/medgpt-0.3.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="medgpt",
+    version="0.3.1",
+    author="w-is-h",
+    author_email="w.kraljevic@gmail.com",
+    description="Temporal modeling of patients and diseases",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/w-is-h/medgpt",
+    packages=['medgpt', 'medgpt.datasets', 'medgpt.metrics', 'medgpt.utils', 'medgpt.models'],
+    install_requires=[
+        'datasets~=1.14.0',
+        'ray==1.3.0',
+        'wandb~=0.10',
+        'x-transformers~=0.20',
+        'ecco==0.0.14',
+        'transformers~=4.11.3',
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
