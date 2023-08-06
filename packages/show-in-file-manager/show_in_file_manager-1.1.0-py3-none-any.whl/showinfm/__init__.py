db8841137cb903d314e11b572f03c7ba9f073214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:13 +0000

    Add package show-in-file-manager/1.1.0/show_in_file_manager-1.1.0-py3-none-any.whl

---
+++ packages/show-in-file-manager/show_in_file_manager-1.1.0-py3-none-any.whl/showinfm/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021 Damon Lynch
+# SPDX - License - Identifier: MIT
+
+from showinfm.showinfm import (
+    show_in_file_manager,
+    valid_file_manager,
+    user_file_manager,
+    stock_file_manager,
+)
