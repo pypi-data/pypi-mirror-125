fac6d848d980aac66ea94b8cb136b60891e04ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:15 +0000

    Add package kari/1.0.0/kari-1.0.0.tar.gz

---
+++ packages/kari/kari-1.0.0.tar.gz/kari-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(current_dir, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Python logic to find the number'
+LONG_DESCRIPTION = 'Logic implementation in python to find the number picked by user'
+
+setup(
+    name="kari",
+    version=VERSION,
+    author="kari (Karthick Sundaram)",
+    author_email="<karthick.sundaram@hotmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'kari', 'mindreader'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
