4f03ee40e04b3aa5b62f16af127b539127c440bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:29 +0000

    Add package bioregistry/0.3.34/bioregistry-0.3.34-py3-none-any.whl

---
+++ packages/bioregistry/bioregistry-0.3.34-py3-none-any.whl/bioregistry/cli.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""Command line interface for the bioregistry."""
+
+import click
+from more_click import make_web_command
+
+from .align.cli import align
+from .compare import compare
+from .export.cli import export
+from .external.cli import download
+from .generate_warnings_file import warnings
+from .lint import lint
+from .version import VERSION
+
+
+@click.group()
+@click.version_option(version=VERSION)
+def main():
+    """Run the Bioregistry CLI."""
+
+
+main.add_command(lint)
+main.add_command(compare)
+main.add_command(warnings)
+main.add_command(export)
+main.add_command(download)
+main.add_command(align)
+main.add_command(make_web_command("bioregistry.app.wsgi:app"))
+
+
+@main.command()
+def versions():
+    """Print the versions."""
+    from tabulate import tabulate
+
+    from .resolve import get_versions
+
+    click.echo(
+        tabulate(
+            sorted(get_versions().items()),
+            headers=["Prefix", "Version"],
+        )
+    )
+
+
+@main.command()
+@click.pass_context
+def update(ctx: click.Context):
+    """Update the Bioregistry."""
+    ctx.invoke(align)
+    ctx.invoke(lint)
+    ctx.invoke(export)
+    ctx.invoke(compare)
+    ctx.invoke(warnings)
+
+
+if __name__ == "__main__":
+    main()
