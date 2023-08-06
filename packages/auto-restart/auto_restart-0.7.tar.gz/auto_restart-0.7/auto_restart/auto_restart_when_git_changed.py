3aa19fbebbeaeee9a7ed86988379648d1bf39c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package auto-restart/0.7/auto_restart-0.7.tar.gz

---
+++ packages/auto-restart/auto_restart-0.7.tar.gz/auto_restart-0.7/auto_restart/auto_restart_when_git_changed.py
@@ -0,0 +1,42 @@
+import subprocess
+import time
+import os
+import threading
+import argparse
+
+"""
+使代码在git分支发生变化后自动重启  
+"""
+
+parser = argparse.ArgumentParser('helper命令行传入参数 解析器', )
+parser.add_argument('-s', '--start_shell_str', type=str, )
+parser.add_argument('-n', '--script_name', type=str, )
+command_args = parser.parse_args()  # type:argparse.Namespace
+
+start_shell_str = command_args.start_shell_str
+script_name = command_args.script_name
+
+
+def restart(args=None):
+    kill_str = f'''ps -aux|grep {script_name} |grep -v grep|awk '{{print $2}}' |xargs kill -9;'''
+    subprocess.getstatusoutput('git pull')
+    threading.Thread(target=os.system, args=(kill_str + start_shell_str,)).start()
+    while True:
+        time.sleep(20)
+        try:
+            r1 = subprocess.getstatusoutput('git pull')
+            print(r1[1])
+            if 'Already up-to-date' in r1[1] or '已经是最新的' in r1[1]:
+                print('no update')
+                continue
+        except Exception as e:  # 只要不在205的dev分支手动修改文件，一般不会出现拉取报错。
+            subprocess.getstatusoutput(kill_str)
+            print(e)
+            break
+        # subprocess.getstatusoutput(cmd_str)
+        print('git内容更新了，执行语句：', kill_str, start_shell_str)
+        threading.Thread(target=os.system, args=(kill_str + start_shell_str,)).start()
+
+
+if __name__ == '__main__':
+    restart()
---
+++ packages/auto-restart/auto_restart-0.7.tar.gz/auto_restart-0.7/tests/test_git_change.py
@@ -0,0 +1,7 @@
+"""
+
+这个是测试内容变化后。自动重启脚本
+"""
+from tests.pac1.m1 import show
+
+show(61)
