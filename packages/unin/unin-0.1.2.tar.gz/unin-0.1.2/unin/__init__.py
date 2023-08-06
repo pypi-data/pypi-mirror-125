2f8e91ed670ec31e01ddcd4fe159f7150a3e7350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:53 +0000

    Add package unin/0.1.2/unin-0.1.2.tar.gz

---
+++ packages/unin/unin-0.1.2.tar.gz/unin-0.1.2/unin/__init__.py
@@ -0,0 +1,23 @@
+#imports essential functions from the unin module.
+from vandal.misc.global_functions import *
+from vandal import eoq, montecarlo, hub
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+import time
+import datetime
+import math
+
+import os, fnmatch
+def findReplace(directory, find, replace, filePattern):
+    for path, dirs, files in os.walk(os.path.abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = os.path.join(path, filename)
+            with open(filepath) as f:
+                s = f.read()
+            s = s.replace(find, replace)
+            with open(filepath, "w") as f:
+                f.write(s)
+
+findReplace('unin', 'vandal', 'unin', '*.py')
