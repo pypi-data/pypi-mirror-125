12ec35f13ab7cee02247f28a1ab7992e62659dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:06 +0000

    Add package UWGeodynamics/2.11.0/UWGeodynamics-2.11.0-py3-none-any.whl

---
+++ packages/UWGeodynamics/UWGeodynamics-2.11.0-py3-none-any.whl/UWGeodynamics/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM UWGeodynamics SETUP.PY
+#
+short_version = '2.11.0'
+version = '2.11.0'
+full_version = '2.11.0-dev-485ad28(HEAD)'
+git_revision = '485ad28cee2b9373a2c9673587e49a96c2e14150'
+release = False
+
+if not release:
+    version = full_version
