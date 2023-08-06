a8bbe02c6f1b7e67162a4d4992dfec4c1e1a3f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:05 +0000

    Add package TFTrainer/0.0.3/TFTrainer-0.0.3.tar.gz

---
+++ packages/TFTrainer/TFTrainer-0.0.3.tar.gz/TFTrainer-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="TFTrainer",
+    version="0.0.3",
+    description="tensorflow utility trainer.",
+    author="Nam-SW",
+    author_email="nsw0311@gmail.com",
+    url="https://github.com/Nam-SW",
+    license="Apache",
+    python_requires=">=3.6.8",
+    install_requires=[
+        "tensorflow>=2.4.1",
+        "tensorboard",
+        "tqdm",
+        "pytz",
+    ],
+    packages=["src"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
