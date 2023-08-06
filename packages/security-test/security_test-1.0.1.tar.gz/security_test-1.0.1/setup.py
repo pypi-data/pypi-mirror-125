d4623b6dd0c918902a40e25adf081945ebfbf846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:52 +0000

    Add package security-test/1.0.1/security_test-1.0.1.tar.gz

---
+++ packages/security-test/security_test-1.0.1.tar.gz/security_test-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+from setuptools.command.install import install
+setup(
+    name='security_test',
+    version='1.0.1',
+    author='security_test',
+    author_email='security_test@github.com',
+    url='https://github.com/security_test',
+    description='security_test',
+    packages=['security_test'],
+)
