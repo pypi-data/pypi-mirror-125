f9e7829624dae7068306730ca784733e65a0f4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:56 +0000

    Add package audiofile/1.0.1/audiofile-1.0.1.tar.gz

---
+++ packages/audiofile/audiofile-1.0.1.tar.gz/audiofile-1.0.1/audiofile/core/utils.py
@@ -0,0 +1,54 @@
+import os
+import subprocess
+import shlex
+
+import sox
+
+import audeer
+
+
+def file_extension(path):
+    """Lower case file extension."""
+    return audeer.file_extension(path).lower()
+
+
+def run(shell_command):
+    """Return the output of a shell command provided as string."""
+    out = subprocess.check_output(
+        shlex.split(shell_command),
+        stderr=subprocess.STDOUT
+    )
+    try:
+        return out.split()[0]
+    except IndexError:
+        return ''
+
+
+def run_ffmpeg(infile, outfile, offset, duration):
+    """Convert audio file to WAV file."""
+    if duration:
+        cmd = f'ffmpeg -ss {offset} -i "{infile}" -t {duration} "{outfile}"'
+    else:
+        cmd = f'ffmpeg -ss {offset} -i "{infile}" "{outfile}"'
+    run(cmd)
+
+
+def run_sox(infile, outfile, offset, duration):
+    """Convert audio file to WAV file."""
+    tfm = sox.Transformer()
+    if duration:
+        tfm.trim(offset, duration + offset)
+    elif offset > 0:
+        tfm.trim(offset)
+    tfm.build(infile, outfile)
+
+
+MAX_CHANNELS = {
+    'wav': 65535,
+    'ogg': 255,
+    'flac': 8,
+}
+r"""Maximum number of channels per format."""
+
+SNDFORMATS = ['wav', 'flac', 'ogg']
+r"""File formats handled by soundfile"""
