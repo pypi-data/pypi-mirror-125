48f5a9a74a84f2c3646e98ec9a5c1548a298c6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:44 +0000

    Add package uecli/0.0.10/uecli-0.0.10.tar.gz

---
+++ packages/uecli/uecli-0.0.10.tar.gz/uecli-0.0.10/uecli/__main__.py
@@ -0,0 +1,31 @@
+import sys
+import argparse
+import campussquare.cli
+import limedio.cli
+from .authenticator import UECCampusSquareAuthenticator, UECLibraryAuthenticator
+from . import info
+
+campussquare_commands = ['syllabus', 'grades', 'courses']
+library_command = 'library'
+
+
+def main():
+    parser = argparse.ArgumentParser('uecli')
+    parser.add_argument('--version', '-V', action='version', version=f'{info.name} {info.version}')
+    parser.add_argument('command', choices=[*campussquare_commands, library_command])
+    parser.add_argument('args', nargs='*')
+    args, _ = parser.parse_known_args()
+
+    if args.command in campussquare_commands:
+        # Campus Square commands
+        parser = campussquare.cli.get_parser(authenticator=UECCampusSquareAuthenticator())
+        campussquare.cli.parse_args_with_handler(parser)
+    elif args.command == library_command:
+        del sys.argv[1]
+
+        # Library Command
+        parser = limedio.cli.get_parser(authenticator=UECLibraryAuthenticator())
+        limedio.cli.parse_args_with_handler(parser)
+    else:
+        # Other commands
+        raise NotImplementedError()
