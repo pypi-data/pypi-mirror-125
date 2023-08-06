2b53e080c275f984e80661cbeb498ae22020b959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:55 +0000

    Add package deepsig/1.1.3/deepsig-1.1.3.tar.gz

---
+++ packages/deepsig/deepsig-1.1.3.tar.gz/deepsig-1.1.3/deepsig/__init__.py
@@ -0,0 +1,8 @@
+# Make main functions accessible on a package level for cleaner imports
+from deepsig.aso import aso, multi_aso
+from deepsig.bootstrap import bootstrap_test
+from deepsig.correction import bonferroni_correction
+from deepsig.permutation import permutation_test
+
+__version__ = "1.1.3"
+__author__ = "Dennis Ulmer"
