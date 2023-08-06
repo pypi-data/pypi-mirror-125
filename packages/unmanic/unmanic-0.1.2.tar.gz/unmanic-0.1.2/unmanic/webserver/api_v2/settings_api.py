041b75088b294e854fb1580bd738b9096b10079f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:00 +0000

    Add package unmanic/0.1.2/unmanic-0.1.2.tar.gz

---
+++ packages/unmanic/unmanic-0.1.2.tar.gz/unmanic-0.1.2/unmanic/webserver/api_v2/settings_api.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    unmanic.settings_api.py
+
+    Written by:               Josh.5 <jsunnex@gmail.com>
+    Date:                     20 Aug 2021, (2:30 PM)
+
+    Copyright:
+           Copyright (C) Josh Sunnex - All Rights Reserved
+
+           Permission is hereby granted, free of charge, to any person obtaining a copy
+           of this software and associated documentation files (the "Software"), to deal
+           in the Software without restriction, including without limitation the rights
+           to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+           copies of the Software, and to permit persons to whom the Software is
+           furnished to do so, subject to the following conditions:
+
+           The above copyright notice and this permission notice shall be included in all
+           copies or substantial portions of the Software.
+
+           THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+           EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+           MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+           IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+           DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+           OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+           OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+import tornado.log
+
+from unmanic import config
+from unmanic.libs.uiserver import UnmanicDataQueues
+from unmanic.webserver.api_v2.base_api_handler import BaseApiError, BaseApiHandler
+from unmanic.webserver.api_v2.schema.schemas import SettingsReadAndWriteSchema, SettingsSystemConfigSchema
+
+
+class ApiSettingsHandler(BaseApiHandler):
+    config = None
+    params = None
+    unmanic_data_queues = None
+
+    routes = [
+        {
+            "path_pattern":      r"/settings/read",
+            "supported_methods": ["GET"],
+            "call_method":       "get_all_settings",
+        },
+        {
+            "path_pattern":      r"/settings/write",
+            "supported_methods": ["POST"],
+            "call_method":       "write_settings",
+        },
+        {
+            "path_pattern":      r"/settings/configuration",
+            "supported_methods": ["GET"],
+            "call_method":       "get_system_configuration",
+        },
+    ]
+
+    def initialize(self, **kwargs):
+        self.params = kwargs.get("params")
+        udq = UnmanicDataQueues()
+        self.unmanic_data_queues = udq.get_unmanic_data_queues()
+        self.config = config.Config()
+
+    def get_all_settings(self):
+        """
+        Settings - read
+        ---
+        description: Returns the application settings.
+        responses:
+            200:
+                description: 'Sample response: Returns the application settings.'
+                content:
+                    application/json:
+                        schema:
+                            SettingsReadAndWriteSchema
+            400:
+                description: Bad request; Check `messages` for any validation errors
+                content:
+                    application/json:
+                        schema:
+                            BadRequestSchema
+            404:
+                description: Bad request; Requested endpoint not found
+                content:
+                    application/json:
+                        schema:
+                            BadEndpointSchema
+            405:
+                description: Bad request; Requested method is not allowed
+                content:
+                    application/json:
+                        schema:
+                            BadMethodSchema
+            500:
+                description: Internal error; Check `error` for exception
+                content:
+                    application/json:
+                        schema:
+                            InternalErrorSchema
+        """
+        try:
+            settings = self.config.get_config_as_dict()
+            response = self.build_response(
+                SettingsReadAndWriteSchema(),
+                {
+                    "settings": settings,
+                }
+            )
+            self.write_success(response)
+            return
+        except BaseApiError as bae:
+            tornado.log.app_log.error("BaseApiError.{}: {}".format(self.route.get('call_method'), str(bae)))
+            return
+        except Exception as e:
+            self.set_status(self.STATUS_ERROR_INTERNAL, reason=str(e))
+            self.write_error()
+
+    def write_settings(self):
+        """
+        Settings - save a dictionary of settings
+        ---
+        description: Save a given dictionary of settings.
+        requestBody:
+            description: Requested a dictionary of settings to save.
+            required: True
+            content:
+                application/json:
+                    schema:
+                        SettingsReadAndWriteSchema
+        responses:
+            200:
+                description: 'Successful request; Returns success status'
+                content:
+                    application/json:
+                        schema:
+                            BaseSuccessSchema
+            400:
+                description: Bad request; Check `messages` for any validation errors
+                content:
+                    application/json:
+                        schema:
+                            BadRequestSchema
+            404:
+                description: Bad request; Requested endpoint not found
+                content:
+                    application/json:
+                        schema:
+                            BadEndpointSchema
+            405:
+                description: Bad request; Requested method is not allowed
+                content:
+                    application/json:
+                        schema:
+                            BadMethodSchema
+            500:
+                description: Internal error; Check `error` for exception
+                content:
+                    application/json:
+                        schema:
+                            InternalErrorSchema
+        """
+        try:
+            json_request = self.read_json_request(SettingsReadAndWriteSchema())
+
+            # Save settings - writing to file.
+            # Throws exception if settings fail to save
+            self.config.set_bulk_config_items(json_request.get('settings', {}))
+
+            self.write_success()
+            return
+        except BaseApiError as bae:
+            tornado.log.app_log.error("BaseApiError.{}: {}".format(self.route.get('call_method'), str(bae)))
+            return
+        except Exception as e:
+            self.set_status(self.STATUS_ERROR_INTERNAL, reason=str(e))
+            self.write_error()
+
+    def get_system_configuration(self):
+        """
+        Settings - read the system configuration
+        ---
+        description: Returns the system configuration.
+        responses:
+            200:
+                description: 'Sample response: Returns the system configuration.'
+                content:
+                    application/json:
+                        schema:
+                            SettingsSystemConfigSchema
+            400:
+                description: Bad request; Check `messages` for any validation errors
+                content:
+                    application/json:
+                        schema:
+                            BadRequestSchema
+            404:
+                description: Bad request; Requested endpoint not found
+                content:
+                    application/json:
+                        schema:
+                            BadEndpointSchema
+            405:
+                description: Bad request; Requested method is not allowed
+                content:
+                    application/json:
+                        schema:
+                            BadMethodSchema
+            500:
+                description: Internal error; Check `error` for exception
+                content:
+                    application/json:
+                        schema:
+                            InternalErrorSchema
+        """
+        try:
+            from unmanic.libs.system import System
+            system = System()
+            system_info = system.info()
+            response = self.build_response(
+                SettingsSystemConfigSchema(),
+                {
+                    "configuration": system_info,
+                }
+            )
+            self.write_success(response)
+            return
+        except BaseApiError as bae:
+            tornado.log.app_log.error("BaseApiError.{}: {}".format(self.route.get('call_method'), str(bae)))
+            return
+        except Exception as e:
+            self.set_status(self.STATUS_ERROR_INTERNAL, reason=str(e))
+            self.write_error()
