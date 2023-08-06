534c4b5d69f16530e2d7517047dafbb171007bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:49 +0000

    Add package pyparsec/0.7.0/pyparsec-0.7.0.tar.gz

---
+++ packages/pyparsec/pyparsec-0.7.0.tar.gz/pyparsec-0.7.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="pyparsec",
+      version="0.7.0",
+      description="Parsec From Haskell Python 3 Portable",
+      long_description="""
+PyParsec is a Haskell combinator library, PyParsec is a parsec library for python 3+
+""",
+      author="marsliu",
+      author_email="mars.liu@outlook.com",
+      url="https://github.com/Dwarfartisan/pyparsec",
+      license="MIT",
+      packages=["parsec", "test"],
+      package_dir={
+          "parsec": "src/parsec",
+          "test": "src/tests"
+      },
+      classifiers=[
+         "Topic :: Utilities",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3 :: Only",
+         "License :: OSI Approved :: MIT License"
+      ]
+)
