c72a5ba00aff27f9ba5a700bbe2df772beb26226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:45 +0000

    Add package Anonim/0.0.0/Anonim-0.0.0.tar.gz

---
+++ packages/Anonim/Anonim-0.0.0.tar.gz/Anonim-0.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='Anonim',
+	version='',
+	author='Anonim6664',
+	author_email='baliwa5179@ecofreon.com',
+	description='',
+	packages=['AnonimTest'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
