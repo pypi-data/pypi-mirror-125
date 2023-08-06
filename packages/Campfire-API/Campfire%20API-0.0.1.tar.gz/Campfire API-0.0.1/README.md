03bf5ddb9676126d6d6576bb665db04bdb9aa734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:01 +0000

    Add package Campfire-API/0.0.1/Campfire%20API-0.0.1.tar.gz

---
+++ packages/Campfire-API/Campfire%20API-0.0.1.tar.gz/Campfire API-0.0.1/README.md
@@ -0,0 +1,8 @@
+# General
+
+CampfireAPI based on [Capfire web](https://github.com/timas130/campfire-web).
+
+## Install
+
+Coming soon...
+
---
+++ packages/Campfire-API/Campfire%20API-0.0.1.tar.gz/Campfire API-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+# Открытие README.md и присвоение его long_description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = ["requests<=2.26.0"]
+
+setuptools.setup(
+    name="Campfire API",
+    version="0.0.1",
+    author="Ghost-3",
+    # Его почта.
+    author_email="ghost3.github@gmail.com",
+    description="CampfireAPI based on Capfire web.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ghost-3/CapfireAPI",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
