7581bfe108b81837065bea76d9dd8f1e1d15a95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:06 +0000

    Add package catalyst/21.10/catalyst-21.10.tar.gz

---
+++ packages/catalyst/catalyst-21.10.tar.gz/catalyst-21.10/catalyst/contrib/datasets/__init__.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+
+from catalyst.settings import SETTINGS
+
+if SETTINGS.cv_required:  # we need imread function here
+    from catalyst.contrib.datasets.market1501 import (
+        Market1501MLDataset,
+        Market1501QGDataset,
+    )
+
+from catalyst.contrib.datasets.mnist import (
+    MnistMLDataset,
+    MnistQGDataset,
+    MNIST,
+)
+
+from catalyst.contrib.datasets.cifar import CIFAR10, CIFAR100
+
+if SETTINGS.ml_required:
+    from catalyst.contrib.datasets.movielens import MovieLens
+
+if SETTINGS.cv_required:
+    from catalyst.contrib.datasets.cv import *
---
+++ packages/catalyst/catalyst-21.10.tar.gz/catalyst-21.10/catalyst/data/dataset/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+from catalyst.data.dataset.torch import (
+    DatasetFromSampler,
+    ListDataset,
+    MergeDataset,
+    NumpyDataset,
+    PathsDataset,
+)
+from catalyst.data.dataset.metric_learning import (
+    MetricLearningTrainDataset,
+    QueryGalleryDataset,
+)
+from catalyst.data.dataset.self_supervised import SelfSupervisedDatasetWrapper
