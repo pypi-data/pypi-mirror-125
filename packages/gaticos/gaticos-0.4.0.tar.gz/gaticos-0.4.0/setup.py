b590098bfd2271d4ddf189fe72c802a42ec46c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:55 +0000

    Add package gaticos/0.4.0/gaticos-0.4.0.tar.gz

---
+++ packages/gaticos/gaticos-0.4.0.tar.gz/gaticos-0.4.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gaticos']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0', 'requests>=2.26.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['gaticos = gaticos.main:cli']}
+
+setup_kwargs = {
+    'name': 'gaticos',
+    'version': '0.4.0',
+    'description': 'Librería de ejemplo',
+    'long_description': None,
+    'author': 'avara1986',
+    'author_email': 'a.vara.1986@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
