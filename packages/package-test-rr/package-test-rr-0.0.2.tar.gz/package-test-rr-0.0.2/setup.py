c2f2d21897c376177c4e04fa646f439960fd320a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:08 +0000

    Add package package-test-rr/0.0.2/package-test-rr-0.0.2.tar.gz

---
+++ packages/package-test-rr/package-test-rr-0.0.2.tar.gz/package-test-rr-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+   long_description = fh.read()
+
+setup (
+   name='package-test-rr',
+   version='0.0.2',
+   description='say hello',
+   py_modules=["qsamplehelloram"],
+   package_dir={'':'src'},
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+   ],
+   install_requires = [
+        "blessings ~= 1.7",
+   ], 
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/ramagopr/sara",
+   author="Ramkumar Ramagopalan",
+   author_email="kumar.gopalan@gmail.com",
+)
