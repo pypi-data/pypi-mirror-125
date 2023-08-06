fb5a5b845b3cd6fa95feb5d98e915287393da7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:38 +0000

    Add package cumulusci/3.47.0/cumulusci-3.47.0-py2.py3-none-any.whl

---
+++ packages/cumulusci/cumulusci-3.47.0-py2.py3-none-any.whl/cumulusci/cli/cci.py
@@ -0,0 +1,188 @@
+import code
+import contextlib
+import pdb
+import runpy
+import sys
+import traceback
+
+import click
+import requests
+
+import cumulusci
+from cumulusci.core.debug import set_debug_mode
+from cumulusci.core.exceptions import CumulusCIUsageError
+from cumulusci.utils import get_cci_upgrade_command
+from cumulusci.utils.logging import tee_stdout_stderr
+
+from .error import error
+from .flow import flow
+from .logger import get_tempfile_logger, init_logger
+from .org import org
+from .plan import plan
+from .project import project
+from .runtime import CliRuntime, pass_runtime
+from .service import service
+from .task import task
+from .utils import check_latest_version, get_installed_version, get_latest_final_version
+
+SUGGEST_ERROR_COMMAND = (
+    """Run this command for more information about debugging errors: cci error --help"""
+)
+
+USAGE_ERRORS = (CumulusCIUsageError, click.UsageError)
+
+
+#
+# Root command
+#
+def main(args=None):
+    """Main CumulusCI CLI entry point.
+
+    This runs as the first step in processing any CLI command.
+
+    This wraps the `click` library in order to do some initialization and centralized error handling.
+    """
+    with contextlib.ExitStack() as stack:
+        args = args or sys.argv
+        # Check for updates _unless_ we've been asked to output JSON,
+        # or if we're going to check anyway as part of the `version` command.
+        is_version_command = len(args) > 1 and args[1] == "version"
+        if "--json" not in args and not is_version_command:
+            check_latest_version()
+
+        # Only create logfiles for commands that are not `cci error`
+        is_error_command = len(args) > 2 and args[1] == "error"
+        tempfile_path = None
+        if not is_error_command:
+            logger, tempfile_path = get_tempfile_logger()
+            stack.enter_context(tee_stdout_stderr(args, logger, tempfile_path))
+
+        debug = "--debug" in args
+        if debug:
+            args.remove("--debug")
+
+        with set_debug_mode(debug):
+            try:
+                runtime = CliRuntime(load_keychain=False)
+            except Exception as e:
+                handle_exception(e, is_error_command, tempfile_path, debug)
+                sys.exit(1)
+
+            runtime.check_cumulusci_version()
+            should_show_stacktraces = runtime.universal_config.cli__show_stacktraces
+
+            init_logger(log_requests=debug)
+            # Hand CLI processing over to click, but handle exceptions
+            try:
+                cli(args[1:], standalone_mode=False, obj=runtime)
+            except click.Abort:  # Keyboard interrupt
+                show_debug_info() if debug else click.echo("\nAborted!", err=True)
+                sys.exit(1)
+            except Exception as e:
+                if debug:
+                    show_debug_info()
+                else:
+                    handle_exception(
+                        e, is_error_command, tempfile_path, should_show_stacktraces
+                    )
+                sys.exit(1)
+
+
+def handle_exception(error, is_error_cmd, logfile_path, should_show_stacktraces=False):
+    """Displays error of appropriate message back to user, prompts user to investigate further
+    with `cci error` commands, and writes the traceback to the latest logfile.
+    """
+    if isinstance(error, requests.exceptions.ConnectionError):
+        connection_error_message()
+    elif isinstance(error, click.ClickException):
+        click.echo(click.style(f"Error: {error.format_message()}", fg="red"), err=True)
+    else:
+        click.echo(click.style(f"{error}", fg="red"), err=True)
+    # Only suggest gist command if it wasn't run
+    if not is_error_cmd:
+        click.echo(click.style(SUGGEST_ERROR_COMMAND, fg="yellow"), err=True)
+
+    # This is None if we're handling an exception for a `cci error` command.
+    if logfile_path:
+        with open(logfile_path, "a") as log_file:
+            traceback.print_exc(file=log_file)  # log stacktrace silently
+
+    if should_show_stacktraces and not isinstance(error, USAGE_ERRORS):
+        raise error
+
+
+def connection_error_message():
+    message = (
+        "We encountered an error with your internet connection. "
+        "Please check your connection and try the last cci command again."
+    )
+    click.echo(click.style(message, fg="red"), err=True)
+
+
+def show_debug_info():
+    """Displays the traceback and opens pdb"""
+    traceback.print_exc()
+    pdb.post_mortem()
+
+
+@click.group("main", help="")
+def cli():
+    """Top-level `click` command group."""
+
+
+@cli.command(name="version", help="Print the current version of CumulusCI")
+def version():
+    click.echo("CumulusCI version: ", nl=False)
+    click.echo(click.style(cumulusci.__version__, bold=True), nl=False)
+    click.echo(f" ({sys.argv[0]})")
+    click.echo(f"Python version: {sys.version.split()[0]}", nl=False)
+    click.echo(f" ({sys.executable})")
+
+    click.echo()
+    current_version = get_installed_version()
+    latest_version = get_latest_final_version()
+    if latest_version > current_version:
+        click.echo(
+            f"There is a newer version of CumulusCI available ({str(latest_version)})."
+        )
+        click.echo(f"To upgrade, run `{get_cci_upgrade_command()}`")
+        click.echo(
+            f"Release notes: https://github.com/SFDO-Tooling/CumulusCI/releases/tag/v{str(latest_version)}"
+        )
+    else:
+        click.echo("You have the latest version of CumulusCI.")
+
+    click.echo()
+
+
+@cli.command(name="shell", help="Drop into a Python shell")
+@click.option("--script", help="Path to a script to run", type=click.Path())
+@click.option("--python", help="Python code to run directly")
+@pass_runtime(require_project=False, require_keychain=True)
+def shell(runtime, script=None, python=None):
+    # alias for backwards-compatibility
+    variables = {
+        "config": runtime,
+        "runtime": runtime,
+        "project_config": runtime.project_config,
+    }
+
+    if script:
+        if python:
+            raise click.UsageError("Cannot specify both --script and --python")
+        runpy.run_path(script, init_globals=variables)
+    elif python:
+        exec(python, variables)
+    else:
+        code.interact(local=variables)
+
+
+# Top Level Groups
+
+cli.add_command(error)
+cli.add_command(project)
+cli.add_command(org)
+cli.add_command(service)
+cli.add_command(task)
+cli.add_command(flow)
+cli.add_command(plan)
---
+++ packages/cumulusci/cumulusci-3.47.0-py2.py3-none-any.whl/cumulusci/cli/tests/test_cci.py
@@ -0,0 +1,509 @@
+import contextlib
+import io
+import os
+import shutil
+import tempfile
+import unittest
+from pathlib import Path
+from unittest import mock
+
+import click
+import pkg_resources
+import pytest
+from requests.exceptions import ConnectionError
+
+import cumulusci
+from cumulusci.cli import cci
+from cumulusci.cli.tests.utils import run_click_command
+from cumulusci.core.config import BaseProjectConfig
+from cumulusci.core.exceptions import CumulusCIException
+from cumulusci.utils import temporary_dir
+
+MagicMock = mock.MagicMock()
+
+
+class TestCCI(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.global_tempdir = tempfile.gettempdir()
+        cls.tempdir = tempfile.mkdtemp()
+        cls.environ_mock = mock.patch.dict(
+            os.environ, {"HOME": tempfile.mkdtemp(), "CUMULUSCI_KEY": ""}
+        )
+        cls.environ_mock.start()
+        assert cls.global_tempdir in os.environ["HOME"]
+
+    @classmethod
+    def tearDownClass(cls):
+        assert cls.global_tempdir in os.environ["HOME"]
+        cls.environ_mock.stop()
+        shutil.rmtree(cls.tempdir)
+
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.init_logger")
+    @mock.patch("cumulusci.cli.cci.check_latest_version")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    @mock.patch("cumulusci.cli.cci.cli")
+    def test_main(
+        self,
+        cli,
+        CliRuntime,
+        check_latest_version,
+        init_logger,
+        get_tempfile_logger,
+        tee,
+    ):
+        get_tempfile_logger.return_value = mock.Mock(), "tempfile.log"
+        cci.main()
+
+        check_latest_version.assert_called_once()
+        init_logger.assert_called_once()
+        CliRuntime.assert_called_once()
+        cli.assert_called_once()
+        tee.assert_called_once()
+
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.init_logger")
+    @mock.patch("cumulusci.cli.cci.check_latest_version")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    @mock.patch("cumulusci.cli.cci.cli")
+    @mock.patch("pdb.post_mortem")
+    @mock.patch("sys.exit")
+    def test_main__debug(
+        self,
+        sys_exit,
+        post_mortem,
+        cli,
+        CliRuntime,
+        check_latest_version,
+        init_logger,
+        get_tempfile_logger,
+        tee,
+    ):
+        cli.side_effect = Exception
+        get_tempfile_logger.return_value = (mock.Mock(), "tempfile.log")
+
+        cci.main(["cci", "--debug"])
+
+        check_latest_version.assert_called_once()
+        init_logger.assert_called_once_with(log_requests=True)
+        CliRuntime.assert_called_once()
+        cli.assert_called_once()
+        post_mortem.assert_called_once()
+        sys_exit.assert_called_once_with(1)
+        get_tempfile_logger.assert_called_once()
+        tee.assert_called_once()
+
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.init_logger")
+    @mock.patch("cumulusci.cli.cci.check_latest_version")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    @mock.patch("cumulusci.cli.cci.cli")
+    @mock.patch("pdb.post_mortem")
+    def test_main__cci_show_stacktraces(
+        self,
+        post_mortem,
+        cli,
+        CliRuntime,
+        check_latest_version,
+        init_logger,
+        get_tempfile_logger,
+        tee,
+    ):
+        runtime = mock.Mock()
+        runtime.universal_config.cli__show_stacktraces = True
+        CliRuntime.return_value = runtime
+        cli.side_effect = Exception
+        get_tempfile_logger.return_value = (mock.Mock(), "tempfile.log")
+
+        with self.assertRaises(Exception):
+            cci.main(["cci"])
+
+        check_latest_version.assert_called_once()
+        init_logger.assert_called_once_with(log_requests=False)
+        CliRuntime.assert_called_once()
+        cli.assert_called_once()
+        post_mortem.assert_not_called()
+
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.init_logger")
+    @mock.patch("cumulusci.cli.cci.cli")
+    @mock.patch("sys.exit")
+    def test_main__abort(
+        self, sys_exit, cli, init_logger, get_tempfile_logger, tee_stdout_stderr
+    ):
+        get_tempfile_logger.return_value = (mock.Mock(), "tempfile.log")
+        cli.side_effect = click.Abort
+        cci.main(["cci"])
+        cli.assert_called_once()
+        sys_exit.assert_called_once_with(1)
+
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.init_logger")
+    @mock.patch("cumulusci.cli.cci.check_latest_version")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    @mock.patch("cumulusci.cli.cci.cli")
+    @mock.patch("pdb.post_mortem")
+    @mock.patch("sys.exit")
+    def test_main__error(
+        self,
+        sys_exit,
+        post_mortem,
+        cli,
+        CliRuntime,
+        check_latest_version,
+        init_logger,
+        get_tempfile_logger,
+        tee,
+    ):
+        runtime = mock.Mock()
+        runtime.universal_config.cli__show_stacktraces = False
+        CliRuntime.return_value = runtime
+
+        cli.side_effect = Exception
+        get_tempfile_logger.return_value = mock.Mock(), "tempfile.log"
+
+        cci.main(["cci", "org", "info"])
+
+        check_latest_version.assert_called_once()
+        init_logger.assert_called_once_with(log_requests=False)
+        CliRuntime.assert_called_once()
+        cli.assert_called_once()
+        post_mortem.call_count == 0
+        sys_exit.assert_called_once_with(1)
+        get_tempfile_logger.assert_called_once()
+        tee.assert_called_once()
+
+        os.remove("tempfile.log")
+
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    def test_main__CliRuntime_error(self, CliRuntime, get_tempfile_logger, tee):
+        CliRuntime.side_effect = CumulusCIException("something happened")
+        get_tempfile_logger.return_value = mock.Mock(), "tempfile.log"
+
+        with contextlib.redirect_stderr(io.StringIO()) as stderr:
+            with mock.patch("sys.exit") as sys_exit:
+                sys_exit.side_effect = SystemExit  # emulate real sys.exit
+                with pytest.raises(SystemExit):
+                    cci.main(["cci", "org", "info"])
+
+        assert "something happened" in stderr.getvalue()
+
+        tempfile = Path("tempfile.log")
+        tempfile.unlink()
+
+    @mock.patch("cumulusci.cli.cci.init_logger")  # side effects break other tests
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    @mock.patch("sys.exit", MagicMock())
+    def test_handle_org_name(
+        self, CliRuntime, tee_stdout_stderr, get_tempfile_logger, init_logger
+    ):
+
+        # get_tempfile_logger doesn't clean up after itself which breaks other tests
+        get_tempfile_logger.return_value = mock.Mock(), ""
+
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            cci.main(["cci", "org", "default", "xyzzy"])
+        assert "xyzzy is now the default org" in stdout.getvalue(), stdout.getvalue()
+
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            cci.main(["cci", "org", "default", "--org", "xyzzy2"])
+        assert "xyzzy2 is now the default org" in stdout.getvalue(), stdout.getvalue()
+
+        with contextlib.redirect_stderr(io.StringIO()) as stderr:
+            cci.main(["cci", "org", "default", "xyzzy1", "--org", "xyzzy2"])
+        assert "not both" in stderr.getvalue(), stderr.getvalue()
+
+        CliRuntime().keychain.get_default_org.return_value = ("xyzzy3", None)
+
+        # cci org remove should really need an attached org
+        with contextlib.redirect_stderr(io.StringIO()) as stderr:
+            cci.main(["cci", "org", "remove"])
+        assert (
+            "Please specify ORGNAME or --org ORGNAME" in stderr.getvalue()
+        ), stderr.getvalue()
+
+    @mock.patch("cumulusci.cli.cci.init_logger")  # side effects break other tests
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr")
+    @mock.patch("sys.exit")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    def test_cci_org_default__no_orgname(
+        self, CliRuntime, exit, tee_stdout_stderr, get_tempfile_logger, init_logger
+    ):
+        # get_tempfile_logger doesn't clean up after itself which breaks other tests
+        get_tempfile_logger.return_value = mock.Mock(), ""
+
+        CliRuntime().keychain.get_default_org.return_value = ("xyzzy4", None)
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            cci.main(["cci", "org", "default"])
+        assert "xyzzy4 is the default org" in stdout.getvalue(), stdout.getvalue()
+
+        CliRuntime().keychain.get_default_org.return_value = (None, None)
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            cci.main(["cci", "org", "default"])
+        assert "There is no default org" in stdout.getvalue(), stdout.getvalue()
+
+    @mock.patch("cumulusci.cli.cci.init_logger", mock.Mock())
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr", mock.MagicMock())
+    @mock.patch("cumulusci.tasks.salesforce.Deploy.__call__", mock.Mock())
+    @mock.patch("sys.exit", mock.Mock())
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    @mock.patch("cumulusci.tasks.salesforce.Deploy.__init__")
+    def test_cci_run_task_options__with_dash(
+        self,
+        Deploy,
+        CliRuntime,
+        get_tempfile_logger,
+    ):
+        # get_tempfile_logger doesn't clean up after itself which breaks other tests
+        Deploy.return_value = None
+        get_tempfile_logger.return_value = mock.Mock(), ""
+        CliRuntime.return_value = runtime = mock.Mock()
+        runtime.get_org.return_value = ("test", mock.Mock())
+        runtime.project_config = BaseProjectConfig(
+            runtime.universal_config,
+            {
+                "project": {"name": "Test"},
+                "tasks": {
+                    "deploy": {"class_path": "cumulusci.tasks.salesforce.Deploy"}
+                },
+            },
+        )
+
+        cci.main(
+            ["cci", "task", "run", "deploy", "--path", "x", "--clean-meta-xml", "False"]
+        )
+        task_config = Deploy.mock_calls[0][1][1]
+        assert "clean_meta_xml" in task_config.options
+
+    @mock.patch("cumulusci.cli.cci.init_logger", mock.Mock())
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr", mock.MagicMock())
+    @mock.patch("cumulusci.tasks.salesforce.Deploy.__call__", mock.Mock())
+    @mock.patch("sys.exit", mock.Mock())
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    @mock.patch("cumulusci.cli.cci.CliRuntime")
+    @mock.patch("cumulusci.tasks.salesforce.Deploy.__init__")
+    def test_cci_run_task_options__old_style_with_dash(
+        self,
+        Deploy,
+        CliRuntime,
+        get_tempfile_logger,
+    ):
+        # get_tempfile_logger doesn't clean up after itself which breaks other tests
+        Deploy.return_value = None
+        get_tempfile_logger.return_value = mock.Mock(), ""
+        CliRuntime.return_value = runtime = mock.Mock()
+        runtime.get_org.return_value = ("test", mock.Mock())
+        runtime.project_config = BaseProjectConfig(
+            runtime.universal_config,
+            {
+                "project": {"name": "Test"},
+                "tasks": {
+                    "deploy": {"class_path": "cumulusci.tasks.salesforce.Deploy"}
+                },
+            },
+        )
+
+        cci.main(
+            [
+                "cci",
+                "task",
+                "run",
+                "deploy",
+                "--path",
+                "x",
+                "-o",
+                "clean-meta-xml",
+                "False",
+            ]
+        )
+        task_config = Deploy.mock_calls[0][1][1]
+        assert "clean_meta_xml" in task_config.options
+
+    @mock.patch("cumulusci.cli.cci.open")
+    @mock.patch("cumulusci.cli.cci.traceback")
+    @mock.patch("cumulusci.cli.cci.click.style")
+    def test_handle_exception(self, style, traceback, cci_open):
+        logfile_path = "file.log"
+        Path(logfile_path).touch()
+
+        error = "Something bad happened."
+        cci_open.__enter__.return_value = mock.Mock()
+
+        cci.handle_exception(error, False, logfile_path)
+
+        style.call_args_list[0][0] == f"Error: {error}"
+        style.call_args_list[1][0] == cci.SUGGEST_ERROR_COMMAND
+        traceback.print_exc.assert_called_once()
+
+        os.remove(logfile_path)
+
+    @mock.patch("cumulusci.cli.cci.open")
+    @mock.patch("cumulusci.cli.cci.traceback")
+    @mock.patch("cumulusci.cli.cci.click.style")
+    def test_handle_exception__error_cmd(self, style, traceback, cci_open):
+        """Ensure we don't write to logfiles when running `cci error ...` commands."""
+        error = "Something bad happened."
+        logfile_path = None
+        cci.handle_exception(error, False, logfile_path)
+
+        style.call_args_list[0][0] == f"Error: {error}"
+        style.call_args_list[1][0] == cci.SUGGEST_ERROR_COMMAND
+        assert not cci_open.called
+
+    @mock.patch("cumulusci.cli.cci.open")
+    @mock.patch("cumulusci.cli.cci.traceback")
+    @mock.patch("cumulusci.cli.cci.click.style")
+    def test_handle_click_exception(self, style, traceback, cci_open):
+        logfile_path = "file.log"
+        Path(logfile_path).touch()
+        cci_open.__enter__.return_value = mock.Mock()
+
+        cci.handle_exception(click.ClickException("oops"), False, logfile_path)
+        style.call_args_list[0][0] == "Error: oops"
+
+        os.remove(logfile_path)
+
+    @mock.patch("cumulusci.cli.cci.open")
+    @mock.patch("cumulusci.cli.cci.connection_error_message")
+    def test_handle_connection_exception(self, connection_msg, cci_open):
+        logfile_path = "file.log"
+        Path(logfile_path).touch()
+
+        cci.handle_exception(ConnectionError(), False, logfile_path)
+        connection_msg.assert_called_once()
+        os.remove(logfile_path)
+
+    @mock.patch("cumulusci.cli.cci.click.style")
+    def test_connection_exception_message(self, style):
+        cci.connection_error_message()
+        style.assert_called_once_with(
+            (
+                "We encountered an error with your internet connection. "
+                "Please check your connection and try the last cci command again."
+            ),
+            fg="red",
+        )
+
+    def test_cli(self):
+        run_click_command(cci.cli)
+
+    @mock.patch(
+        "cumulusci.cli.cci.get_latest_final_version",
+        mock.Mock(return_value=pkg_resources.parse_version("100")),
+    )
+    @mock.patch("click.echo")
+    def test_version(self, echo):
+        run_click_command(cci.version)
+        assert cumulusci.__version__ in echo.call_args_list[1][0][0]
+
+    @mock.patch(
+        "cumulusci.cli.cci.get_latest_final_version",
+        mock.Mock(return_value=pkg_resources.parse_version("100")),
+    )
+    @mock.patch("click.echo")
+    def test_version__latest(self, echo):
+        with mock.patch(
+            "cumulusci.cli.cci.get_latest_final_version", cci.get_installed_version
+        ):
+            run_click_command(cci.version)
+        assert (
+            "You have the latest version of CumulusCI." in echo.call_args_list[-2][0][0]
+        )
+
+    @mock.patch("code.interact")
+    def test_shell(self, interact):
+        run_click_command(cci.shell)
+        interact.assert_called_once()
+        self.assertIn("config", interact.call_args[1]["local"])
+        self.assertIn("runtime", interact.call_args[1]["local"])
+
+    @mock.patch("runpy.run_path")
+    def test_shell_script(self, runpy):
+        run_click_command(cci.shell, script="foo.py")
+        runpy.assert_called_once()
+        self.assertIn("config", runpy.call_args[1]["init_globals"])
+        self.assertIn("runtime", runpy.call_args[1]["init_globals"])
+        assert runpy.call_args[0][0] == "foo.py", runpy.call_args[0]
+
+    @mock.patch("builtins.print")
+    def test_shell_code(self, print):
+        run_click_command(cci.shell, python="print(config, runtime)")
+        print.assert_called_once()
+
+    @mock.patch("cumulusci.cli.cci.print")
+    def test_shell_mutually_exclusive_args(self, print):
+        with self.assertRaises(Exception) as e:
+            run_click_command(
+                cci.shell, script="foo.py", python="print(config, runtime)"
+            )
+        self.assertIn("Cannot specify both", str(e.exception))
+
+    @mock.patch("code.interact")
+    def test_shell__no_project(self, interact):
+        with temporary_dir():
+            run_click_command(cci.shell)
+            interact.assert_called_once()
+
+    def test_cover_command_groups(self):
+        run_click_command(cci.project)
+        run_click_command(cci.org)
+        run_click_command(cci.task)
+        run_click_command(cci.flow)
+        run_click_command(cci.service)
+        # no assertion; this test is for coverage of empty methods
+
+    @mock.patch(
+        "cumulusci.cli.runtime.CliRuntime.get_org",
+        lambda *args, **kwargs: (MagicMock(), MagicMock()),
+    )
+    @mock.patch("cumulusci.core.runtime.BaseCumulusCI._load_keychain", MagicMock())
+    @mock.patch("pdb.post_mortem", MagicMock())
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr", MagicMock())
+    @mock.patch("cumulusci.cli.cci.init_logger", MagicMock())
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    def test_run_task_debug(self, get_tempfile_logger):
+        get_tempfile_logger.return_value = (mock.Mock(), "tempfile.log")
+
+        gipnew = "cumulusci.tasks.preflight.packages.GetInstalledPackages._run_task"
+        with mock.patch(gipnew, mock_validate_debug(False)):
+            cci.main(["cci", "task", "run", "get_installed_packages"])
+        with mock.patch(gipnew, mock_validate_debug(True)):
+            cci.main(["cci", "task", "run", "get_installed_packages", "--debug"])
+
+    @mock.patch(
+        "cumulusci.cli.runtime.CliRuntime.get_org",
+        lambda *args, **kwargs: (MagicMock(), MagicMock()),
+    )
+    @mock.patch("cumulusci.core.runtime.BaseCumulusCI._load_keychain", MagicMock())
+    @mock.patch("pdb.post_mortem", MagicMock())
+    @mock.patch("cumulusci.cli.cci.tee_stdout_stderr", MagicMock())
+    @mock.patch("cumulusci.cli.cci.init_logger", MagicMock())
+    @mock.patch("cumulusci.tasks.robotframework.RobotLibDoc", MagicMock())
+    @mock.patch("cumulusci.cli.cci.get_tempfile_logger")
+    def test_run_flow_debug(self, get_tempfile_logger):
+        get_tempfile_logger.return_value = (mock.Mock(), "tempfile.log")
+        rtd = "cumulusci.tasks.robotframework.RobotTestDoc._run_task"
+
+        with mock.patch(rtd, mock_validate_debug(False)):
+            cci.main(["cci", "flow", "run", "robot_docs"])
+        with mock.patch(rtd, mock_validate_debug(True)):
+            cci.main(["cci", "flow", "run", "robot_docs", "--debug"])
+
+
+def mock_validate_debug(value):
+    def _run_task(self, *args, **kwargs):
+        assert bool(self.debug_mode) == bool(value)
+
+    return _run_task
