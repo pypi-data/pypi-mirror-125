171d144257011c6f9994da6e12df717bcbc7ed3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:13 +0000

    Add package aibro/0.5.5/aibro-0.5.5.tar.gz

---
+++ packages/aibro/aibro-0.5.5.tar.gz/aibro-0.5.5/aibro/tools/prints.py
@@ -0,0 +1,27 @@
+from colorama import Fore
+from colorama import Style
+
+
+def print_sending(msg: str):
+    print(f"[SENDING]: {msg}")
+
+
+def print_receiving(msg: str):
+    print(f"[RECEIVING]: {msg}")
+
+
+def print_launching(msg: str):
+    print(f"[LAUNCHING]: {msg}")
+
+
+def print_gearing(msg: str):
+    print(f"[GEARING]: {msg}")
+
+
+def print_success(msg: str):
+    print(f"[SUCCESS]: {msg}")
+
+
+def print_err(msg: str):
+    print(Fore.RED + f"[ERROR]: {msg}")
+    print(Style.RESET_ALL)
