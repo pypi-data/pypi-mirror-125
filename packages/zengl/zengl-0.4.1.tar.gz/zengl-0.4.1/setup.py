8672dc99966bd9468d3dc908008bc5da79ecf68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:32 +0000

    Add package zengl/0.4.1/zengl-0.4.1.tar.gz

---
+++ packages/zengl/zengl-0.4.1.tar.gz/zengl-0.4.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import Extension, setup
+
+ext = Extension(
+    name='zengl',
+    sources=['zengl.cpp'],
+    depends=['zengl.hpp'],
+    extra_compile_args=['-fpermissive'],
+)
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='zengl',
+    version='0.4.1',
+    py_modules=['_zengl'],
+    data_files=[('.', ['zengl.pyi'])],
+    ext_modules=[ext],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/szabolcsdombi/zengl/',
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    license='MIT',
+)
