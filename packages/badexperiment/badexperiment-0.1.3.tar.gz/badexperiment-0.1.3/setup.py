3d37f6f01ca964578923c0da4a355de2b19537db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:40 +0000

    Add package badexperiment/0.1.3/badexperiment-0.1.3.tar.gz

---
+++ packages/badexperiment/badexperiment-0.1.3.tar.gz/badexperiment-0.1.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['badexperiment']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0',
+ 'google-api-python-client',
+ 'google-auth-oauthlib',
+ 'pandas']
+
+setup_kwargs = {
+    'name': 'badexperiment',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
