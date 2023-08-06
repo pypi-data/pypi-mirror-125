ea308a583211222207f07854e444eb97e4f40476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:02 +0000

    Add package polygon/0.8.6/polygon-0.8.6.tar.gz

---
+++ packages/polygon/polygon-0.8.6.tar.gz/polygon-0.8.6/polygon/__init__.py
@@ -0,0 +1,10 @@
+# ========================================================= #
+from .stocks import StocksClient
+from .streaming import StreamClient, AsyncStreamClient
+from .forex import ForexClient
+from .crypto import CryptoClient
+from .reference_apis import ReferenceClient
+from .options import (OptionsClient, build_option_symbol, parse_option_symbol, OptionSymbol,
+                      build_option_symbol_for_tda, parse_option_symbol_from_tda)
+from .base_client import (BaseClient, BaseAsyncClient)
+# ========================================================= #
