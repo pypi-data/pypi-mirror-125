289e654335a6406f08a03da6fa59becf57456e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:32 +0000

    Add package aflowey/0.0.11/aflowey-0.0.11-py3-none-any.whl

---
+++ packages/aflowey/aflowey-0.0.11-py3-none-any.whl/aflowey/f.py
@@ -0,0 +1,38 @@
+import functools
+from typing import Any
+from typing import Callable
+
+from aflowey.types import Function
+
+
+class F:
+    """tiny wrapper around a function
+
+    Args:
+        func: callable
+
+    """
+
+    def __init__(self, func: Function) -> None:
+        self.func = func
+        functools.update_wrapper(self, func)
+
+    def __rshift__(self, other: Callable[[Function], "F"]) -> "F":
+        """equivalent of decoration"""
+        return F(other(self.func))
+
+    def __gt__(self, other: Function) -> "F":
+        """function composition"""
+
+        @functools.wraps(other)
+        def wrapped(*args: Any, **kwargs: Any) -> Any:
+            return other(self.func(*args, **kwargs))
+
+        return F(wrapped)
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        """ simply call the inner function"""
+        return self.func(*args, **kwargs)
+
+    def __repr__(self) -> str:
+        return f"<F instance: {repr(self.func)}>"
