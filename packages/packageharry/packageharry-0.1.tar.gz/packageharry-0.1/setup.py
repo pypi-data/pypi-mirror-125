cf8260957d4c7718613ddd854f0e05be5155627f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:02 +0000

    Add package packageharry/0.1/packageharry-0.1.tar.gz

---
+++ packages/packageharry/packageharry-0.1.tar.gz/packageharry-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="packageharry",
+version="0.1",
+description="This is DHANUSH Smart Calculator",
+long_description="This is smartest calculator work on ur sentence",
+author="DHANUSH",
+package=['PACKAGEDHANUSH'],
+install_requires=[])
