a1b7e9ecd117140686bdaa494a7135b94e47c9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:46 +0000

    Add package gym-twolinkarm-env/0.0.2/gym-twolinkarm-env-0.0.2.tar.gz

---
+++ packages/gym-twolinkarm-env/gym-twolinkarm-env-0.0.2.tar.gz/gym-twolinkarm-env-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gym-twolinkarm-env",
+    version="0.0.2",
+    author="gyuta",
+    description="This is the open-ai-gym environment for controlling two link arm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gyuta/gym-twolinkarm-env",
+    project_urls={
+        "Bug Tracker": "https://github.com/gyuta/gym-twolinkarm-env/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "gym>=0.21.0",
+        "scipy>=1.7.1",
+    ]
+)
