44e8566dceb3e03a335be0013b91ffabb6dbe9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:05 +0000

    Add package bpoptimizer/1.4.1/bpoptimizer-1.4.1-py2.py3-none-any.whl

---
+++ packages/bpoptimizer/bpoptimizer-1.4.1-py2.py3-none-any.whl/bpoptimizer/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for BlockParty Floor Optimizer."""
+
+__author__ = """RadioactiveDroid"""
+__email__ = "radioactivedroid@gmail.com"
+__version__ = "1.4.1"
+
+
+from .floor import Floor
