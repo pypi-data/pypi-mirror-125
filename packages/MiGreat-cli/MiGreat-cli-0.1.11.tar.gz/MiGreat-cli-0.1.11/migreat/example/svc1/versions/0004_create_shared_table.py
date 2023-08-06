2cc356c38181c33483864ee748da53f4bf518977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:35 +0000

    Add package MiGreat-cli/0.1.11/MiGreat-cli-0.1.11.tar.gz

---
+++ packages/MiGreat-cli/MiGreat-cli-0.1.11.tar.gz/MiGreat-cli-0.1.11/migreat/example/svc1/versions/0004_create_shared_table.py
@@ -0,0 +1,20 @@
+import time
+
+def upgrade(conn):
+    # Users in group (svc_group), which includes all users, will all get universal access
+    # to "svc1.shared", but nothing else within the "svc1" schema space.
+    conn.execute("""
+        CREATE TABLE shared (
+            id BIGSERIAL PRIMARY KEY,
+            name TEXT NOT NULL
+        );
+
+        GRANT ALL PRIVILEGES ON TABLE shared TO GROUP svc_group;
+        GRANT ALL PRIVILEGES ON shared_id_seq TO GROUP svc_group;
+    """)
+
+    time.sleep(5)
+
+def downgrade(conn):
+    """
+    """
