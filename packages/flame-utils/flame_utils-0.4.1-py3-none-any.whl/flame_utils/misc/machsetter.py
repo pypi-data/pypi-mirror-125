609a5e371aa31e29cfb31bf31bd6ccf4f22aec36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:16 +0000

    Add package flame-utils/0.4.1/flame_utils-0.4.1-py3-none-any.whl

---
+++ packages/flame-utils/flame_utils-0.4.1-py3-none-any.whl/flame_utils/misc/machsetter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from flame import Machine
+
+import numpy as np
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def machine_setter(_latfile=None, _machine=None, _handle_name=None):
+    """ set flame machine, prefer *_latfile*
+
+    :return: FLAME machine object
+    """
+    if _latfile is not None:
+        try:
+            with open(_latfile, 'rb') as f:
+                m = Machine(f)
+        except:
+            if _machine is None:
+                _LOGGER.error("{}: Failed to initialize flame machine".format(
+                    _handle_name))
+                return None
+            else:
+                _LOGGER.warning("{}: Failed to initialize flame machine, "
+                                "use _machine instead".format(_handle_name))
+                m = _machine
+    else:
+        if _machine is None:
+            return None
+        else:
+            m = _machine
+    return m
+
+
+def is_zeros_states(s):
+    """ test if flame machine states is all zeros
+
+    Returns
+    -------
+    True or False
+        True if is all zeros, else False
+    """
+    return np.alltrue(getattr(s, 'moment0') == np.zeros([7, 1]))
