9a5e7d7232d10bcaa05cc307f158a734aa48daf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:14 +0000

    Add package colorain/0.0.9/colorain-0.0.9.tar.gz

---
+++ packages/colorain/colorain-0.0.9.tar.gz/colorain-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.9' 
+DESCRIPTION = 'A simple colouring package for making console programmes great again.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="colorain", 
+        version=VERSION,
+        author="Susmit Islam",
+        author_email="<susmitislam31@gmail.com>",
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['colour','color','terminal'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
