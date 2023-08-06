a3e144d98c455bc9e3f017d6e936af8bc216767c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:13 +0000

    Add package Flask-Authlib/1.5.0/Flask_Authlib-1.5.0-py3-none-any.whl

---
+++ packages/Flask-Authlib/Flask_Authlib-1.5.0-py3-none-any.whl/flask_authlib/settings.py
@@ -0,0 +1,74 @@
+class BaseConfig:
+    HOME_URL: str = "/"
+    LOGIN_URL: str = "/login"
+    REGISTER_URL: str = "/register"
+    LOGOUT_URL: str = "/logout"
+
+    logout: dict = {
+        "path": LOGOUT_URL,
+        "name": "logout_view"
+    }
+    login: dict = {
+        "path": LOGIN_URL,
+        "name": "login_view"
+    }
+    register: dict = {
+        "path": REGISTER_URL,
+        "name": "register_view"
+    }
+
+    BLUEPRINT_NAME: str = "auth"
+
+    TEMPLATES_FILE_NAME: str = "templates.zip"
+
+    TEMPLATES_FOLDER_NAME: str = "templates"
+    STATIC_FOLDER_NAME: str = "static"
+
+    LOGIN_MESSAGE_CATEGORY = "info"
+
+    TABLENAME: str = "user"
+    MIN_PASSWORD_LENGTH: int = 8
+
+    EMAIL_UNIQUE: bool = True
+
+
+class Alerts:
+    EMAIL_ALERT: str = "This email is already taken!"
+    USERNAME_ALERT: str = "This username is already taken!"
+
+    PASSWORD_LENGTH: str = "Password must be {length} characters long!"
+
+    REGISTER_SUCCESS: str = "Registration was successful!"
+
+    LOGIN_FAIL: str = "The username or password is incorrect!"
+
+    REGISTER_FAIL: str = "This email and username are already taken!"
+
+    BAD_REQUEST: str = "Bad request!"
+    REQUIRED_FIELD: str = "Please, fill in all required fields!"
+
+
+class TemplateConfig:
+    USERNAME_LABEL: str = "Username"
+    PASSWORD_LABEL: str = "Password"
+    EMAIL_LABEL: str = "Email address"
+
+    LOGIN_TITLE: str = "Login"
+    LOGIN_BTN_TEXT: str = "Login"
+    LOGIN_PRIMARY_COLOR: str = "red"
+
+    REGISTER_TITLE: str = "Register"
+    REGISTER_BTN_TEXT: str = "Register"
+    REGISTER_PRIMARY_COLOR: str = "yellow"
+
+
+COLORS: dict = {
+    "blue": "primary",
+    "violet": "secondary",
+    "green": "success",
+    "red": "danger",
+    "yellow": "warning",
+    "light-blue": "info",
+    "white": "light",
+    "black": "dark"
+}
---
+++ packages/Flask-Authlib/Flask_Authlib-1.5.0-py3-none-any.whl/flask_authlib/utils.py
@@ -0,0 +1,67 @@
+import secrets
+
+from typing import Callable
+
+from flask import Flask
+
+from flask import request
+from flask import redirect
+from flask.views import View
+
+from flask_login import current_user
+
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+from pydantic import ValidationError
+from pydantic.main import ModelMetaclass
+
+from .settings import BaseConfig
+
+
+bcrypt = Bcrypt()
+
+
+def validate_request_body(schema: ModelMetaclass) -> bool:
+    try:
+        return bool(
+            schema.parse_obj(request.form.to_dict())
+        )
+    except ValidationError:
+        return False
+
+
+def initalize_base_view(cls, base_view: View) -> None:
+    base_view.db = cls.db
+    base_view.base_config = cls.base_config
+    base_view.alerts = cls.alerts
+    base_view.template_config = cls.template_config
+
+    base_view.User = cls.User
+    base_view.HOME_URL = base_view.base_config.HOME_URL
+    base_view.LOGIN_URL = base_view.base_config.LOGIN_URL
+    base_view.REGISTER_URL = base_view.base_config.REGISTER_URL
+
+
+def is_authenticated(function) -> Callable:
+    def wrapper(*args, **kwargs):
+        if current_user.is_authenticated:
+            return redirect("/")
+        return function(*args, **kwargs)
+    return wrapper
+
+
+def check_table_name(db: SQLAlchemy, table_name: str) -> bool:
+    return table_name in db.engine.table_names()
+
+
+def set_flask_app_config(app: Flask, config: BaseConfig) -> None:
+    config: dict = dict(
+        SECRET_KEY=secrets.token_hex(),
+        STATIC_FOLDER=config.STATIC_FOLDER_NAME,
+        SQLALCHEMY_ECHO=False,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    for key, value in config.items():
+        app.config[key] = value
