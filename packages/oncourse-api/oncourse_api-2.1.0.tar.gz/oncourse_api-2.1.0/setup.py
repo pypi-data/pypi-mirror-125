fc2396544d4385e7fe2ffb05d393a4d0e106d1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:41 +0000

    Add package oncourse-api/2.1.0/oncourse_api-2.1.0.tar.gz

---
+++ packages/oncourse-api/oncourse_api-2.1.0.tar.gz/oncourse_api-2.1.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="oncourse_api",
+    version="2.1.0",
+    description="A python api wrapper for OnCourse Connect",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Wolfhound905/OnCourse-API",
+    author="Wolfhound905",
+    author_email="aiden8green@gmail.com",
+    license="MIT",
+    packages=find_packages(exclude=["test", "tests", "testing"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests"],
+)
