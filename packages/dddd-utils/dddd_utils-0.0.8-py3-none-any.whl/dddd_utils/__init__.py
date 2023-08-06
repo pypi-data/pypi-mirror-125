e5a99a4e81ad31a4cafc7f21986b98158683863f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:11 +0000

    Add package dddd-utils/0.0.8/dddd_utils-0.0.8-py3-none-any.whl

---
+++ packages/dddd-utils/dddd_utils-0.0.8-py3-none-any.whl/dddd_utils/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from dddd_utils.basespider import BaseSpider
+from dddd_utils.basespider_selenium import SeleniumBaseSpider
+from dddd_utils.retry import retry
+from dddd_utils.ftp import FtpDownload
+from dddd_utils.mysql import StoreMysqlPool
+from dddd_utils.ffdm import get_ffdm_api
+from dddd_utils.logger import get_logger
+from dddd_utils.mail import UtilMail
+
+__all__ = [
+    'BaseSpider',  # 爬虫基类
+    'SeleniumBaseSpider',  # 浏览器爬虫基类
+    'retry',  # 重试装饰器
+    'FtpDownload',  # ftp工具类
+    'StoreMysqlPool',  # mysql
+    'get_ffdm_api',  # 验证码
+    'get_logger',  # logger
+    'UtilMail'  # 邮件工具类
+]
