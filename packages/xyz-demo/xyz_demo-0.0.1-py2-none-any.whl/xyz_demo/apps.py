71d25714bcf4a1b047cc837a861ad4671c20816c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:27 +0000

    Add package xyz-demo/0.0.1/xyz_demo-0.0.1-py2-none-any.whl

---
+++ packages/xyz-demo/xyz_demo-0.0.1-py2-none-any.whl/xyz_demo/apps.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:denishuang
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Config(AppConfig):
+    name = 'xyz_demo'
+    label = 'demo'
+    verbose_name = '原型'
