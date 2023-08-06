ca59e750bf8a90501b95aaeb626f105e68ab79a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:41 +0000

    Add package felupe/1.2.0/felupe-1.2.0-py3-none-any.whl

---
+++ packages/felupe/felupe-1.2.0-py3-none-any.whl/felupe/dof/__init__.py
@@ -0,0 +1,22 @@
+from ._boundary import Boundary
+from ._tools import (
+    get_dof0,
+    get_dof1,
+    partition,
+    extend,
+    apply,
+)
+from ._loadcase import (
+    symmetry,
+    uniaxial,
+    biaxial,
+    planar,
+)
+
+try:
+    from ._multipoint import (
+        MultiPointConstraint,
+        MultiPointContact,
+    )
+except:
+    pass
