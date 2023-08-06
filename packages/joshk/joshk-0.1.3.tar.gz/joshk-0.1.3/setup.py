3ab248094000d96326be7eeffa4e8072aeff192e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:53 +0000

    Add package joshk/0.1.3/joshk-0.1.3.tar.gz

---
+++ packages/joshk/joshk-0.1.3.tar.gz/joshk-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['joshk']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0', 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'joshk',
+    'version': '0.1.3',
+    'description': "A package of Josh K's things",
+    'long_description': None,
+    'author': 'Josh K',
+    'author_email': '59419126+this-josh@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
