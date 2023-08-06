071127ce4d89f1201bf9ab641bc3c720a365b223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:42 +0000

    Add package gammy/0.4.5/gammy-0.4.5.tar.gz

---
+++ packages/gammy/gammy-0.4.5.tar.gz/gammy-0.4.5/gammy/__init__.py
@@ -0,0 +1,18 @@
+"""Importable modules in the package
+
+.. autosummary::
+   :toctree: _autosummary
+
+   arraymapper
+   formulae
+   models
+   plot
+   utils
+
+"""
+
+from .arraymapper import ArrayMapper, x
+from .formulae import *
+from .models import *
+from .plot import *
+from .utils import *
