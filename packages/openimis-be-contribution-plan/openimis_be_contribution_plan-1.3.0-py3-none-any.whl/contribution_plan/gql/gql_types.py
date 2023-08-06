6c8bbf14111ecc9bdee152c48aae8ff4b9d11714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:58 +0000

    Add package openimis-be-contribution-plan/1.3.0/openimis_be_contribution_plan-1.3.0-py3-none-any.whl

---
+++ packages/openimis-be-contribution-plan/openimis_be_contribution_plan-1.3.0-py3-none-any.whl/contribution_plan/gql/gql_types.py
@@ -0,0 +1,84 @@
+import graphene
+from contribution_plan.models import ContributionPlanBundle, ContributionPlan, ContributionPlanBundleDetails
+from core import ExtendedConnection, prefix_filterset
+from graphene_django import DjangoObjectType
+from product.schema import ProductGQLType
+
+
+class ContributionPlanGQLType(DjangoObjectType):
+
+    class Meta:
+        model = ContributionPlan
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "id": ["exact"],
+            "version": ["exact"],
+            "code": ["exact", "istartswith", "icontains", "iexact"],
+            "name": ["exact", "istartswith", "icontains", "iexact"],
+            **prefix_filterset("benefit_plan__", ProductGQLType._meta.filter_fields),
+            "calculation": ["exact"],
+            "periodicity": ["exact", "lt", "lte", "gt", "gte"],
+            "date_created": ["exact", "lt", "lte", "gt", "gte"],
+            "date_updated": ["exact", "lt", "lte", "gt", "gte"],
+            "user_created": ["exact"],
+            "user_updated": ["exact"],
+            "is_deleted": ["exact"]
+        }
+
+        connection_class = ExtendedConnection
+
+    @classmethod
+    def get_queryset(cls, queryset, info):
+        return ContributionPlan.get_queryset(queryset, info)
+
+
+class ContributionPlanBundleGQLType(DjangoObjectType):
+
+    class Meta:
+        model = ContributionPlanBundle
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "id": ["exact"],
+            "version": ["exact"],
+            "code": ["exact", "istartswith", "icontains", "iexact"],
+            "name": ["exact", "istartswith", "icontains", "iexact"],
+            "periodicity": ["exact", "lt", "lte", "gt", "gte"],
+            "date_created": ["exact", "lt", "lte", "gt", "gte"],
+            "date_updated": ["exact", "lt", "lte", "gt", "gte"],
+            "user_created": ["exact"],
+            "user_updated": ["exact"],
+            "is_deleted": ["exact"]
+        }
+
+        connection_class = ExtendedConnection
+
+    @classmethod
+    def get_queryset(cls, queryset, info):
+        return ContributionPlanBundle.get_queryset(queryset, info)
+
+
+class ContributionPlanBundleDetailsGQLType(DjangoObjectType):
+
+    class Meta:
+        model = ContributionPlanBundleDetails
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            "id": ["exact"],
+            "version": ["exact"],
+            **prefix_filterset("contribution_plan_bundle__",
+                               ContributionPlanBundleGQLType._meta.filter_fields),
+            **prefix_filterset("contribution_plan__",
+                               ContributionPlanGQLType._meta.filter_fields),
+            "date_created": ["exact", "lt", "lte", "gt", "gte"],
+            "date_updated": ["exact", "lt", "lte", "gt", "gte"],
+            "user_created": ["exact"],
+            "user_updated": ["exact"],
+            "is_deleted": ["exact"]
+        }
+
+        connection_class = ExtendedConnection
+
+    @classmethod
+    def get_queryset(cls, queryset, info):
+        return ContributionPlanBundleDetails.get_queryset(queryset, info)
+
