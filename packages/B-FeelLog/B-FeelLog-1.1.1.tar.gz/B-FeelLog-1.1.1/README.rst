f4a0e2d483d267ad0fd13c50a8d50ccd253717a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:41 +0000

    Add package B-FeelLog/1.1.1/B-FeelLog-1.1.1.tar.gz

---
+++ packages/B-FeelLog/B-FeelLog-1.1.1.tar.gz/B-FeelLog-1.1.1/README.rst
@@ -0,0 +1,72 @@
+B-FeelLog
+=========
+
+.. figure:: https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/B-feelog_logo1.jpg?raw=true
+   :alt: logo
+
+   logo
+::
+
+        Blog feel-log ,feel Blog   
+
+free source minimal multilingual blog maker and manager for different
+blog entries and multiple blog entries , with web interface. in this
+blog can use images but we looking to keep it minimalism.
+
+now is bilingual with support in - Spanish - English - German - Basque -
+Italian - Russian
+
+**more information**
+`here <https://github.com/jero98772/B-FeelLog/blob/main/docs/FAQs.md>`__
+
+**video tutorial** `here <https://vimeo.com/manage/videos/580068235>`__
+
+please report some issues or if you need help ### Made for: manage a
+blog ,sharing and proposing a tool for expressing
+
+Download
+~~~~~~~~
+
+Download repo
+
+::
+
+    git clone https://github.com/jero98772/B-FeelLog.git
+
+Update
+~~~~~~
+
+run in repo folder
+
+::
+
+    git pull
+
+Install
+~~~~~~~
+
+run install :
+
+::
+
+    python setup.py install
+
+Usage
+~~~~~
+
+the app run in `localhost:9600 <http://localhost:9600/this.html>`__
+remember you need authenticate in `localhost:9600/blog/< TOKEN
+>/ <localhost:9600/this/defaulttoken/>`__ the initial token is:
+
+::
+
+    defaulttoken
+
+Screenshots
+~~~~~~~~~~~
+
+|main| |flex blog| |translation|
+
+.. |main| image:: https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/2021-05-06-185257_770x321_scrot.png
+.. |flex blog| image:: https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/2021-05-06-165512_752x551_scrot.png
+.. |translation| image:: https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/2021-05-07-203242_822x595_scrot.png
---
+++ packages/B-FeelLog/B-FeelLog-1.1.1.tar.gz/B-FeelLog-1.1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-"
+"""
+B→FeelLog - 2021 - por jero98772
+B→FeelLog - 2021 - by jero98772
+"""
+from core.tools.webutils import genTokenFile
+from setuptools import setup, find_packages
+setup(
+	name='B-FeelLog',
+	version='1.1.1',
+	license='GPLv3',
+	author_email='jero98772@protonmail.com',
+	author='jero98772',
+	description='free source minimal multilingual blog maker and manager for different blog entries and multiple blog entries , with web interface. in this blog can use images but we looking to keep it minimalism.',
+	url='https://jero98772.pythonanywhere.com/',
+	packages=find_packages(),
+    install_requires=['Flask','deep-translator'],
+    include_package_data=True,
+    long_description="""# B-FeelLog\n\n![logo](https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/B-feelog_logo1.jpg?raw=true)\n\n\t\tBlog feel-log ,feel Blog   \n\nfree source minimal multilingual blog maker and manager for different blog entries and multiple blog entries , with web interface. in this blog can use images but we looking to keep it minimalism.\n\nnow is bilingual with support in \n- Spanish \n- English\n- German\n- Basque\n- Italian\n- Russian\n\n**more information** [here](https://github.com/jero98772/B-FeelLog/blob/main/docs/FAQs.md)\n\n**video tutorial** [here](https://vimeo.com/manage/videos/580068235)\n\nplease report some issues or if you need help \n### Made for:\nmanage a blog ,sharing and proposing a tool for expressing\n\n### Download \nDownload repo\n\n\tgit clone https://github.com/jero98772/B-FeelLog.git\n### Update\nrun in repo folder\n\n\tgit pull\n\n### Install\n\nrun install : \n\n\tpython setup.py install\n\n### Usage \n\nthe app run in [localhost:9600](http://localhost:9600/this.html)\nremember you need authenticate in [localhost:9600/blog/< TOKEN >/](localhost:9600/this/defaulttoken/)\nthe initial token is:\n\t\n\tdefaulttoken\n\n### Screenshots\n![main](https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/2021-05-06-185257_770x321_scrot.png)\n![flex blog](https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/2021-05-06-165512_752x551_scrot.png)\n![translation](https://github.com/jero98772/B-FeelLog/blob/main/docs/Screenshots/2021-05-07-203242_822x595_scrot.png)\n'""",
+    long_description_content_type='text/markdown',
+	)
+genTokenFile("data/token.txt")
