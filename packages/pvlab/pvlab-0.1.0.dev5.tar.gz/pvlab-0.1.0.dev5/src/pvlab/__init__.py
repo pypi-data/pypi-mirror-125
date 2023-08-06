b05f566167b18b30ba1c3a5c099cb5528290a4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:16 +0000

    Add package pvlab/0.1.0.dev5/pvlab-0.1.0.dev5.tar.gz

---
+++ packages/pvlab/pvlab-0.1.0.dev5.tar.gz/pvlab-0.1.0.dev5/src/pvlab/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# License: bsd-3-clause
+# Copyright (C) 2021 J. P. Silva. All rights reserved.
+
+"""
+
+The package ``pvlab``provides tools for solar sensors and pv devices.
+
+PVLAB is a project devoted to the development and improvement of scientific
+software for the measurement, calibration and modeling of the performance of
+photoltaic devices and solar sensors. PVLAB package was born from the efforts
+made to perform the data treatment for the calibration of pyranometers at
+the Laboratory of Photovoltaic Solar Energy (PVLAb), in the Research Center
+for Energy, Environment and Technology (CIEMAT) in Madrid, Spain. The aim of
+PVLAB is to provide a set of tools, mainly consisting in classes and
+functions, to perform the data treatment for the calibration of pyranometers.
+"""
+__version__ = '0.1.0.dev5'
