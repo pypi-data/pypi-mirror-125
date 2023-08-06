db7c3abac049d0ee1f34dc0e321a0d5da5efbcef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:32 +0000

    Add package metaphor-connectors/0.7.4/metaphor-connectors-0.7.4.tar.gz

---
+++ packages/metaphor-connectors/metaphor-connectors-0.7.4.tar.gz/metaphor-connectors-0.7.4/metaphor/common/extractor.py
@@ -0,0 +1,77 @@
+import asyncio
+import logging
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import List, Optional, Type
+
+from metaphor.models.metadata_change_event import MetadataChangeEvent
+from serde import deserialize
+from serde.json import from_json
+from serde.yaml import from_yaml
+from smart_open import open
+
+from .api_sink import ApiSink, ApiSinkConfig
+from .file_sink import FileSink, FileSinkConfig
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+@deserialize
+@dataclass
+class OutputConfig:
+    """Config for where to output the data"""
+
+    api: Optional[ApiSinkConfig] = None
+    file: Optional[FileSinkConfig] = None
+
+
+@deserialize
+@dataclass
+class RunConfig:
+    """Base class for runtime parameters
+
+    All subclasses should add the @dataclass @deserialize decorators
+    """
+
+    output: OutputConfig
+
+    @classmethod
+    def from_json_file(cls, path: str) -> "RunConfig":
+        with open(path, encoding="utf8") as fin:
+            return from_json(cls, fin.read())
+
+    @classmethod
+    def from_yaml_file(cls, path: str) -> "RunConfig":
+        with open(path, encoding="utf8") as fin:
+            return from_yaml(cls, fin.read())
+
+
+class BaseExtractor(ABC):
+    """Base class for metadata extractors"""
+
+    @staticmethod
+    @abstractmethod
+    def config_class() -> Type[RunConfig]:
+        """Returns the corresponding config class"""
+
+    def run(self, config: RunConfig) -> List[MetadataChangeEvent]:
+        """Callable function to extract metadata and send/post messages"""
+        logger.info(f"Starting extractor {self.__class__.__name__}")
+
+        events: List[MetadataChangeEvent] = asyncio.run(self.extract(config))
+
+        logger.info(f"Fetched {len(events)} entities")
+
+        if config.output.api is not None:
+            ApiSink(config.output.api).sink(events)
+
+        if config.output.file is not None:
+            FileSink(config.output.file).sink(events)
+
+        logger.info("Extractor ended successfully")
+        return events
+
+    @abstractmethod
+    async def extract(self, config: RunConfig) -> List[MetadataChangeEvent]:
+        """Extract metadata and build messages, should be overridden"""
