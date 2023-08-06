e2b40f20f70d0cd3addfe7e3908c719e49b9b0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:46 +0000

    Add package algop/0.0.4/algop-0.0.4.tar.gz

---
+++ packages/algop/algop-0.0.4.tar.gz/algop-0.0.4/src/algop/__init__.py
@@ -0,0 +1,21 @@
+#Isa Haji
+
+def bubbleSort(array,say=False):
+    try:
+    # We go through the list as many times as there are elements
+        for i in range(len(array)):
+            # We want the last pair of adjacent elements to be (n-2, n-1)
+            for j in range(len(array) - 1):
+                if array[j] > array[j+1]:
+                    # Swap
+                    array[j], array[j+1] = array[j+1], array[j]
+    except:
+        print("Not a Valid Data Type")
+
+  
+    if say:
+        print(array)        
+        return array
+    else:
+        return array
+
