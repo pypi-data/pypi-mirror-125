c7b4d1fbfdb2498f24a5248676792d9cc64541d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:13 +0000

    Add package apu/0.1.18/apu-0.1.18.tar.gz

---
+++ packages/apu/apu-0.1.18.tar.gz/apu-0.1.18/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""apu: Antons Python Utilities."""
+
+import sys
+import platform
+
+# Third party
+from setuptools import setup
+
+requires_designpattern = ["dill"]
+requires_datetime = ["pytz", "pint", "tzlocal"]
+requires = ["GitPython"]
+requires_geographie = ["numpy"]
+requires_ml = ["torch"]
+requires_io = ["h5py", "mat4py", "pyyaml", "dill", "msgpack"]
+if not platform.system().lower() == "windows":
+    requires_io.append("python_magic")
+requires_all = (requires_datetime + requires + requires_geographie +
+                requires_designpattern + requires_io + requires_ml)
+
+import pathlib
+
+sys.path.append(pathlib.Path(__file__).parent.absolute())
+
+from apu import __version__
+
+setup(
+    version=".".join([str(v) for v in __version__]),
+    package_data={"apu": []},
+    project_urls={
+        'Documentation': 'https://afeldman.github.io/apu/',
+        'Source': 'https://github.com/afeldman/apu',
+        'Tracker': 'https://github.com/afeldman/apu/issues',
+    },
+    install_requires=[
+        "dill",
+        "pytz",
+        "pint",
+        "tzlocal",
+        "GitPython",
+        "numpy",
+        "mat4py",
+    ],
+    extra_requires={
+        "all": requires_all,
+        "datetime": requires_datetime,
+        "setup": requires,
+        "geo": requires_geographie,
+        "designpattern": requires_designpattern
+    },
+)
