b2f7dcded6812f61eebd42d7ca261e9fd906a3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:50 +0000

    Add package mlserver/0.5.3/mlserver-0.5.3.tar.gz

---
+++ packages/mlserver/mlserver-0.5.3.tar.gz/mlserver-0.5.3/mlserver/types/__init__.py
@@ -0,0 +1,52 @@
+from .dataplane import (
+    MetadataServerResponse,
+    MetadataServerErrorResponse,
+    MetadataTensor,
+    MetadataModelErrorResponse,
+    Parameters,
+    Tags,
+    TensorData,
+    RequestOutput,
+    ResponseOutput,
+    InferenceResponse,
+    InferenceErrorResponse,
+    MetadataModelResponse,
+    RequestInput,
+    InferenceRequest,
+)
+
+from .model_repository import (
+    RepositoryIndexRequest,
+    RepositoryIndexResponseItem,
+    State,
+    RepositoryIndexResponse,
+    RepositoryIndexErrorResponse,
+    RepositoryLoadErrorResponse,
+    RepositoryUnloadErrorResponse,
+)
+
+__all__ = [
+    # Dataplane
+    "MetadataServerResponse",
+    "MetadataServerErrorResponse",
+    "MetadataTensor",
+    "MetadataModelErrorResponse",
+    "Parameters",
+    "Tags",
+    "TensorData",
+    "RequestOutput",
+    "ResponseOutput",
+    "InferenceResponse",
+    "InferenceErrorResponse",
+    "MetadataModelResponse",
+    "RequestInput",
+    "InferenceRequest",
+    # Model Repository
+    "RepositoryIndexRequest",
+    "RepositoryIndexResponseItem",
+    "State",
+    "RepositoryIndexResponse",
+    "RepositoryIndexErrorResponse",
+    "RepositoryLoadErrorResponse",
+    "RepositoryUnloadErrorResponse",
+]
