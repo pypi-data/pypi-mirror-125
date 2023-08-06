60819f3f44e911053256c28ad6994159c47c5585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:18 +0000

    Add package greport/0.1.0/greport-0.1.0.tar.gz

---
+++ packages/greport/greport-0.1.0.tar.gz/greport-0.1.0/README.md
@@ -0,0 +1,9 @@
+# greport
+
+Python tool that generate *html* test report from googletest *xml* file
+
+## Installation
+
+```
+pip install greport
+```
---
+++ packages/greport/greport-0.1.0.tar.gz/greport-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name='greport',
+    version='0.1.0',
+    description='Generate html from googletest xml file',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='niedong',
+    author_email='niedong0816@126.com',
+    install_requires=["jinja2"],
+    packages=find_packages(),
+    package_data={'greport': ['templates/template.html']},
+    include_package_data=True,
+    entry_points={'console_scripts': ["greport=greport.__main__:main"]},
+)
