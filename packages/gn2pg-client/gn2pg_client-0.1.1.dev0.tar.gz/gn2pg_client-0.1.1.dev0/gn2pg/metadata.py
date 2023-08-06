3e3ac1862b61f29e6389d0193969c08f42722f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:52 +0000

    Add package gn2pg-client/0.1.1.dev0/gn2pg_client-0.1.1.dev0.tar.gz

---
+++ packages/gn2pg-client/gn2pg_client-0.1.1.dev0.tar.gz/gn2pg_client-0.1.1.dev0/gn2pg/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = "gn2pg_client"
+project = "GeoNature 2 PostgreSQL Client application"
+project_no_spaces = project.replace(" ", "")
+version = "0.0.6-dev"
+description = "Import tool from GeoNature to a PostgreSQL database through Export module API (client side)"
+authors = ["@lpofredc"]
+authors_string = ", ".join(authors)
+emails = ["frederic.cloitre@lpo.fr"]
+license = "AGPL-3.0"
+copyright = "2020 " + authors_string
+url = "https://github.com/lpoaura/gn2pg_client"
