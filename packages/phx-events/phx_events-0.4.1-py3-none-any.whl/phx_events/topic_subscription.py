529ce0961fc5db4a3a352bac10f95b712e169734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:25 +0000

    Add package phx-events/0.4.1/phx_events-0.4.1-py3-none-any.whl

---
+++ packages/phx-events/phx_events-0.4.1-py3-none-any.whl/phx_events/topic_subscription.py
@@ -0,0 +1,25 @@
+from asyncio import Event
+from dataclasses import dataclass
+from enum import IntEnum, unique
+from typing import Optional
+
+from phx_events.phx_messages import PHXEventMessage
+
+
+@unique
+class SubscriptionStatus(IntEnum):
+    FAILED = 0
+    SUCCESS = 1
+
+
+@dataclass(frozen=True)
+class TopicSubscribeResult:
+    status: SubscriptionStatus
+    result_message: PHXEventMessage
+
+
+@dataclass()
+class TopicRegistration:
+    status_updated_event: Event
+    connection_ref: Optional[str] = None
+    result: Optional[TopicSubscribeResult] = None
