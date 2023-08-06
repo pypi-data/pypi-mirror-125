0d09f6284ac57f49baacb49c6c0460e5d2242f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:56 +0000

    Add package pyAnVIL/0.0.10rc8/pyAnVIL-0.0.10rc8.tar.gz

---
+++ packages/pyAnVIL/pyAnVIL-0.0.10rc8.tar.gz/pyAnVIL-0.0.10rc8/anvil/transformers/fhir/organization.py
@@ -0,0 +1,59 @@
+"""Represent fhir entity."""
+
+from anvil.transformers.fhir import make_workspace_id
+
+
+class Organization:
+    """Create fhir entity."""
+
+    class_name = "organization"
+    resource_type = "Organization"
+
+    @staticmethod
+    def slug(resource):
+        """Make id."""
+        return make_workspace_id(resource)
+
+    @staticmethod
+    def build_entity(workspace, parent=None):
+        """Create fhir entity."""
+        study_id = workspace.id
+        id = Organization.slug(workspace)
+        # institution = workspace.institute
+        # if not institution:
+        #     logging.getLogger(__name__).warning(f'workspace {study_id} missing institute')
+        #     institution = f"{study_id}-missing-institution"
+
+        # workspace = workspace.attributes.workspace.attributes
+
+        entity = {
+            "resourceType": Organization.resource_type,
+            "id": f"{id}",
+            "meta": {
+                "profile": [
+                    "http://hl7.org/fhir/StructureDefinition/Organization"
+                ]
+            },
+            "identifier": [
+                {
+                    "system": "https://anvil.terra.bio/#workspaces/anvil-datastorage/",
+                    "value": study_id,
+                },
+                {
+                    "system": "urn:ncpi:unique-string",
+                    "value": f"{id}",
+                },
+            ],
+            "name": study_id,
+        }
+
+        if parent:
+            entity['partOf'] = {
+                "reference": parent
+            }
+        elif 'attributes' in workspace:
+            entity['partOf'] = {
+                "reference": f"Organization/{workspace.attributes.reconciler_name.lower()}"
+            }
+
+        return entity
