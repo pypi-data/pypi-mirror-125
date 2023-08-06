71ba0623324edd9d1ab2877efe586aeb9b462a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:46 +0000

    Add package edx-notices/0.10.0/edx-notices-0.10.0.tar.gz

---
+++ packages/edx-notices/edx-notices-0.10.0.tar.gz/edx-notices-0.10.0/notices/static/notices/js/utils.js
@@ -0,0 +1,40 @@
+const ACKNOWLEDGMENT_TYPES = {
+  DISMISSED: "dismissed",
+  CONFIRMED: "confirmed"
+}
+
+function confirmNoticeClick(forwarding_url_override){
+  const url = forwarding_url_override || forwardingUrl;
+  sendAcknowledgment(ACKNOWLEDGMENT_TYPES.CONFIRMED, url)
+}
+
+function dismissNoticeClick(event){
+  let nextUrl;
+  if (event.target.href){
+    // If caller is a link, stop redirection until after API call.
+    event.preventDefault();
+    nextUrl = event.target.href;
+  } else {
+    nextUrl = forwardingUrl;
+  }
+  sendAcknowledgment(ACKNOWLEDGMENT_TYPES.DISMISSED, nextUrl)
+};
+
+function sendAcknowledgment(type, url){
+  const callback = () => {
+    window.location = url;
+  }
+  const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+  let params = { notice_id: noticeId, acknowledgment_type: type};
+  let postRequest = new XMLHttpRequest();
+  postRequest.open("POST", "/notices/api/v1/acknowledge");
+  postRequest.setRequestHeader("Content-type", "application/json");
+  postRequest.setRequestHeader("X-CSRFToken", csrftoken);
+  postRequest.send(JSON.stringify(params));
+  postRequest.onreadystatechange = () => {
+    if (postRequest.readyState === 4 && postRequest.status === 204) {
+      console.log("acknowledgment successful");
+      callback();
+    }
+  };
+}
