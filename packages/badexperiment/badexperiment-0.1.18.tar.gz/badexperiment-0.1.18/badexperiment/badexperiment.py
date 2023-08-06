8a9bf8cb5f044720374531d76d07e79292657f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:01 +0000

    Add package badexperiment/0.1.18/badexperiment-0.1.18.tar.gz

---
+++ packages/badexperiment/badexperiment-0.1.18.tar.gz/badexperiment-0.1.18/badexperiment/badexperiment.py
@@ -0,0 +1,9 @@
+# class badexp(userinput):
+# 	"""docstring for ClassName"""
+# 	def __init__(self, arg):
+# 		super(ClassName, self).__init__()
+# 		self.arg = arg
+# 	print("hello")
+
+def hello():
+    return "hello"
---
+++ packages/badexperiment/badexperiment-0.1.18.tar.gz/badexperiment-0.1.18/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['badexperiment']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0',
+ 'google-api-python-client',
+ 'google-auth-oauthlib',
+ 'pandas',
+ 'pyyaml']
+
+entry_points = \
+{'console_scripts': ['becli = badexperiment.becli:make_iot_yaml']}
+
+setup_kwargs = {
+    'name': 'badexperiment',
+    'version': '0.1.18',
+    'description': 'see https://github.com/turbomam/badexperiment',
+    'long_description': None,
+    'author': 'Mark A. Miller',
+    'author_email': 'mamillerpa@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
