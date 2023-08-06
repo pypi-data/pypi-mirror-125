57cdef34acbea8e1d56d8bc26f4b560b49d9b7fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:29 +0000

    Add package satella/2.18.6/satella-2.18.6.tar.gz

---
+++ packages/satella/satella-2.18.6.tar.gz/satella-2.18.6/satella/coding/structures/__init__.py
@@ -0,0 +1,64 @@
+from .dictionaries import DictObject, apply_dict_object, DictionaryView, TwoWayDictionary, \
+    DirtyDict, KeyAwareDefaultDict, ExpiringEntryDict, SelfCleaningDefaultDict, \
+    CacheDict, ExclusiveWritebackCache, CountingDict, LRUCacheDict, DefaultDict
+from .hashable_objects import HashableWrapper
+from .heaps import Heap, SetHeap, TimeBasedHeap, TimeBasedSetHeap
+from .immutable import Immutable, frozendict
+from .mixins import OmniHashableMixin, ReprableMixin, StrEqHashableMixin, ComparableIntEnum, \
+    HashableIntEnum, ComparableAndHashableBy, ComparableAndHashableByInt, ComparableEnum, \
+    HashableMixin, ComparableAndHashableByStr
+from .proxy import Proxy
+from .queues import Subqueue
+from .ranking import Ranking
+from .singleton import Singleton, SingletonWithRegardsTo, get_instances_for_singleton, \
+    delete_singleton_for
+from .sorted_list import SortedList, SliceableDeque
+from .sparse_matrix import SparseMatrix
+from .typednamedtuple import typednamedtuple
+from .lru import LRU
+from .syncable_droppable import DBStorage, SyncableDroppable
+from .tuples import Vector
+from .push_iterable import PushIterable
+
+__all__ = [
+    'PushIterable',
+    'Vector',
+    'DBStorage', 'SyncableDroppable',
+    'LRU',
+    'LRUCacheDict',
+    'HashableMixin',
+    'CountingDict',
+    'Subqueue',
+    'ExclusiveWritebackCache',
+    'DefaultDict',
+    'ComparableEnum',
+    'CacheDict',
+    'KeyAwareDefaultDict',
+    'Proxy',
+    'ReprableMixin',
+    'ComparableAndHashableByInt',
+    'StrEqHashableMixin',
+    'ComparableIntEnum',
+    'ComparableAndHashableBy',
+    'HashableIntEnum',
+    'ComparableAndHashableByStr',
+    'DirtyDict',
+    'SortedList',
+    'SelfCleaningDefaultDict',
+    'SliceableDeque',
+    'Ranking',
+    'ExpiringEntryDict',
+    'TwoWayDictionary',
+    'typednamedtuple',
+    'HashableWrapper',
+    'DictionaryView',
+    'frozendict',
+    'SparseMatrix',
+    'OmniHashableMixin',
+    'Singleton',
+    'SingletonWithRegardsTo',
+    'DictObject',
+    'apply_dict_object',
+    'Immutable',
+    'SetHeap', 'TimeBasedHeap', 'TimeBasedSetHeap', 'Heap'
+]
