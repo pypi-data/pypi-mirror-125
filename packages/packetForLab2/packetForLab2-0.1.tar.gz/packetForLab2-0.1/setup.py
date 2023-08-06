e2cfe790882331e9affc17ed6c10a12f832b3ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:46 +0000

    Add package packetForLab2/0.1/packetForLab2-0.1.tar.gz

---
+++ packages/packetForLab2/packetForLab2-0.1.tar.gz/packetForLab2-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='packetForLab2',
+    version='0.1',
+    description='Package for lab2',
+    url='',
+    install_requires=['pandas<0.23'],
+    packages=['packet'],
+)
