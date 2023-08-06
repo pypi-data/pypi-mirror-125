cc38b0901c9e3a1c0f771ab1a9b2c92ddb479b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:28 +0000

    Add package cyint-aws-ml-ops-tools/1.2.0/cyint-aws-ml-ops-tools-1.2.0.tar.gz

---
+++ packages/cyint-aws-ml-ops-tools/cyint-aws-ml-ops-tools-1.2.0.tar.gz/cyint-aws-ml-ops-tools-1.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cyint_aws_ml_ops_tools', 'cyint_aws_ml_ops_tools.glue']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.19.6,<2.0.0']
+
+setup_kwargs = {
+    'name': 'cyint-aws-ml-ops-tools',
+    'version': '1.2.0',
+    'description': 'Tools for assisting with managing modern ml-ops-pipelines in AWS Sagemaker',
+    'long_description': None,
+    'author': 'Daniel Fredriksen',
+    'author_email': 'dfredriksen@cyint.technology',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
