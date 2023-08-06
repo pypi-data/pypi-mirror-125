1235a4e7de55c4f28f6021ebc49fb67b3f97045f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:00 +0000

    Add package javasoup/0.2/javasoup-0.2.tar.gz

---
+++ packages/javasoup/javasoup-0.2.tar.gz/javasoup-0.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'javasoup',        
+  packages = ['javasoup'],  
+  version = '0.2',    
+  license='MIT',       
+  description = 'Simple python library that uses puppeteer to pull HTML from a loaded SPA (REQUIRES NODE, NPM, AND PUPPETEER)',   
+  author = 'rs0n',                  
+  author_email = 'rs0n.evolv3@gmail.com',     
+  url = 'https://github.com/R-s0n/javasoup',  
+  download_url = 'https://github.com/R-s0n/javasoup/archive/refs/tags/v_01.tar.gz',  
+  keywords = ['webscraping', 'beautifulsoup', 'soup'],  
+  install_requires=[           
+          'subprocess'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
