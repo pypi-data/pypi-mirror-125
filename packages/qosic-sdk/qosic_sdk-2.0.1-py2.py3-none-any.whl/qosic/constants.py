97dd184b0bbc079ec294e2f46ecf26e168f090dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:38 +0000

    Add package qosic-sdk/2.0.1/qosic_sdk-2.0.1-py2.py3-none-any.whl

---
+++ packages/qosic-sdk/qosic_sdk-2.0.1-py2.py3-none-any.whl/qosic/constants.py
@@ -0,0 +1,8 @@
+REFUND_PATH = "/QosicBridge/user/refund"
+# MTN
+MTN_PAYMENT_PATH = "/QosicBridge/user/requestpayment"
+MTN_PAYMENT_STATUS_PATH = "/QosicBridge/user/gettransactionstatus"
+MTN_PREFIXES = ["51", "52", "53", "61", "62", "66", "67", "69", "90", "91", "96", "97"]
+# MOOV
+MOOV_PAYMENT_PATH = "/QosicBridge/user/requestpaymentmv"
+MOOV_PREFIXES = ["55", "60", "63", "64", "65", "68", "94", "95", "98", "99"]
