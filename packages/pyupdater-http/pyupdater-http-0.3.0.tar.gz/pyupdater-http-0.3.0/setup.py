19db72194d14b9c6ce31b82bbda20a8e728b4582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:02 +0000

    Add package pyupdater-http/0.3.0/pyupdater-http-0.3.0.tar.gz

---
+++ packages/pyupdater-http/pyupdater-http-0.3.0.tar.gz/pyupdater-http-0.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyupdater_http']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.20.0,<0.21.0']
+
+entry_points = \
+{'pyupdater.plugins': ['http_uploader = pyupdater_http.uploader:HTTPUploader']}
+
+setup_kwargs = {
+    'name': 'pyupdater-http',
+    'version': '0.3.0',
+    'description': 'Plugin: HTTP upload support',
+    'long_description': None,
+    'author': 'Kirill Pinchuk',
+    'author_email': 'cybergrind@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
