53964bc6e61e473404bd0a109197c2d5fcfa1a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:05 +0000

    Add package putio-automator/2.1.1.dev4/putio-automator-2.1.1.dev4.tar.gz

---
+++ packages/putio-automator/putio-automator-2.1.1.dev4.tar.gz/putio-automator-2.1.1.dev4/putio_automator/commands/account.py
@@ -0,0 +1,32 @@
+"""
+Commands for managing Put.IO account
+"""
+
+import click
+import yaml
+import putiopy
+import sys
+
+from putio_automator import date_handler, echo, logger
+from putio_automator.cli import cli
+
+
+@cli.group()
+def account():
+    pass
+
+@account.command()
+@click.pass_context
+def info(ctx):
+    "Show account info"
+    try:
+        response = ctx.obj['CLIENT'].Account.info()
+    except putiopy.ClientError as exc:
+        echo('error', exc.message)
+        ctx.exit(1)
+
+    if response['status'] == 'OK':
+        echo('info', yaml.dump(response['info']))
+    else:
+        echo('error', yaml.dump(response))
+        ctx.exit(1)
