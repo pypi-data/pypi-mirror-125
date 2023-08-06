e279955cc19a11a4e2e94bc6ac1efea641719d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:20 +0000

    Add package pybfbc2stats/0.1.2/pybfbc2stats-0.1.2.tar.gz

---
+++ packages/pybfbc2stats/pybfbc2stats-0.1.2.tar.gz/pybfbc2stats-0.1.2/pybfbc2stats/__init__.py
@@ -0,0 +1,15 @@
+from .connection import Connection
+from .client import Client
+from .exceptions import PyBfbc2StatsError, PyBfbc2StatsTimeoutError, PyBfbc2StatsParameterError, \
+    PyBfbc2StatsNotFoundError
+
+"""
+pybfbc2stats.
+Simple Python library for retrieving statistics of Battlefield: Bad Company 2 players.
+"""
+
+__version__ = '0.1.2'
+__author__ = 'cetteup'
+__credits__ = 'nemo, Luigi Auriemma'
+__all__ = ['Connection', 'Client', 'PyBfbc2StatsError', 'PyBfbc2StatsParameterError', 'PyBfbc2StatsTimeoutError',
+           'PyBfbc2StatsNotFoundError']
