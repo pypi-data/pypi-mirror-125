98518c42ccefd826145f72ad6a0113ce2f99c5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:17 +0000

    Add package cordy.py/0.1.0.dev1/cordy.py-0.1.0.dev1.tar.gz

---
+++ packages/cordy.py/cordy.py-0.1.0.dev1.tar.gz/cordy.py-0.1.0.dev1/cordy/util.py
@@ -0,0 +1,9 @@
+import json
+from typing import Any, Callable, Union
+
+# Swappable json encoders and decoders to be used in the library
+Json = Union[dict[str, Any], list[Union[dict[str, Any], Any]]]
+Msg = dict[str, Any]
+
+loads: Callable[[str], Any] = json.loads # Any allows custom type without cast
+dumps: Callable[[Json], str] = lambda dat: json.dumps(dat, separators=(',', ':'))
---
+++ packages/cordy.py/cordy.py-0.1.0.dev1.tar.gz/cordy.py-0.1.0.dev1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cordy', 'cordy.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.7.4,<4.0.0',
+ 'typing-extensions>=3.10.0,<4.0.0',
+ 'yarl>=1.6.3,<2.0.0']
+
+extras_require = \
+{'docs': ['Sphinx>=4.1.2,<5.0.0', 'furo>=2021.7.5-beta.38,<2022.0.0']}
+
+setup_kwargs = {
+    'name': 'cordy.py',
+    'version': '0.1.0.dev1',
+    'description': 'A simple to use Discord API wrapper',
+    'long_description': None,
+    'author': 'BytesToBits',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
