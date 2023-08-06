4009d9ea2eb73bdfc22ce8f4dcfe00d67315d6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:14 +0000

    Add package pypresence-stats/1.7/pypresence-stats-1.7.tar.gz

---
+++ packages/pypresence-stats/pypresence-stats-1.7.tar.gz/pypresence-stats-1.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+ 
+classifiers = []
+ 
+setup(
+  name='pypresence-stats',
+  version='1.7',
+  description='Presence-stats but you can make python script and launch it.',
+  long_description=open("README.md").read(),
+  long_description_content_type='text/markdown',
+  url='https://redirect.biomooping.tk/HOB1UlWkCg',  
+  author='Rukchad Wongprayoon',
+  author_email='contact@biomooping.tk',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Tools', 
+  py_packages=["presencestats"],
+  install_requires=["pypresence","psutil"],
+  entry_points={
+    'console_scripts': ['pypresence=presencestats:launcharg']
+  }
+)
