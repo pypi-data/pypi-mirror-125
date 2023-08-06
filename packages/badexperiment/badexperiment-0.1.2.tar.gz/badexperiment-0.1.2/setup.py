b7b8439508d41e95beda922617fd6fa557529b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:12 +0000

    Add package badexperiment/0.1.2/badexperiment-0.1.2.tar.gz

---
+++ packages/badexperiment/badexperiment-0.1.2.tar.gz/badexperiment-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['badexperiment']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.3,<9.0.0']
+
+setup_kwargs = {
+    'name': 'badexperiment',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
