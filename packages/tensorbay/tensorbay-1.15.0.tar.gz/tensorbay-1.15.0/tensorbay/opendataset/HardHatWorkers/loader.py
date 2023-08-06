aa2e118ecc69df4a94d9fb4eb7ec35b23352280e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:45 +0000

    Add package tensorbay/1.15.0/tensorbay-1.15.0.tar.gz

---
+++ packages/tensorbay/tensorbay-1.15.0.tar.gz/tensorbay-1.15.0/tensorbay/opendataset/HardHatWorkers/loader.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name
+# pylint: disable=missing-module-docstring
+
+import os
+from typing import List
+from xml.etree import ElementTree
+
+from tensorbay.dataset import Data, Dataset
+from tensorbay.label import LabeledBox2D
+from tensorbay.opendataset._utility import glob
+
+DATASET_NAME = "HardHatWorkers"
+
+
+def HardHatWorkers(path: str) -> Dataset:
+    """Dataloader of the `Hard Hat Workers`_ dataset.
+
+    .. _Hard Hat Workers: https://makeml.app/datasets/hard-hat-workers
+
+    The file structure should be like::
+
+        <path>
+            annotations/
+                hard_hat_workers0.xml
+                ...
+            images/
+                hard_hat_workers0.png
+                ...
+
+    Arguments:
+        path: The root directory of the dataset.
+
+    Returns:
+        Loaded :class:`~tensorbay.dataset.dataset.Dataset` instance.
+
+    """
+    dataset = Dataset(DATASET_NAME)
+    dataset.load_catalog(os.path.join(os.path.dirname(__file__), "catalog.json"))
+    segment = dataset.create_segment()
+    image_paths = glob(os.path.join(path, "images", "*.png"))
+    for image_path in image_paths:
+        data = Data(image_path)
+        file_name = os.path.splitext(os.path.basename(image_path))[0]
+        data.label.box2d = _load_labels(os.path.join(path, "annotations", file_name + ".xml"))
+        segment.append(data)
+    return dataset
+
+
+def _load_labels(label_file: str) -> List[LabeledBox2D]:
+    label_tree = ElementTree.parse(label_file)
+    labels = []
+    for obj in label_tree.findall("object"):
+        bndbox = obj.find("bndbox")
+        labels.append(
+            LabeledBox2D(
+                *(int(child.text) for child in bndbox),  # type: ignore[arg-type, union-attr]
+                category=obj.find("name").text  # type: ignore[union-attr]
+            )
+        )
+    return labels
---
+++ packages/tensorbay/tensorbay-1.15.0.tar.gz/tensorbay-1.15.0/tensorbay/opendataset/VOC2012Detection/loader.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 Graviti. Licensed under MIT License.
+#
+# pylint: disable=invalid-name, missing-module-docstring
+
+import os
+from xml.etree import ElementTree
+
+from tensorbay.dataset import Data, Dataset
+from tensorbay.label import LabeledBox2D
+
+_SEGMENT_NAMES = ("train", "val")
+_BOOLEAN_ATTRIBUTES = {"occluded", "difficult", "truncated"}
+DATASET_NAME = "VOC2012Detection"
+
+
+def VOC2012Detection(path: str) -> Dataset:
+    """Dataloader of the 'VOC2012Detection'_ dataset.
+
+    .. _VOC2012Detection: http://host.robots.ox.ac.uk/pascal/VOC/voc2012/
+
+    The file structure should be like::
+
+        <path>
+            Annotations/
+                <image_name>.xml
+                ...
+            JPEGImages/
+                <image_name>.jpg
+                ...
+            ImageSets/
+                Main/
+                    train.txt
+                    val.txt
+                    ...
+                ...
+            ...
+
+    Arguments:
+        path: The root directory of the dataset.
+
+    Returns:
+        Loaded :class: `~tensorbay.dataset.dataset.Dataset` instance.
+
+    """
+    root_path = os.path.abspath(os.path.expanduser(path))
+    annotation_path = os.path.join(root_path, "Annotations")
+    image_path = os.path.join(root_path, "JPEGImages")
+    main_path = os.path.join(root_path, "ImageSets", "Main")
+
+    dataset = Dataset(DATASET_NAME)
+    dataset.load_catalog(os.path.join(os.path.dirname(__file__), "catalog.json"))
+
+    for segment_name in _SEGMENT_NAMES:
+        segment = dataset.create_segment(segment_name)
+        with open(os.path.join(main_path, f"{segment_name}.txt")) as fp:
+            for filename in fp:
+                filename = filename.strip()
+                segment.append(_get_data(filename, image_path, annotation_path))
+    return dataset
+
+
+def _get_data(filename: str, image_path: str, annotation_path: str) -> Data:
+    """Get all information of the datum corresponding to filename.
+
+    Arguments:
+        filename: The filename of the data.
+        image_path: The path of the image directory.
+        annotation_path: The path of the annotation directory.
+
+    Returns:
+        Data: class: `~tensorbay.dataset.data.Data` instance.
+
+    """
+    data = Data(os.path.join(image_path, f"{filename}.jpg"))
+    data.label.box2d = []
+    tree = ElementTree.parse(os.path.join(annotation_path, f"{filename}.xml"))
+    for obj in tree.findall("object"):
+        attributes = {}
+        for child in obj:
+            if child.tag == "name":
+                category = child.text
+            elif child.tag == "bndbox":
+                box = (
+                    float(child.find("xmin").text),  # type:ignore[arg-type, union-attr]
+                    float(child.find("ymin").text),  # type:ignore[arg-type, union-attr]
+                    float(child.find("xmax").text),  # type:ignore[arg-type, union-attr]
+                    float(child.find("ymax").text),  # type:ignore[arg-type, union-attr]
+                )
+            elif child.tag == "pose":
+                attributes[child.tag] = child.text
+            elif child.tag in _BOOLEAN_ATTRIBUTES:
+                attributes[child.tag] = bool(
+                    int(child.text)  # type:ignore[assignment, arg-type]
+                )
+        data.label.box2d.append(LabeledBox2D(*box, category=category, attributes=attributes))
+    return data
