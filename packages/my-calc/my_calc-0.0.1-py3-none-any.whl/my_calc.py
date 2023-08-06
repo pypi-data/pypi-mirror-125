7c71f6edd8f83b4b3a405d20ffebed33ae172bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:41 +0000

    Add package my-calc/0.0.1/my_calc-0.0.1-py3-none-any.whl

---
+++ packages/my-calc/my_calc-0.0.1-py3-none-any.whl/my_calc.py
@@ -0,0 +1,12 @@
+class Calc:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
