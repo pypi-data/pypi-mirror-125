3ca62ee6f6b7b92aa4fdcee0a45c3c839bbe9137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:02 +0000

    Add package Beetroot/1.1.3.2/Beetroot-1.1.3.2-py2.py3-none-any.whl

---
+++ packages/Beetroot/Beetroot-1.1.3.2-py2.py3-none-any.whl/beetroot/text.py
@@ -0,0 +1,54 @@
+try:
+    import upsidedown  
+    
+except (ModuleNotFoundError, ImportError):
+    pass
+
+try:
+    from zalgo_text import zalgo as zalg
+    
+except (ModuleNotFoundError, ImportError):
+    pass
+
+import random
+
+from .exception import *
+
+gen = random.SystemRandom()
+
+
+
+class teg:
+    def __init__(self):
+        self.zal = zalg.zalgo()
+        
+    def udown(self, intext):
+        """Generates upside-down text"""
+        try:
+            return upsidedown.transform(str(intext))
+        
+        except NameError:
+            raise ModuleError("Upsidedown must be installed. Try pip install upsidedown or pip install beetroot[text].")
+        
+    def zalgo(self, intext, **kwargs):
+        """Generates Zalgo text"""
+        craziness = int(
+            round(
+                kwargs.get(
+                    "crazy",
+                    10
+                )
+            )
+        )
+        try:
+            self.zal.numAccentsUp = (craziness, craziness * 10)
+            self.zal.numAccentsDown = (craziness, craziness * 10)
+            self.zal.numAccentsMiddle = (craziness, craziness * 10)
+            self.zal.maxAccentsPerLetter = craziness * 40
+            return self.zal.zalgofy(str(intext))
+        
+        except NameError:
+            raise ModuleError("Zalgo_text must be installed. Try pip install zalgo-text or pip install beetroot[text].")
+        
+text = teg()
+del teg
