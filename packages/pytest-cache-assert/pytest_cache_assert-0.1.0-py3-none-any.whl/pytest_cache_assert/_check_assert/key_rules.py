e3f712b7a7321dbf0f935b54c359bd1c74047c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:00 +0000

    Add package pytest-cache-assert/0.1.0/pytest_cache_assert-0.1.0-py3-none-any.whl

---
+++ packages/pytest-cache-assert/pytest_cache_assert-0.1.0-py3-none-any.whl/pytest_cache_assert/_check_assert/key_rules.py
@@ -0,0 +1,80 @@
+"""Key Rules."""
+
+from typing import Callable, List
+
+import attr
+import pendulum
+from attrs_strict import type_validator
+from beartype import beartype
+from pendulum.parsing.exceptions import ParserError
+
+from .constants import DIFF_TYPES
+
+
+@beartype
+def check_suppress(old: DIFF_TYPES, new: DIFF_TYPES) -> bool:
+    """Return True to suppress differences.
+
+    Args:
+        old: the old value
+        new: the new value
+
+    Returns:
+        bool: Always True
+
+    """
+    return True
+
+
+@beartype
+def check_exact(old: DIFF_TYPES, new: DIFF_TYPES) -> bool:
+    """Check for value equality.
+
+    Args:
+        old: the old value
+        new: the new value
+
+    Returns:
+        bool: True if both values are the exact same
+
+    """
+    return old == new
+
+
+@beartype
+def check_type(old: DIFF_TYPES, new: DIFF_TYPES) -> bool:
+    """Check if both values are the exact same or same non-string type.
+
+    Will attempt to parse strings to int, float, or datetime.
+
+    Args:
+        old: the old value
+        new: the new value
+
+    Returns:
+        bool: True if both values are the same kind
+
+    """
+    if (
+        check_exact(old=old, new=new)  # Catches if both null, etc.
+        or (not isinstance(old, str) and isinstance(old, type(new)))
+    ):
+        return True
+
+    for converter in [int, float, pendulum.parse]:
+        try:
+            converter(old)
+            converter(new)
+            return True
+        except (ValueError, ParserError):
+            pass
+
+    return False
+
+
+@attr.s(auto_attribs=True, frozen=True, kw_only=True)
+class KeyRule:  # noqa: H601
+    """Key Rule."""
+
+    key_list: List[str] = attr.ib(validator=type_validator())
+    func: Callable[[DIFF_TYPES, DIFF_TYPES], bool] = attr.ib(default=check_exact, validator=type_validator())
