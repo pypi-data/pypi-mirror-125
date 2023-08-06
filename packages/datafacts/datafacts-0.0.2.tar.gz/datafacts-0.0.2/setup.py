aed9713e4f861e22f478da904592c13e1b7e4550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:12 +0000

    Add package datafacts/0.0.2/datafacts-0.0.2.tar.gz

---
+++ packages/datafacts/datafacts-0.0.2.tar.gz/datafacts-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('LICENSE', 'r') as f:
+    license_file = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+setup(
+    name='datafacts',
+    version='0.0.2',
+    description='Quickly generate an image with population-level facts about your dataset',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license=license_file,
+    author='Eric Yates',
+    author_email='eric@medleyagency.com',
+    url='https://github.com/MedleyLabs/datafacts',
+    packages=['datafacts'],
+    install_requires=[req for req in requirements if not req.startswith('#')],
+)
