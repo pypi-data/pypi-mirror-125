e9e8bfcadf9adf1e27da68b1dab5e086ca4ac8b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:47 +0000

    Add package pymeet/0.0.5/pymeet-0.0.5.tar.gz

---
+++ packages/pymeet/pymeet-0.0.5.tar.gz/pymeet-0.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from setuptools import setup
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+NAME = 'pymeet'
+VERSION = '0.0.5'
+URL = 'https://github.com/SSripilaipong/pymeet'
+LICENSE = 'MIT'
+AUTHOR = 'SSripilaipong'
+EMAIL = 'SHSnail@mail.com'
+CONSOLE_SCRIPT = 'pymeet=pymeet.cli:main'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=['pymeet'],
+    url=URL,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=None,
+    long_description=None,
+    python_requires='>=3.6',
+    install_requires=requirements,
+    classifiers=[],
+    entry_points={
+        'console_scripts': [CONSOLE_SCRIPT],
+    }
+)
