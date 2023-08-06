814d159154c696c856ed902458d24cb18b6ee457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:05 +0000

    Add package streamlit-vtkjs/0.0.5a0/streamlit-vtkjs-0.0.5a0.tar.gz

---
+++ packages/streamlit-vtkjs/streamlit-vtkjs-0.0.5a0.tar.gz/streamlit-vtkjs-0.0.5a0/streamlit_vtkjs/frontend/build/precache-manifest.6a087122be8f7f74b0f92392a6a366c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965cfa2c58bda38164306dee146eff71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5e03f741dd82851780e",
+    "url": "./static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "e5e03f741dd82851780e",
+    "url": "./static/js/2.895d7596.chunk.js"
+  },
+  {
+    "revision": "5fe1283a12dc6b274f8cda89e6a00211",
+    "url": "./static/js/2.895d7596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc680e4ac20ff4931c57",
+    "url": "./static/js/main.4c33d1d9.chunk.js"
+  },
+  {
+    "revision": "1d52cc22638050ace4c0",
+    "url": "./static/js/runtime-main.6eda6c42.js"
+  }
+]);
