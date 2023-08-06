52aaa957511f67fd95d49380e21aa54226e72766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:21 +0000

    Add package lxwisbadwei/0.1/lxwisbadwei-0.1.tar.gz

---
+++ packages/lxwisbadwei/lxwisbadwei-0.1.tar.gz/lxwisbadwei-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='lxwisbadwei',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='https://gitee.com/badwei/',
+      author='lxwbadwei',
+      author_email='1211304872@qq.com',
+      license='MIT',
+      packages=['lxwisbadwei'],
+      zip_safe=False)
