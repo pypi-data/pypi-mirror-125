937ea8e0ea0b14217e3532d6a5a60e8a2347a469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:51 +0000

    Add package some-da2021-package/0.1.0/some_da2021_package-0.1.0.tar.gz

---
+++ packages/some-da2021-package/some_da2021_package-0.1.0.tar.gz/some_da2021_package-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='some_da2021_package',
+    version='0.1.0',  
+    url='https://github.com/lejabque',
+    description='A example Python package',
+    author='Denis Vorkozhokov',
+    author_email='vorkdenis1@gmail.com',
+    license='MIT',
+    packages=['some_da2021_package'],
+    install_requires=['numpy==1.0.0'],
+)
