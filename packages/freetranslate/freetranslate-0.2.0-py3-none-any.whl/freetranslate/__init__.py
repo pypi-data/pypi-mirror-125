49bffa164e06c8f38ddae29e2669c5ad9104dc00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:42 +0000

    Add package freetranslate/0.2.0/freetranslate-0.2.0-py3-none-any.whl

---
+++ packages/freetranslate/freetranslate-0.2.0-py3-none-any.whl/freetranslate/__init__.py
@@ -0,0 +1,7 @@
+"""
+Another translate API for Python.
+"""
+from . import googletranslate, googlewebtranslate, libretranslate, bingtranslator, translateresult
+
+__version__ = "0.2.0"
+__author__ = "tretrauit"
