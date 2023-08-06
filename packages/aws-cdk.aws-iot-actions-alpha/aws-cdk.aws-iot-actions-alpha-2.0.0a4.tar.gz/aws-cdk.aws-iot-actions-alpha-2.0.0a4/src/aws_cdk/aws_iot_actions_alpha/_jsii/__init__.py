acf04c821535051df84536f208214cfee32b314c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:48 +0000

    Add package aws-cdk.aws-iot-actions-alpha/2.0.0a4/aws-cdk.aws-iot-actions-alpha-2.0.0a4.tar.gz

---
+++ packages/aws-cdk.aws-iot-actions-alpha/aws-cdk.aws-iot-actions-alpha-2.0.0a4.tar.gz/aws-cdk.aws-iot-actions-alpha-2.0.0a4/src/aws_cdk/aws_iot_actions_alpha/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/aws-iot-actions-alpha",
+    "2.0.0-alpha.4",
+    __name__[0:-6],
+    "aws-iot-actions-alpha@2.0.0-alpha.4.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
