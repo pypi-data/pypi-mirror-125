9e63f462bbc16d0ad829115ba23412bed1cc6f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:52 +0000

    Add package pylabel/0.1.3/pylabel-0.1.3.tar.gz

---
+++ packages/pylabel/pylabel-0.1.3.tar.gz/pylabel-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pylabel',
+    packages=['pylabel'],
+    version='0.1.3',
+    description='Transform, analyze, and visualize computer vision annotations.',
+    author='PyLabel Project',
+    license='MIT',
+    install_requires=['pandas','bbox_visualizer','matplotlib','opencv-python','scikit-learn'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
