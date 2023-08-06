513927e351eacfd106fdfbe6c1a182a5b0603fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:37 +0000

    Add package twj-test/0.1/twj-test-0.1.tar.gz

---
+++ packages/twj-test/twj-test-0.1.tar.gz/twj-test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(name='twj-test',
+    version='0.1',
+    description='A math package',
+    author='twj',
+    author_email='twjxi@qq.com',
+    url='https://github.com/TWJxi/gittest1',
+    packages=find_packages()
+)
