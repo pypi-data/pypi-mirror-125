de2d546f04fbdb182c344938f3d2bb26a3f3ed7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:05 +0000

    Add package staircase/2.1.0/staircase-2.1.0-py3-none-any.whl

---
+++ packages/staircase/staircase-2.1.0-py3-none-any.whl/staircase/__init__.py
@@ -0,0 +1,38 @@
+from staircase.constants import inf
+from staircase.core.arrays.aggregation import _aggregate as agg
+from staircase.core.arrays.aggregation import _max as max
+from staircase.core.arrays.aggregation import _mean as mean
+from staircase.core.arrays.aggregation import _median as median
+from staircase.core.arrays.aggregation import _min as min
+from staircase.core.arrays.aggregation import _sum as sum
+from staircase.core.arrays.transform import corr, cov, limit, sample
+from staircase.core.slicing import StairsSlicer
+from staircase.core.stairs import Stairs, _add_operations
+from staircase.core.stats.distribution import Dist
+from staircase.test_data import make_test_data
+
+_add_operations()
+
+
+def get_version():
+    def get_version_post_py38():
+        from importlib.metadata import version  # type: ignore
+
+        return version(__name__)
+
+    def get_version_pre_py38():
+        from pkg_resources import get_distribution
+
+        return get_distribution(__name__).version
+
+    def default_version():
+        return "unknown"
+
+    for func in (get_version_post_py38, get_version_pre_py38, default_version):
+        try:
+            return func()
+        except Exception:
+            pass
+
+
+__version__ = get_version()
