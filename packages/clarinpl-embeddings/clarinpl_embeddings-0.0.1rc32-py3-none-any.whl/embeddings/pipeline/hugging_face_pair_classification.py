f2ceb493002cc1f068f4b2328ff76e9af49356f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:25 +0000

    Add package clarinpl-embeddings/0.0.1rc32/clarinpl_embeddings-0.0.1rc32-py3-none-any.whl

---
+++ packages/clarinpl-embeddings/clarinpl_embeddings-0.0.1rc32-py3-none-any.whl/embeddings/pipeline/hugging_face_pair_classification.py
@@ -0,0 +1,46 @@
+from typing import Any, Dict, Optional, Tuple
+
+import datasets
+import numpy as np
+from flair.data import Corpus
+
+from embeddings.data.data_loader import HuggingFaceDataLoader
+from embeddings.data.dataset import HuggingFaceDataset
+from embeddings.data.io import T_path
+from embeddings.embedding.auto_flair import AutoFlairDocumentEmbedding
+from embeddings.evaluator.text_classification_evaluator import TextClassificationEvaluator
+from embeddings.model.flair_model import FlairModel
+from embeddings.pipeline.standard_pipeline import StandardPipeline
+from embeddings.task.flair_task.text_pair_classification import TextPairClassification
+from embeddings.transformation.flair_transformation.pair_classification_corpus_transformation import (
+    PairClassificationCorpusTransformation,
+)
+
+
+class HuggingFacePairClassificationPipeline(
+    StandardPipeline[str, datasets.DatasetDict, Corpus, Dict[str, np.ndarray], Dict[str, Any]]
+):
+    def __init__(
+        self,
+        embedding_name: str,
+        dataset_name: str,
+        input_columns_names_pair: Tuple[str, str],
+        target_column_name: str,
+        output_path: T_path,
+        task_model_kwargs: Optional[Dict[str, Any]] = None,
+        task_train_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        dataset = HuggingFaceDataset(dataset_name)
+        data_loader = HuggingFaceDataLoader()
+        transformation = PairClassificationCorpusTransformation(
+            input_columns_names_pair, target_column_name
+        )
+        embedding = AutoFlairDocumentEmbedding.from_hub(embedding_name)
+        task = TextPairClassification(
+            output_path,
+            task_model_kwargs=task_model_kwargs,
+            task_train_kwargs=task_train_kwargs,
+        )
+        model = FlairModel(embedding, task)
+        evaluator = TextClassificationEvaluator()
+        super().__init__(dataset, data_loader, transformation, model, evaluator)
