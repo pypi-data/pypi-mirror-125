6ca4654affaacb9c926942308aedc561b9bb88bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:29 +0000

    Add package zcls/0.13.5/zcls-0.13.5.tar.gz

---
+++ packages/zcls/zcls-0.13.5.tar.gz/zcls-0.13.5/zcls/config/key_word.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/12/28 上午10:35
+@file: key_word.py
+@author: zj
+@description: 
+"""
+
+KEY_OUTPUT = 'output'
+KEY_LOSS = 'loss'
+
+KEY_SEP = ',,'
+KEY_DATASET = 'data.csv'
+KEY_CLASSES = 'cls.csv'
