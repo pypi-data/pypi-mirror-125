71eecec598d0bd0221054b4ea9e3ce8d4258672d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:09 +0000

    Add package gcm-common/0.1.9/gcm_common-0.1.9.tar.gz

---
+++ packages/gcm-common/gcm_common-0.1.9.tar.gz/gcm_common-0.1.9/gcm_common/updater.py
@@ -0,0 +1,50 @@
+import compileall
+import os
+import shutil
+from datetime import datetime
+from pathlib import Path
+from shutil import copyfile
+
+
+def update(just_pyc=False):
+    for subdir, dirs, files in os.walk("."):
+        if 'venv' in subdir or '.env' in subdir:
+            continue
+        if '__pycache__' in subdir:
+            shutil.rmtree(subdir)
+
+    compileall.compile_dir('.')
+    if os.path.exists('update'):
+        shutil.rmtree('update')
+
+    Path('update').mkdir(parents=True, exist_ok=True)
+
+    for subdir, dirs, files in os.walk("."):
+        if 'git' in subdir or 'update' in subdir or '.idea' in subdir or subdir.startswith(
+                '.\\log') or 'venv' in subdir or 'test' in subdir:
+            continue
+
+        if '__pycache__' in subdir:
+            for file in files:
+                if 'update' in file:
+                    continue
+                filename = os.path.join('update', subdir.replace('__pycache__', ''), file)
+                filename = filename.replace('.cpython-36', '')
+                copyfile(os.path.join(subdir, file), filename)
+            continue
+
+        if just_pyc:
+            continue
+        Path(os.path.join('update'), subdir).mkdir(parents=True, exist_ok=True)
+
+        for file in files:
+            if 'git' in file or file.endswith('.py') or file.endswith('.zip') or file.endswith('.md') or file.endswith(
+                    '.jpg'):
+                continue
+            filename = os.path.join('update', subdir) + '\\' + file
+            copyfile(os.path.join(subdir, file), filename)
+
+    now = datetime.now()
+    output_filename = 'update' + now.strftime('%Y%m%d-%H%M')
+    shutil.make_archive(output_filename, 'zip', "update")
+    shutil.rmtree('update')
