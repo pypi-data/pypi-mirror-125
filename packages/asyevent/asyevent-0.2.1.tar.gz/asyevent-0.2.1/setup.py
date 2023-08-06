32712d3042e8e72bf75bfd64a4788bdf4ec06d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:46 +0000

    Add package asyevent/0.2.1/asyevent-0.2.1.tar.gz

---
+++ packages/asyevent/asyevent-0.2.1.tar.gz/asyevent-0.2.1/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="asyevent",
+    version="0.2.1",
+    description="An implementation of events and asynchronous callbacks using decorators.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HerveZu/asyevent",
+    author="Zucchinetti Hervé",
+    author_email="herve.zucchinetti@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=[
+        "asyevent",
+        "asyevent/examples",
+        "asyevent/utils"
+    ],
+    include_package_data=True,
+    install_requires=["asyncio"],
+)
