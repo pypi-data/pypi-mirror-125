4e47b7f30f788d222efdd520602d1922ba8639ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:34 +0000

    Add package mre/0.11.1/mre-0.11.1-py3-none-any.whl

---
+++ packages/mre/mre-0.11.1-py3-none-any.whl/mre/Lookaround.py
@@ -0,0 +1,20 @@
+from .Regex import Regex
+from .Group import Group
+from typing import Union
+
+
+class Lookahead(Group):
+    """Lookahead class."""
+
+    def __init__(self, regex: Union[str, int, Regex] = "", must_not_include: bool = False):
+        lookahead = "?!" if must_not_include else "?="
+
+        if isinstance(regex, int):
+            self._init_attributes()
+            regex = Regex(lookahead, self.backreferences(regex))
+        elif isinstance(regex, str):
+            regex = lookahead + regex
+        else:
+            regex = lookahead + regex.get()
+
+        super().__init__(regex, non_capturing=False)
---
+++ packages/mre/mre-0.11.1-py3-none-any.whl/mre/__init__.py
@@ -0,0 +1,10 @@
+# From Regex
+from .Regex import Regex
+from .Anchor import Anchor
+from .Comment import Comment
+from .Quantifier import Quantifier
+from .Set import Set
+
+# From Group
+from .Group import Group
+from .Lookaround import Lookahead
