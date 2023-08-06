ee544a08cde1aa1f610adb7a104a7870b60360da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:17 +0000

    Add package oneNeuron-pypi-Rishbah-76/0.0.1/oneNeuron_pypi-Rishbah-76-0.0.1.tar.gz

---
+++ packages/oneNeuron-pypi-Rishbah-76/oneNeuron_pypi-Rishbah-76-0.0.1.tar.gz/oneNeuron_pypi-Rishbah-76-0.0.1/README.md
@@ -0,0 +1,6 @@
+# oneNeuron_pypi
+oneNeuron_pypi | Perceptron Pypi Package
+
+# Referecnce -
+[Official Python Package](https://packaging.python.org/tutorials/packaging-projects/)
+[Github official Doc for Github actions](https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-python)
---
+++ packages/oneNeuron-pypi-Rishbah-76/oneNeuron_pypi-Rishbah-76-0.0.1.tar.gz/oneNeuron_pypi-Rishbah-76-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+USERNAME="Rishbah-76"
+PROJECTNAME="oneNeuron_pypi"
+
+setuptools.setup(
+    name=f"{PROJECTNAME}-{USERNAME}",
+    version="0.0.1",
+    author=USERNAME,
+    author_email="rbrishabh76@example.com",
+    description="It is a Pereceptron Implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USERNAME}/{PROJECTNAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USERNAME}/{PROJECTNAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "tqdm"
+    ],
+)
