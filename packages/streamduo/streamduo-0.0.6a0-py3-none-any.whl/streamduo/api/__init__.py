56028d606545bc65b3f22767d50bc5d9f4b4a245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:39 +0000

    Add package streamduo/0.0.6a0/streamduo-0.0.6a0-py3-none-any.whl

---
+++ packages/streamduo/streamduo-0.0.6a0-py3-none-any.whl/streamduo/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from streamduo.api.client_controller_api import ClientControllerApi
+from streamduo.api.health_controller_api import HealthControllerApi
+from streamduo.api.organization_controller_api import OrganizationControllerApi
+from streamduo.api.record_controller_api import RecordControllerApi
+from streamduo.api.stream_controller_api import StreamControllerApi
+from streamduo.api.user_controller_api import UserControllerApi
