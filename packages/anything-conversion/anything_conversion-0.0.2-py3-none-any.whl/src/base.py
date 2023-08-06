c6a20cfe214dc33d9558c21e1d10803a07a503c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:55 +0000

    Add package anything-conversion/0.0.2/anything_conversion-0.0.2-py3-none-any.whl

---
+++ packages/anything-conversion/anything_conversion-0.0.2-py3-none-any.whl/src/base.py
@@ -0,0 +1,39 @@
+from __future__ import division
+import math
+class AreaConverter():
+    def squareyard_squarefeet_or_foot(sft):
+        """
+        here f
+        1 square yard = 9 suare feet
+        
+        """
+        res=9*sft
+        return res
+    def acre_squarefeet_or_foot(acr):
+        """
+        here f
+        1 acre = 43560 suare feet
+        
+        """
+        res=acr*43560
+        return res
+
+class WeightConverter():
+    def kilogram_gram(kg):
+        """
+        here f
+        1 kilogram = 1000 grams 
+        
+        """
+        res=kg*1000
+        return res
+    def metricton_kilogram(mrct):
+        """
+        here f
+        1 metricton = 1000 kg
+        
+        """
+        res=mrct*9
+        return res
+
+        
