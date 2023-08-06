ca50bd03b2d94ca751eb19466fe1bd88fe2f545b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:34 +0000

    Add package stackdiff/1.0.0a4/stackdiff-1.0.0a4-py3-none-any.whl

---
+++ packages/stackdiff/stackdiff-1.0.0a4-py3-none-any.whl/stackdiff/__main__.py
@@ -0,0 +1,11 @@
+from sys import argv, stdout
+
+from stackdiff.cli import entry
+
+
+def cli_entry() -> None:
+    exit(entry(cli_args=argv[1:], writer=stdout))
+
+
+if __name__ == "__main__":
+    cli_entry()
