c6db64dd6b4636820c7b4e70b993c85860c0c21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:32 +0000

    Add package mytelegraph/0.0.2/mytelegraph-0.0.2.tar.gz

---
+++ packages/mytelegraph/mytelegraph-0.0.2.tar.gz/mytelegraph-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='mytelegraph',
+	author='Cingkariak Sawah',
+	author_email='fajrim228@gmail.com',
+	description='a tiny helper for posting to a Telegra.ph page.',
+	version='0.0.2', 
+	packages=['telegraph'], 
+	scripts=['bin/posting']
+)
