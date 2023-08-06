1bb57b5f3dbcf70bf56869b41d2d843818bb2c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:09 +0000

    Add package ovirt-engine-sdk-python/4.5.0/ovirt-engine-sdk-python-4.5.0.tar.gz

---
+++ packages/ovirt-engine-sdk-python/ovirt-engine-sdk-python-4.5.0.tar.gz/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+VERSION="4.5.0"
+MILESTONE=
+RPM_RELEASE="1"
+
+PACKAGE_NAME="python-ovirt-engine-sdk4"
+
+RPM_VERSION=$VERSION
+PACKAGE_VERSION=$VERSION
+[ -n "$MILESTONE" ] && PACKAGE_VERSION+="_$MILESTONE"
+DISPLAY_VERSION=$PACKAGE$VERSION
+
+TARBALL="ovirt-engine-sdk-python-$PACKAGE_VERSION.tar.gz"
+
+
+dist() {
+  echo "Creating tar archive '$TARBALL' ... "
+  sed \
+   -e "s|@RPM_VERSION@|$RPM_VERSION|g" \
+   -e "s|@RPM_RELEASE@|$RPM_RELEASE|g" \
+   -e "s|@PACKAGE_NAME@|$PACKAGE_NAME|g" \
+   -e "s|@PACKAGE_VERSION@|$PACKAGE_VERSION|g" \
+   < python-ovirt-engine-sdk4.spec.in > python-ovirt-engine-sdk4.spec
+  sed \
+   -e "s|@RPM_VERSION@|$RPM_VERSION|g" \
+   -e "s|@RPM_RELEASE@|$RPM_RELEASE|g" \
+   -e "s|@PACKAGE_NAME@|$PACKAGE_NAME|g" \
+   -e "s|@PACKAGE_VERSION@|$PACKAGE_VERSION|g" \
+   < python38-ovirt-engine-sdk4.spec.in > python38-ovirt-engine-sdk4.spec
+
+  find ./* -not -name '*.spec' -not -name 'mocker-*' -not -name 'ci_build_summary.html' -type f | tar --files-from /proc/self/fd/0 -czf "$TARBALL" python-ovirt-engine-sdk4.spec
+  echo "tar archive '$TARBALL' created."
+}
+
+$1
