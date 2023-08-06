a3e16a8e9f5c81b444b90e250e49eb1ac25ae96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:27 +0000

    Add package rlzero/0.3.0/rlzero-0.3.0.tar.gz

---
+++ packages/rlzero/rlzero-0.3.0.tar.gz/rlzero-0.3.0/rlzero/Keyboard.py
@@ -0,0 +1,37 @@
+from raylib import ffi, rl
+
+
+def _fix_key(kname):
+    # return is a reserved word, so alias enter to return
+    #if kname == 'enter':
+    #    kname = 'return'
+    kname = kname.upper()
+    if not kname.startswith("KEY_"):
+        kname = "KEY_" + kname
+    return kname
+
+
+class Keyboard:
+    """
+    Handles input from keyboard
+    """
+    def __getattr__(self, kname):
+        f = _fix_key(kname)
+        return rl.IsKeyDown(getattr(rl, f))
+
+
+
+    def key_down(self, kname):
+        """
+        Test if key is currently down
+        """
+        f = _fix_key(kname)
+        return rl.IsKeyDown(getattr(rl, f))
+
+    def key_pressed(self, kname):
+        """
+        Test if key was pressed recently
+        """
+        f = _fix_key(kname)
+        return rl.IsKeyPressed(getattr(rl, f))
+
