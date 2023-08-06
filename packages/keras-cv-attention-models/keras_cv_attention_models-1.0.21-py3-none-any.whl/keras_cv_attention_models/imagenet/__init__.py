eba6bbed482b523f739ca630fa10a8ad7d359af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:10 +0000

    Add package keras-cv-attention-models/1.0.21/keras_cv_attention_models-1.0.21-py3-none-any.whl

---
+++ packages/keras-cv-attention-models/keras_cv_attention_models-1.0.21-py3-none-any.whl/keras_cv_attention_models/imagenet/__init__.py
@@ -0,0 +1,4 @@
+from keras_cv_attention_models.imagenet.eval_func import evaluation, plot_hists
+from keras_cv_attention_models.imagenet import data
+from keras_cv_attention_models.imagenet import callbacks
+# from keras_cv_attention_models.imagenet.train import train
