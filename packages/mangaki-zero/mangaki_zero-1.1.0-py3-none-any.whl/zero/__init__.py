641157129097f97b0d330d86b288e36daff1e64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:51 +0000

    Add package mangaki-zero/1.1.0/mangaki_zero-1.1.0-py3-none-any.whl

---
+++ packages/mangaki-zero/mangaki_zero-1.1.0-py3-none-any.whl/zero/__init__.py
@@ -0,0 +1,13 @@
+from .recommendation_algorithm import (RecommendationAlgorithm,
+                                       register_algorithm)
+from .dataset import Dataset
+
+from .als import MangakiALS
+from .als2 import MangakiALS2
+from .knn import MangakiKNN
+from .sgd import MangakiSGD
+from .sgd2 import MangakiSGD2
+from .svd import MangakiSVD
+from .svd2 import MangakiSVD2
+from .zero import MangakiZero
+from .lasso import MangakiLASSO
