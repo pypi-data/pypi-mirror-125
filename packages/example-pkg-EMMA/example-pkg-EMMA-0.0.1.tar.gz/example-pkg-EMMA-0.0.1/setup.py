bdf9651bd5261fd16550d77c40ab99bc9bb94c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:14 +0000

    Add package example-pkg-EMMA/0.0.1/example-pkg-EMMA-0.0.1.tar.gz

---
+++ packages/example-pkg-EMMA/example-pkg-EMMA-0.0.1.tar.gz/example-pkg-EMMA-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-EMMA",
+    version="0.0.1",
+    author="Emma",
+    author_email="author@example.com",
+    description="package to get urls from config file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
