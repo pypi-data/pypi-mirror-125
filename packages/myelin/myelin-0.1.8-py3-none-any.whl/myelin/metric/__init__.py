4f8f3915c1fbd25c7019d91c4ddca63ca6c6f88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:12 +0000

    Add package myelin/0.1.8/myelin-0.1.8-py3-none-any.whl

---
+++ packages/myelin/myelin-0.1.8-py3-none-any.whl/myelin/metric/__init__.py
@@ -0,0 +1,22 @@
+from .metric_client import MetricClient
+import os
+import myelin.hpo
+__all__ = ['MetricClient']
+
+__MYELIN_CLIENT__ = MetricClient()
+
+
+def get_loss_metric():
+    return os.environ["LOSS_METRIC"]
+
+
+def post_hpo_result(config_id, config, budget, loss_metric, loss, tags, info_map):
+    __MYELIN_CLIENT__.post_hpo_result(config_id, config, budget, loss_metric, loss, tags, info_map)
+
+
+def publish_result(loss_value, loss_metric, tags=None):
+    if tags is None:
+        tags = {}
+    __MYELIN_CLIENT__.post_update(loss_metric, loss_value)
+    if myelin.hpo.hpo_enabled():
+        myelin.hpo.publish_result(loss_metric, loss_value, tags)
