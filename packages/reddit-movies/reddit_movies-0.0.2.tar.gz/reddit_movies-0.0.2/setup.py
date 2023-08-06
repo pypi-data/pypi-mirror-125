be0daee4f26d51c6be51345eab7f2b0acbaa5b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:05 +0000

    Add package reddit-movies/0.0.2/reddit_movies-0.0.2.tar.gz

---
+++ packages/reddit-movies/reddit_movies-0.0.2.tar.gz/reddit_movies-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='reddit_movies',
+    version='0.0.2',
+    packages=['reddit_movies'],
+    url='',
+    license='apache2',
+    author='jarbas',
+    author_email='jarbasai@mailfence.com',
+    description='',
+    install_requires=[
+        "json_database", "feedparser", "beautifulsoup4"
+    ]
+)
