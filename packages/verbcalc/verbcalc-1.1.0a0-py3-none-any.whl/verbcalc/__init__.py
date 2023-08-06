0e9f23b0cbf7029fc583e4e66a54779da2f7c4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:29 +0000

    Add package verbcalc/1.1.0a0/verbcalc-1.1.0a0-py3-none-any.whl

---
+++ packages/verbcalc/verbcalc-1.1.0a0-py3-none-any.whl/verbcalc/__init__.py
@@ -0,0 +1,7 @@
+"""
+Python library for making calculations from natural language.
+"""
+from verbcalc.core.symbols import Symbols
+from verbcalc.core.translator import translate
+
+__all__ = ['translate', 'Symbols']
