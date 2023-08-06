f96486c3e354c77b5970164df27c8bd7c507cc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:59 +0000

    Add package gethash/4.6/gethash-4.6-py3-none-any.whl

---
+++ packages/gethash/gethash-4.6-py3-none-any.whl/gethash/utils/click.py
@@ -0,0 +1,34 @@
+import sys
+from typing import Any, Optional, Sequence
+
+import click
+
+__all__ = ["Command", "MultiCommand", "Group", "CommandCollection"]
+
+
+class Command(click.Command):
+    def main(self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any) -> Any:
+        if args is None:
+            args = sys.argv[1:]
+        return super().main(args, *pargs, **kwargs)
+
+
+class MultiCommand(click.MultiCommand):
+    def main(self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any) -> Any:
+        if args is None:
+            args = sys.argv[1:]
+        return super().main(args, *pargs, **kwargs)
+
+
+class Group(click.Group):
+    def main(self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any) -> Any:
+        if args is None:
+            args = sys.argv[1:]
+        return super().main(args, *pargs, **kwargs)
+
+
+class CommandCollection(click.CommandCollection):
+    def main(self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any) -> Any:
+        if args is None:
+            args = sys.argv[1:]
+        return super().main(args, *pargs, **kwargs)
