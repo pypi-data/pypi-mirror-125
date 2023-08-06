ecf1661833879cd3c2143ca0b561165b62595a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:15 +0000

    Add package aoku/2.1.2/aoku-2.1.2.tar.gz

---
+++ packages/aoku/aoku-2.1.2.tar.gz/aoku-2.1.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+requirements = [
+	"discord",
+	"discord.py[voice]",
+	"discord_components",
+	"youtube_dl",
+	"datetime",
+	"requests"
+]
+
+setuptools.setup(
+	name = "aoku",
+	version = "2.1.2",
+	author = "AineD3V",
+	author_email = "priskozena@gmail.com",
+	description = "The package for writing your own discord bot",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://aokuapi.herokuapp.com/",
+	packages = setuptools.find_packages(),
+	install_requires = requirements,
+	classifiers=[
+		"Programming Language :: Python :: 3.8",
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
