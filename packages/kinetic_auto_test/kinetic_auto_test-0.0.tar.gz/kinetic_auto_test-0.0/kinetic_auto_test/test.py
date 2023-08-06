f976d0b962990ac620f6484b691b9325012ae0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:10 +0000

    Add package kinetic_auto_test/0.0/kinetic_auto_test-0.0.tar.gz

---
+++ packages/kinetic_auto_test/kinetic_auto_test-0.0.tar.gz/kinetic_auto_test-0.0/kinetic_auto_test/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# Author: Srikanth Malla
+
+class Test:
+  def __init__(self, name):
+    self.name = name
+  def run(self):
+    print(self.name)
---
+++ packages/kinetic_auto_test/kinetic_auto_test-0.0.tar.gz/kinetic_auto_test-0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kinetic_auto_test',
+      version='0.0',
+      description='testing package for kinetic',
+      author='Srikanth Malla',
+      author_email='srikanth@kinetic.auto',
+      url='http://www.kinetic.auto//',
+      packages=['kinetic_auto_test'],
+     )
