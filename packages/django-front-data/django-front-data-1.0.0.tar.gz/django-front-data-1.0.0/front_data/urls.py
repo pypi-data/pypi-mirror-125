023dfbe75b0fb816cc90f669bb1599b46e068aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:26 +0000

    Add package django-front-data/1.0.0/django-front-data-1.0.0.tar.gz

---
+++ packages/django-front-data/django-front-data-1.0.0.tar.gz/django-front-data-1.0.0/front_data/urls.py
@@ -0,0 +1,17 @@
+try:
+    from django.urls import include, path
+    from rest_framework.routers import DefaultRouter
+
+    from . import views
+
+    router = DefaultRouter()
+    router.register(r'faqs', views.FAQViewSet, 'faqs')
+    router.register(r'front-data', views.FrontDataViewSet, 'front-data')
+    router.register(r'front-data-std', views.StdFrontDataViewSet, 'front-data-std')
+    router.register(r'front-data-full', views.FullFrontDataViewSet, 'front-data-full')
+
+    urlpatterns = [
+        path('', include(router.urls)),
+    ]
+except ModuleNotFoundError:
+    urlpatterns = []
