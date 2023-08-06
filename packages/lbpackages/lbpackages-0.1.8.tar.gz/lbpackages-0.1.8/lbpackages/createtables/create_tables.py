44fe0c20ead8a6bd4ea620bba06ade03fa471fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:13 +0000

    Add package lbpackages/0.1.8/lbpackages-0.1.8.tar.gz

---
+++ packages/lbpackages/lbpackages-0.1.8.tar.gz/lbpackages-0.1.8/lbpackages/createtables/create_tables.py
@@ -0,0 +1,13 @@
+"""Script to create DB tables."""
+from lbpackages.models.stocks import Base
+from lbpackages.models.stocks import create_engine, sessionmaker
+
+
+def create_tables():
+    """Program entrypoint."""
+    #engine = create_engine(conn_str)
+    engine = create_engine('postgresql://user:user@stock-data-postgres:5432/stocks')
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+    
