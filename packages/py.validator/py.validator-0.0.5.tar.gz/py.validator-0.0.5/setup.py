9c273a29e088d4dc5c5eda5dc9ac619fe6b671e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:54 +0000

    Add package py.validator/0.0.5/py.validator-0.0.5.tar.gz

---
+++ packages/py.validator/py.validator-0.0.5.tar.gz/py.validator-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+VERSION = '0.0.5'
+DESCRIPTION = 'String validation and sanitization'
+LONG_DESCRIPTION = 'Package that allows for validate and sanitize of string values. For example to check if a string is an ascii string etc.'
+
+# Setting up
+setup(
+    name="py.validator",
+    version=VERSION,
+    author="Sanel",
+    author_email="sanelbgd@hotmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=['validator'],
+    install_requires=[],
+    keywords=['python', 'validation', 'sanitization', 'javascript'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    license='MIT'
+)
