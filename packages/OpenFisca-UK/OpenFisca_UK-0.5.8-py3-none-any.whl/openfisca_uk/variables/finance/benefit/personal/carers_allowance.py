ba580f3c40dbeb7c2a8de73feac1b1d37ec846e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:08 +0000

    Add package OpenFisca-UK/0.5.8/OpenFisca_UK-0.5.8-py3-none-any.whl

---
+++ packages/OpenFisca-UK/OpenFisca_UK-0.5.8-py3-none-any.whl/openfisca_uk/variables/finance/benefit/personal/carers_allowance.py
@@ -0,0 +1,19 @@
+from openfisca_uk.tools.general import *
+from openfisca_uk.entities import *
+
+
+class carers_allowance(Variable):
+    value_type = float
+    entity = Person
+    label = u"Carer's Allowance"
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        return person("carers_allowance_reported", period)
+
+
+class carers_allowance_reported(Variable):
+    value_type = float
+    entity = Person
+    label = u"Carer's Allowance (reported)"
+    definition_period = YEAR
