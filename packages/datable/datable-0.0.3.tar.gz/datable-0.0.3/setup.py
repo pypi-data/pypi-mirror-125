66564594321aeec1f12518d9ee24c3db0397ed2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:03 +0000

    Add package datable/0.0.3/datable-0.0.3.tar.gz

---
+++ packages/datable/datable-0.0.3.tar.gz/datable-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import os
+#pkgs=list(filter(None, [x[0].replace('{}/src/'.format(os.getcwd()), '').replace('/','.') if '.git' not in x[0] else '' for x in os.walk('{}/src/'.format(os.getcwd()))]))
+setup(
+    name='datable',
+    version='0.0.3',
+    license='MIT',
+    author="Ziplux LHS",
+    author_email='ziplux.so@ziplux.so',
+    packages=['datable'],
+    url='https://github.com/ZipluxLhs/datable',
+    keywords='data',
+    install_requires=[
+           'pip',
+	  'setuptools',
+      ],
+
+)
