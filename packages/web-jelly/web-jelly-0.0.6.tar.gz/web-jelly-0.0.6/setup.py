4f697de40bb86a3d1624c2eb6ee8ea4d66bdbb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:03 +0000

    Add package web-jelly/0.0.6/web-jelly-0.0.6.tar.gz

---
+++ packages/web-jelly/web-jelly-0.0.6.tar.gz/web-jelly-0.0.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="web-jelly",
+    version="0.0.6",
+    author="Jellyfish",
+    author_email="hello@web-jelly.com",
+    description="web-jelly.com python client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://web-jelly.com",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/hello590/jelly-python-client/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'paho-mqtt'
+    ],
+    python_requires=">=3.6",
+)
