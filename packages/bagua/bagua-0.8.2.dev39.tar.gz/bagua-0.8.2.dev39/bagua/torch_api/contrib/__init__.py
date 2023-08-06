df153931e9096300124ae27f6a6c2e1728cb3e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:47 +0000

    Add package bagua/0.8.2.dev39/bagua-0.8.2.dev39.tar.gz

---
+++ packages/bagua/bagua-0.8.2.dev39.tar.gz/bagua-0.8.2.dev39/bagua/torch_api/contrib/__init__.py
@@ -0,0 +1,7 @@
+from .fused_optimizer import FusedOptimizer  # noqa: F401
+from .load_balancing_data_loader import (  # noqa: F401
+    LoadBalancingDistributedSampler,
+    LoadBalancingDistributedBatchSampler,
+)
+from .cache_loader import CacheLoader  # noqa: F401
+from .cached_dataset import CachedDataset  # noqa: F401
