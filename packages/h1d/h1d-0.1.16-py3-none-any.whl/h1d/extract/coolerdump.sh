0a654785307124220aa379916eeb57096940a23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:13 +0000

    Add package h1d/0.1.16/h1d-0.1.16-py3-none-any.whl

---
+++ packages/h1d/h1d-0.1.16-py3-none-any.whl/h1d/extract/coolerdump.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cool=$1
+binsize=$2
+chr=$3
+gt=$4
+outname=$5
+
+pwd=$(cd $(dirname $0) && pwd)
+
+echo Start dump matrix
+chrnum=$(echo $chr |sed 's/chr//g')
+cooler dump -r $chr --join $cool | cut -f 2,5,7  > sparse.temp || cooler dump -r $chrnum --join $cool | cut -f 2,5,7 > sparse.temp
+echo Finish dump matrix
+
+mkdir -p $outname/${binsize}
+
+outdir=$outname/${binsize}/${chr}.matrix.gz
+
+$pwd/convert_JuicerDump_to_dense.py sparse.temp $outdir $gt $chr $binsize
+
+rm sparse.temp
