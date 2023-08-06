cc61d2ee0c95af9a994eb19d2b99e650510230e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:29 +0000

    Add package netbox-interface-sync/0.2.0/netbox-interface-sync-0.2.0.tar.gz

---
+++ packages/netbox-interface-sync/netbox-interface-sync-0.2.0.tar.gz/netbox-interface-sync-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='netbox-interface-sync',
+    version='0.2.0',
+    description='Syncing interfaces with the interfaces from device type for NetBox devices',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Victor Golovanenko',
+    author_email='drygdryg2014@yandex.com',
+    license='GPL-3.0',
+    install_requires=[],
+    packages=["netbox_interface_sync"],
+    package_data={"netbox_interface_sync": ["templates/netbox_interface_sync/*.html"]},
+    zip_safe=False
+)
