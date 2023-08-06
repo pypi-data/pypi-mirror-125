ae6dd133c59f467575fde23c9fa4cd46e5226c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:17 +0000

    Add package robyn/0.7.1/robyn-0.7.1-cp38-cp38-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/robyn/robyn-0.7.1-cp38-cp38-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/robyn/__init__.py
@@ -0,0 +1,179 @@
+# default imports
+import os
+import argparse
+import asyncio
+from inspect import signature
+
+from .robyn import Server
+from .responses import static_file, jsonify
+from .dev_event_handler import EventHandler
+from .log_colors import Colors
+
+
+from watchdog.observers import Observer
+
+
+
+class Robyn:
+    """This is the python wrapper for the Robyn binaries.
+    """
+    def __init__(self, file_object):
+        directory_path = os.path.dirname(os.path.abspath(file_object))
+        self.file_path = file_object
+        self.directory_path = directory_path
+        self.server = Server(directory_path)
+        self.dev = self._is_dev()
+
+    def _is_dev(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--dev', default=False, type=lambda x: (str(x).lower() == 'true'))
+        return parser.parse_args().dev
+
+
+    def add_route(self, route_type, endpoint, handler):
+        """
+        [This is base handler for all the decorators]
+
+        :param route_type [str]: [route type between GET/POST/PUT/DELETE/PATCH]
+        :param endpoint [str]: [endpoint for the route added]
+        :param handler [function]: [represents the sync or async function passed as a handler for the route]
+        """
+
+        """ We will add the status code here only
+        """
+        number_of_params = len(signature(handler).parameters)
+        self.server.add_route(
+            route_type, endpoint, handler, asyncio.iscoroutinefunction(handler), number_of_params
+        )
+
+    def add_directory(self, route, directory_path, index_file=None, show_files_listing=False):
+        self.server.add_directory(route, directory_path, index_file, show_files_listing)
+
+    def add_header(self, key, value):
+        self.server.add_header(key, value)
+
+    def remove_header(self, key):
+        self.server.remove_header(key)
+    
+    def start(self, url="127.0.0.1", port=5000):
+        """
+        [Starts the server]
+
+        :param port [int]: [reperesents the port number at which the server is listening]
+        """
+        if not self.dev:
+            self.server.start(url, port)
+        else:
+            event_handler = EventHandler(self.file_path)
+            event_handler.start_server_first_time()
+            print(f"{Colors.OKBLUE}Dev server initialised with the directory_path : {self.directory_path}{Colors.ENDC}")
+            observer = Observer()
+            observer.schedule(event_handler, path=self.directory_path, recursive=True)
+            observer.start()
+            try:
+                while True:
+                    pass
+            finally:
+                observer.stop()
+                observer.join()
+
+    def get(self, endpoint):
+        """
+        [The @app.get decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("GET", endpoint, handler)
+
+        return inner
+
+    def post(self, endpoint):
+        """
+        [The @app.post decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("POST", endpoint, handler)
+
+        return inner
+
+    def put(self, endpoint):
+        """
+        [The @app.put decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("PUT", endpoint, handler)
+
+        return inner
+
+    def delete(self, endpoint):
+        """
+        [The @app.delete decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("DELETE", endpoint, handler)
+
+        return inner
+
+    def patch(self, endpoint):
+        """
+        [The @app.patch decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("PATCH", endpoint, handler)
+           
+        return inner
+
+    def head(self, endpoint):
+        """
+        [The @app.head decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("HEAD", endpoint, handler)
+
+        return inner
+
+    def options(self, endpoint):
+        """
+        [The @app.options decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("OPTIONS", endpoint, handler)
+
+        return inner
+
+
+    def connect(self, endpoint):
+        """
+        [The @app.connect decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("CONNECT", endpoint, handler)
+
+        return inner
+
+    def trace(self, endpoint):
+        """
+        [The @app.trace decorator to add a get route]
+
+        :param endpoint [str]: [endpoint to server the route]
+        """
+        def inner(handler):
+            self.add_route("TRACE", endpoint, handler)
+
+        return inner
+
