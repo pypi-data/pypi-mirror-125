01c8e09ff08e382773fb1c5d4659958f3f14dc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:45 +0000

    Add package orbitkit/0.1.0/orbitkit-0.1.0.tar.gz

---
+++ packages/orbitkit/orbitkit-0.1.0.tar.gz/orbitkit-0.1.0/orbitkit/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from orbitkit import util
+from orbitkit.file_extractor.dispatcher import FileDispatcher
+
+name = 'orbitkit'
+
+__version__ = '0.1.0'
+VERSION = __version__
+
+__all__ = [
+    'util',
+    'FileDispatcher',
+]
