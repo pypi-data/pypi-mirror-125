8c4e2bfdbf5d7c2fc34f59275c648230934dd0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:51 +0000

    Add package gsdl/0.0.1/gsdl-0.0.1.tar.gz

---
+++ packages/gsdl/gsdl-0.0.1.tar.gz/gsdl-0.0.1/gsdl/__init__.py
@@ -0,0 +1,9 @@
+from . import hellomsg
+import pygame
+from .fps_clock import FPS
+from .utils import *
+from .frame import GFrame
+from .image import GImage
+from .label import GLabel
+
+del hellomsg
