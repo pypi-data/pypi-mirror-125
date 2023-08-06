ce0d06ceb56f0f7c7763b8201676d88ea46efe9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:27 +0000

    Add package Flask-Express/0.1.3/Flask_Express-0.1.3-py3-none-any.whl

---
+++ packages/Flask-Express/Flask_Express-0.1.3-py3-none-any.whl/flask_express/request.py
@@ -0,0 +1,52 @@
+from flask.wrappers import Request as RequestBase
+from flask.globals import session
+import typing as t
+from .munch import Munch
+
+
+if t.TYPE_CHECKING:
+    from munch import Munch
+    from flask.sessions import SessionMixin
+
+class Request(RequestBase):
+    """
+    The default response class for the flask-express app.
+    """
+    def __init__(self, *wargs, **kwargs) -> None:
+        super(Request, self).__init__(*wargs, **kwargs)
+
+    @property
+    def json(self) -> t.Type["Munch"]:
+        """
+        it provides you the json based data. 
+        """
+        return Munch(self.get_json())
+
+    @property
+    def query(self) -> t.Type["Munch"]:
+        """
+        it provides you the args based data. 
+        """
+        return Munch(self.args)
+
+    @property
+    def body(self) -> t.Type["Munch"]:
+        """
+        it provides you the form based data. 
+        """
+        return Munch(self.form)
+
+    @property
+    def header(self) -> t.Type["Munch"]:
+        """
+        it provides you the headers based data. 
+        """
+        return Munch(self.headers)
+
+    @property
+    def session(self) -> t.Type["SessionMixin"]:
+        """
+        it provides you the default session object of flask 
+        globals as a property of `request.Request` class.
+        """
+        return session
