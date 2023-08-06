5bda8f6f7fc60b4d6363053fec65f9675029165b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:54 +0000

    Add package getDynamicUrl/0.0.1/getDynamicUrl-0.0.1.tar.gz

---
+++ packages/getDynamicUrl/getDynamicUrl-0.0.1.tar.gz/getDynamicUrl-0.0.1/getDynamicUrl/__init__.py
@@ -0,0 +1,36 @@
+from google.cloud import bigquery
+
+db = bigquery.Client()
+
+
+def get_url(headers, query):
+    """Check the appoimentID from the url
+    Match with the database to validate if exist
+
+    Args:
+        headers ([type]): [Headers Objec]
+
+    Returns:
+        url [string]: [url validate]
+    """
+
+    url_origin = headers["Origin"]
+    try:
+        appointment_id = url_origin.split("-")[2]
+    except IndexError:
+        appointment_id = ""
+
+    # Set the name to None to use positional parameters.
+    # Note that you cannot mix named and positional parameters.
+    # Based in cloud bigquery documentation.
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter(
+                "appointment_id", "STRING", appointment_id
+            )
+        ]
+    )
+    df = db.query(query["query"], job_config=job_config).to_dataframe()
+    url = df.query["field"][0] if not df.empty else ""
+
+    return url
