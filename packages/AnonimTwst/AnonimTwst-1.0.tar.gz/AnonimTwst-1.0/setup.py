e54c774320553c25eae363bbd82527bb03a680b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:45 +0000

    Add package AnonimTwst/1.0/AnonimTwst-1.0.tar.gz

---
+++ packages/AnonimTwst/AnonimTwst-1.0.tar.gz/AnonimTwst-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='AnonimTwst',
+	version='1.0',
+	author='Anonim',
+	author_email='yilegeb242@dukeoo.com',
+	description='',
+	packages=['AnonimTest'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
