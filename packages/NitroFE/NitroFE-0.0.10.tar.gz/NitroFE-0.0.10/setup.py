9374c5df8c866846f5735e10a3fdb79029abd977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:50 +0000

    Add package NitroFE/0.0.10/NitroFE-0.0.10.tar.gz

---
+++ packages/NitroFE/NitroFE-0.0.10.tar.gz/NitroFE-0.0.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(name='NitroFE',
+      version='0.0.10',
+      url='https://github.com/NITRO-AI/NitroFE',
+      author='Nitro-AI',
+      author_email='nitro.ai.solutions@gmail.com',
+      license='Apache License 2.0',
+      packages=['NitroFE','NitroFE.encoding.','NitroFE.time_based_features','NitroFE.time_based_features.indicator_features','NitroFE.time_based_features.moving_average_features','NitroFE.time_based_features.weighted_window_features'],
+      zip_safe=True,
+	description="NitroFE is a Python feature engineering engine which provides a variety of feature engineering modules designed to handle continous calcualtion.",
+      long_description=long_description  ,
+	long_description_content_type='text/markdown',
+	  install_requires=[
+		"pandas>=1.3.2",
+		"numpy>=1.21.2",
+		"scipy",
+            "plotly"
+		],
+      )
