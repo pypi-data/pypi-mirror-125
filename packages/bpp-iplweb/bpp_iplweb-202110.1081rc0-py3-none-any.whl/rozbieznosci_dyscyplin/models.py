f6c163bc693bb1fb58db2164e0de503ed0e7e4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:52 +0000

    Add package bpp-iplweb/202110.1081rc0/bpp_iplweb-202110.1081rc0-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202110.1081rc0-py3-none-any.whl/rozbieznosci_dyscyplin/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.db.models import DO_NOTHING
+
+from bpp.fields import YearField
+from bpp.models import BazaModeluOdpowiedzialnosciAutorow, TupleField
+
+
+class RozbieznosciViewBase(models.Model):
+    id = TupleField(models.IntegerField(), size=3, primary_key=True)
+    rekord = models.ForeignKey("bpp.Rekord", DO_NOTHING, related_name="+")
+    rok = YearField()
+    autor = models.ForeignKey("bpp.Autor", DO_NOTHING, related_name="+")
+    dyscyplina_rekordu = models.ForeignKey(
+        "bpp.Dyscyplina_Naukowa", DO_NOTHING, related_name="+", null=True, blank=True
+    )
+    dyscyplina_autora = models.ForeignKey(
+        "bpp.Dyscyplina_Naukowa", DO_NOTHING, related_name="+"
+    )
+    subdyscyplina_autora = models.ForeignKey(
+        "bpp.Dyscyplina_Naukowa", DO_NOTHING, related_name="+", null=True, blank=True
+    )
+
+    class Meta:
+        managed = False
+        abstract = True
+
+
+class BrakPrzypisaniaView(RozbieznosciViewBase):
+    class Meta:
+        managed = False
+
+
+class RozbieznePrzypisaniaView(RozbieznosciViewBase):
+    class Meta:
+        managed = False
+
+
+class RozbieznosciView(RozbieznosciViewBase):
+    # Uwaga: w sytuacji, gdy praca będzie miała jednego i tego samego autora (np w roli redaoktora
+    # oraz autora) to ten model i funkcja get_wydawnictwo_autor_obj zawiedzie.
+    class Meta:
+        managed = False
+        verbose_name = "rozbieżność rekordu i dyscyplin"
+        verbose_name_plural = "rozbieżności rekordów i dyscyplin"
+
+    def get_wydawnictwo_autor_obj(self) -> BazaModeluOdpowiedzialnosciAutorow:
+        # Uwaga: w sytuacji, gdy praca będzie miała jednego i tego samego autora (np w roli redaoktora
+        # oraz autora) to ten model i funkcja get_wydawnictwo_autor_obj zawiedzie (zwraca wyłacznie pierwszy
+        # rekord z powiazaniem autora + rekordu)
+        return self.rekord.original.autorzy_set.filter(autor=self.autor).first()
