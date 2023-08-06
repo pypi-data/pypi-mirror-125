8ffbf4d6ea832a3a583ccd6431120a93fc4e2b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:41 +0000

    Add package muffin-rest/4.0.0/muffin_rest-4.0.0-py3-none-any.whl

---
+++ packages/muffin-rest/muffin_rest-4.0.0-py3-none-any.whl/muffin_rest/__init__.py
@@ -0,0 +1,49 @@
+"""REST helpers for Muffin Framework."""
+
+__version__ = "4.0.0"
+__project__ = "muffin-rest"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+# Default query params
+FILTERS_PARAM = 'where'
+LIMIT_PARAM = 'limit'
+OFFSET_PARAM = 'offset'
+SORT_PARAM = 'sort'
+
+
+from .api import API                # noqa
+from .handler import RESTHandler    # noqa
+from .errors import APIError        # noqa
+
+# Just an alias to support legacy style
+Api = API
+
+
+# Support Peewee ORM
+try:
+    from .peewee import PWRESTHandler
+    from .peewee.filters import PWFilter, PWFilters
+    from .peewee.sorting import PWSort, PWSorting
+except ImportError as exc:
+    pass
+
+
+# Support SQLAlchemy ORM
+try:
+    from .sqlalchemy import SARESTHandler
+    from .sqlalchemy.filters import SAFilter, SAFilters
+    from .sqlalchemy.sorting import SASort, SASorting
+except ImportError:
+    pass
+
+
+# Support Mongo ORM
+try:
+    from .mongo import MongoRESTHandler
+    from .mongo.filters import MongoFilter, MongoFilters
+    from .mongo.sorting import MongoSort, MongoSorting
+except ImportError:
+    pass
+
+# pylama:ignore=W0611
