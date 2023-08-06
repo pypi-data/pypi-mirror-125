13a874ad03753699a87c953422c2a1d173308eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:38 +0000

    Add package spock-config/2.2.6/spock_config-2.2.6-py3-none-any.whl

---
+++ packages/spock-config/spock_config-2.2.6-py3-none-any.whl/spock/args.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Copyright FMR LLC <opensource@fidelity.com>
+# SPDX-License-Identifier: Apache-2.0
+
+"""Handles import aliases to allow backwards compat with backends"""
+
+# from spock.backend.dataclass.args import *
+from spock.backend.typed import SavePath
