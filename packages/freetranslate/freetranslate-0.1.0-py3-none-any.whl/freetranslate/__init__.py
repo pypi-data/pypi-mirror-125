676991ed0a7b7d88a5c7a428444b6dd710f9dd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:10 +0000

    Add package freetranslate/0.1.0/freetranslate-0.1.0-py3-none-any.whl

---
+++ packages/freetranslate/freetranslate-0.1.0-py3-none-any.whl/freetranslate/__init__.py
@@ -0,0 +1,7 @@
+"""
+Another translate API for Python.
+"""
+from . import *
+
+__version__ = "0.1.0"
+__author__ = "tretrauit"
---
+++ packages/freetranslate/freetranslate-0.1.0-py3-none-any.whl/freetranslate/translateresult.py
@@ -0,0 +1,20 @@
+class TranslateResult():
+    """
+    This class contain translation result.
+    @translated_text: Translated text.
+    @original_text: Original text to translate.
+    @original_lang: Translated text language.
+    @endpoint: Endpoint used to translate.
+    @raw: Raw result returned from endpoint (sometimes deobfuscated).
+    @raw_obfuscated: Raw obfuscated result returned from endpoint if @raw is deobfuscated).
+    """
+    def __init__(self, translated_text, original_text, original_lang, endpoint, raw, 
+    raw_obfuscated = None, interjection_original = None, interjection_translated = None):
+        self.translated_text = translated_text
+        self.original_text = original_text
+        self.original_lang = original_lang
+        self.endpoint = endpoint
+        self.interjection_original = interjection_original
+        self.interjection_translated = interjection_translated
+        self.raw = raw
+        self.raw_obfuscated = raw_obfuscated
