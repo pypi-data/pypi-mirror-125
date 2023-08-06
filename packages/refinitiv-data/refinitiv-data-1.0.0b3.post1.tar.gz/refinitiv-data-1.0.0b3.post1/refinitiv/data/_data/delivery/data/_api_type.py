b111d673041215892d1b1d86cd861dc40b4a86b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:55 +0000

    Add package refinitiv-data/1.0.0b3.post1/refinitiv-data-1.0.0b3.post1.tar.gz

---
+++ packages/refinitiv-data/refinitiv-data-1.0.0b3.post1.tar.gz/refinitiv-data-1.0.0b3.post1/refinitiv/data/_data/delivery/data/_api_type.py
@@ -0,0 +1,12 @@
+from enum import Enum, auto
+
+
+class APIType(Enum):
+    CFS = auto()
+    FINANCIAL_CONTRACTS = auto()
+    CURVES_AND_SURFACES = auto()
+    HISTORICAL_PRICING = auto()
+    ESG = auto()
+    PRICING = auto()
+    OWNERSHIP = auto()
+    CHAINS = auto()
