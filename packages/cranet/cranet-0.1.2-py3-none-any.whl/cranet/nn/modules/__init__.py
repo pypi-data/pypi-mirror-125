bb83e14a008b85490c672600cf61d596f8c851b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:27 +0000

    Add package cranet/0.1.2/cranet-0.1.2-py3-none-any.whl

---
+++ packages/cranet/cranet-0.1.2-py3-none-any.whl/cranet/nn/modules/__init__.py
@@ -0,0 +1,33 @@
+from .activation import (
+    ReLU,
+    Sigmoid,
+    Tanh,
+    Softmax,
+)
+from .container import (
+    Sequential
+)
+from .conv import (
+    Conv2d
+)
+from .dropout import (
+    Dropout
+)
+from .flatten import (
+    Flatten
+)
+from .linear import (
+    Linear
+)
+from .loss import (
+    MSELoss,
+    L1Loss,
+    CrossEntropyLoss,
+    NLLLoss,
+)
+from .module import (
+    Module
+)
+from .pooling import (
+    MaxPool2d
+)
