8bf340a9a2280135b73d2a485255f9e158c5fd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:04 +0000

    Add package zoidberg/0.1.1/zoidberg-0.1.1.tar.gz

---
+++ packages/zoidberg/zoidberg-0.1.1.tar.gz/zoidberg-0.1.1/zoidberg/__init__.py
@@ -0,0 +1,14 @@
+import zoidberg
+
+from . import field, fieldtracer, grid, plot
+from .zoidberg import make_maps, write_maps
+
+__all__ = [
+    "field",
+    "fieldtracer",
+    "grid",
+    "make_maps",
+    "plot",
+    "write_maps",
+    "zoidberg",
+]
