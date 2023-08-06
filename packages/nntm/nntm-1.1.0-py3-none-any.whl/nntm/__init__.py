0d450d2618b0d77c68e8cb81606a0d310446c675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:21 +0000

    Add package nntm/1.1.0/nntm-1.1.0-py3-none-any.whl

---
+++ packages/nntm/nntm-1.1.0-py3-none-any.whl/nntm/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+__version__ = "1.1.0"
+
+__all__ = [
+    "datasets",
+    "model_selection",
+]
---
+++ packages/nntm/nntm-1.1.0-py3-none-any.whl/nntm/datasets/__init__.py
@@ -0,0 +1,21 @@
+from ._numerai_main import (
+    fetch_numerai_training,
+    fetch_numerai_test,
+    fetch_numerai_validation,
+    fetch_numerai_live,
+    fetch_numerai_tournament,
+    fetch_numerai_example_predictions,
+    fetch_numerai_example_validation_predictions,
+    fetch_numerai_feature_metadata,
+)
+
+__all__ = [
+    "fetch_numerai_training",
+    "fetch_numerai_test",
+    "fetch_numerai_validation",
+    "fetch_numerai_live",
+    "fetch_numerai_tournament",
+    "fetch_numerai_example_predictions",
+    "fetch_numerai_example_validation_predictions",
+    "fetch_numerai_feature_metadata",
+]
