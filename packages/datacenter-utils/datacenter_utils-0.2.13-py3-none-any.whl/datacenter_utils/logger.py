57527efbb64a147cd2a066f9ad3ab07d33c6cbab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:19 +0000

    Add package datacenter-utils/0.2.13/datacenter_utils-0.2.13-py3-none-any.whl

---
+++ packages/datacenter-utils/datacenter_utils-0.2.13-py3-none-any.whl/datacenter_utils/logger.py
@@ -0,0 +1,8 @@
+# -*- encoding:utf-8 -*-
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(filename)s[func:%(funcName)s, line:%(lineno)d] - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger()
