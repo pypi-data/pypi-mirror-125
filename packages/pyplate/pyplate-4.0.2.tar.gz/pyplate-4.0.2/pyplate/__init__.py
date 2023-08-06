3e052fdfe075cebfb5675f4cc8bfdae1dddd8c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:52 +0000

    Add package pyplate/4.0.2/pyplate-4.0.2.tar.gz

---
+++ packages/pyplate/pyplate-4.0.2.tar.gz/pyplate-4.0.2/pyplate/__init__.py
@@ -0,0 +1,8 @@
+from . import database
+from . import metadata
+from . import image
+from . import conf
+from . import process
+from ._version import __version__
+
+__all__ = ['metadata', 'image', 'database', 'conf', 'process']
