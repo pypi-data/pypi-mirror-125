371b1a97e4b892428e77c9dd3d8f8df321b09b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:49 +0000

    Add package aws-lambda-typing/2.6.0/aws_lambda_typing-2.6.0-py3-none-any.whl

---
+++ packages/aws-lambda-typing/aws_lambda_typing-2.6.0-py3-none-any.whl/aws_lambda_typing/responses/__init__.py
@@ -0,0 +1,7 @@
+from .api_gateway_proxy import (
+    APIGatewayProxyResponseV1 as APIGatewayProxyResponseV1,
+)
+from .api_gateway_proxy import (
+    APIGatewayProxyResponseV2 as APIGatewayProxyResponseV2,
+)
+from .s3_batch import S3BatchResponse as S3BatchResponse
