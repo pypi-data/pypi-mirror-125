e060671470ba45e1428b364b7d40e923e7a07b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:41 +0000

    Add package tpdne-py/0.0.4/tpdne-py-0.0.4.tar.gz

---
+++ packages/tpdne-py/tpdne-py-0.0.4.tar.gz/tpdne-py-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+setup(name='tpdne-py',
+    version='0.0.4',
+    description='a very simple wrapper around requests/aiohttp for thispersondoesnotexist.com',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='anytarseir67',
+    author_email = '',
+    url='https://github.com/anytarseir67/tpdne-py',
+    license="GPLv3",
+    packages=['tpdne'],
+    install_requires=requirements,
+    )
