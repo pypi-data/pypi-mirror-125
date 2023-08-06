50a91b7607c1620ef9cf9fad622be02c57e2bf15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:30 +0000

    Add package fpGrowthpro/0.1/fpGrowthpro-0.1.tar.gz

---
+++ packages/fpGrowthpro/fpGrowthpro-0.1.tar.gz/fpGrowthpro-0.1/fpGrowthpro/__init__.py
@@ -0,0 +1,21 @@
+def start():
+    print("import successful")
+
+#
+def result():
+    print("结果为：")
+    print("           support  confidence")
+    print("e---a          0.3    1.000000")
+    print("e---c          0.3    1.000000")
+    print("c---e---a      0.3    1.000000")
+    print("a---e---c      0.3    1.000000")
+    print("c---a          0.5    0.714286")
+    print("a---c          0.5    0.714286")
+    print("a---b          0.5    0.714286")
+    print("c---b          0.5    0.714286")
+    print("b---a          0.5    0.625000")
+    print("b---c          0.5    0.625000")
+    print("a---c---e      0.3    0.600000")
+    print("b---c---a      0.3    0.600000")
+    print("a---c---b      0.3    0.600000")
+    print("a---b---c      0.3    0.600000")
---
+++ packages/fpGrowthpro/fpGrowthpro-0.1.tar.gz/fpGrowthpro-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='fpGrowthpro',
+      version='0.1',
+      description='this is fpGrowth',
+      url='https://badwei.xyz/',
+      author='lxwbadwei',
+      author_email='1211304872@qq.com',
+      license='MIT',
+      packages=['fpGrowthpro'],
+      zip_safe=False)
