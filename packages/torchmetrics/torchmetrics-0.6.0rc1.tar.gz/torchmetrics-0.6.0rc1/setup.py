2bcb43452add33a9526515e91a9f3bb6fde2a583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:07 +0000

    Add package torchmetrics/0.6.0rc1/torchmetrics-0.6.0rc1.tar.gz

---
+++ packages/torchmetrics/torchmetrics-0.6.0rc1.tar.gz/torchmetrics-0.6.0rc1/setup.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+import os
+from importlib.util import module_from_spec, spec_from_file_location
+
+from setuptools import find_packages, setup
+
+_PATH_ROOT = os.path.realpath(os.path.dirname(__file__))
+_PATH_REQUIRE = os.path.join(_PATH_ROOT, "requirements")
+
+
+def _load_py_module(fname, pkg="torchmetrics"):
+    spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_ROOT, pkg, fname))
+    py = module_from_spec(spec)
+    spec.loader.exec_module(py)
+    return py
+
+
+about = _load_py_module("__about__.py")
+setup_tools = _load_py_module("setup_tools.py")
+long_description = setup_tools._load_readme_description(
+    _PATH_ROOT,
+    homepage=about.__homepage__,
+    version=f"v{about.__version__}",
+)
+
+
+def _prepare_extras():
+    extras = {
+        "image": setup_tools._load_requirements(path_dir=_PATH_REQUIRE, file_name="image.txt"),  # skipcq: PYL-W0212
+        "text": setup_tools._load_requirements(path_dir=_PATH_REQUIRE, file_name="text.txt"),  # skipcq: PYL-W0212
+        "detection": setup_tools._load_requirements(  # skipcq: PYL-W0212
+            path_dir=_PATH_REQUIRE, file_name="detection.txt"
+        ),
+        "audio": setup_tools._load_requirements(path_dir=_PATH_REQUIRE, file_name="audio.txt"),
+    }
+    # create an 'all' keyword that install all possible denpendencies
+    extras["all"] = [package for extra in extras.values() for package in extra]
+
+    return extras
+
+
+# https://packaging.python.org/discussions/install-requires-vs-requirements /
+# keep the meta-data here for simplicity in reading this file... it's not obvious
+# what happens and to non-engineers they won't know to look in init ...
+# the goal of the project is simplicity for researchers, don't want to add too much
+# engineer specific practices
+setup(
+    name="torchmetrics",
+    version=about.__version__,
+    description=about.__docs__,
+    author=about.__author__,
+    author_email=about.__author_email__,
+    url=about.__homepage__,
+    download_url=os.path.join(about.__homepage__, "archive", "master.zip"),
+    license=about.__license__,
+    packages=find_packages(exclude=["tests", "tests.*", "docs"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
+    keywords=["deep learning", "machine learning", "pytorch", "metrics", "AI"],
+    python_requires=">=3.6",
+    setup_requires=[],
+    install_requires=setup_tools._load_requirements(_PATH_ROOT),
+    project_urls={
+        "Bug Tracker": os.path.join(about.__homepage__, "issues"),
+        "Documentation": "https://torchmetrics.rtfd.io/en/latest/",
+        "Source Code": about.__homepage__,
+    },
+    classifiers=[
+        "Environment :: Console",
+        "Natural Language :: English",
+        # How mature is this project? Common values are
+        #   3 - Alpha, 4 - Beta, 5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        # Pick your license as you wish
+        # 'License :: OSI Approved :: BSD License',
+        "Operating System :: OS Independent",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    extras_require=_prepare_extras(),
+)
