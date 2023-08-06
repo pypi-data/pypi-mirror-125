3fa9c0299801298ff10e29f59a604bf0c42da9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:34 +0000

    Add package pb2df/1.0.0/pb2df-1.0.0.tar.gz

---
+++ packages/pb2df/pb2df-1.0.0.tar.gz/pb2df-1.0.0/README.md
@@ -0,0 +1,32 @@
+# Pb2DF
+
+A python module for converting Protobuf(3) Message type/object to Spark DataFrame object.
+
+## Features
+
+- [x] proto3 type to spark schema
+- [x] proto3 object to spark row object
+- [ ] proto3 object to spark dataframe
+
+## Install
+
+```bash
+pip install pb2df
+```
+
+## Usage
+
+In main.py file you would see an example usage for the package
+
+## Test
+
+```bash
+make test
+```
+
+## Acknowledgements
+
+This project is inspired by the following projects:
+
+- [pb2df](https://github.com/bridgewell/pb2df)
+- [protobuf-dataframe](https://github.com/aroch/protobuf-dataframe)
---
+++ packages/pb2df/pb2df-1.0.0.tar.gz/pb2df-1.0.0/setup.py
@@ -0,0 +1,53 @@
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+import pb2df
+
+requirements = ["protobuf>=3.13.0", "pyspark>=3.0.0"]
+setup_requirements = [
+    "pytest-runner",
+]
+test_requirements = [
+    "pytest>=6",
+]
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.pytest_args = []
+
+    def finalize_options(self):
+        super().finalize_options()
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name="pb2df",
+    description="A python module for converting proto3 type/object to spark dataframe object.",
+    long_description=__doc__,
+    version=pb2df.__version__,
+    author=pb2df.__author__,
+    author_email=pb2df.__email__,
+    url="https://github.com/zhangxianbing/pb2df",
+    packages=find_packages(),
+    platforms="any",
+    setup_requires=setup_requirements,
+    install_requires=requirements,
+    tests_require=test_requirements,
+    cmdclass={"test": PyTest},
+    test_suite="tests",
+    zip_safe=False,
+)
