bccbb594bd0cd8951d8078c69847fd6bff2fb5cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:35 +0000

    Add package felupe/1.1.0/felupe-1.1.0.tar.gz

---
+++ packages/felupe/felupe-1.1.0.tar.gz/felupe-1.1.0/felupe/__init__.py
@@ -0,0 +1,66 @@
+from .__about__ import __version__
+from . import math
+from . import mesh
+from . import quadrature
+from . import dof
+from . import element
+from . import tools
+from . import constitution
+from . import solve
+from . import region
+
+from .region import Region
+from ._assembly import (
+    IntegralForm,
+    IntegralFormMixed,
+    IntegralFormAxisymmetric,
+)
+from ._field import (
+    Field,
+    FieldAxisymmetric,
+    FieldMixed,
+)
+from .dof import Boundary
+
+try:
+    from .dof import (
+        MultiPointConstraint,
+        MultiPointContact,
+    )
+except:
+    pass
+from .element import (
+    Line,
+    #
+    Quad,
+    ConstantQuad,
+    #
+    ArbitraryOrderLagrange as ArbitraryOrderLagrangeElement,
+    #
+    Hexahedron,
+    ConstantHexahedron,
+    QuadraticHexahedron,
+    TriQuadraticHexahedron,
+    #
+    Triangle,
+    TriangleMINI,
+    QuadraticTriangle,
+    #
+    Tetra,
+    TetraMINI,
+    QuadraticTetra,
+)
+from .quadrature import (
+    GaussLegendre,
+    Triangle as TriangleQuadrature,
+    Tetrahedron as TetrahedronQuadrature,
+)
+from .mesh import (
+    Mesh,
+    Rectangle,
+    Cube,
+)
+
+__all__ = [
+    "__version__",
+]
---
+++ packages/felupe/felupe-1.1.0.tar.gz/felupe-1.1.0/felupe/dof/__init__.py
@@ -0,0 +1,20 @@
+from ._boundary import Boundary
+from ._tools import (
+    get_dof0,
+    get_dof1,
+    partition,
+    extend,
+    apply,
+)
+from ._loadcase import (
+    symmetry,
+    uniaxial,
+)
+
+try:
+    from ._multipoint import (
+        MultiPointConstraint,
+        MultiPointContact,
+    )
+except:
+    pass
