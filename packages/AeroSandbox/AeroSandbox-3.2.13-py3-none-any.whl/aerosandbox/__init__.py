810e5fc5dc547fea7031493d29cc121e4029cbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:40 +0000

    Add package AeroSandbox/3.2.13/AeroSandbox-3.2.13-py3-none-any.whl

---
+++ packages/AeroSandbox/AeroSandbox-3.2.13-py3-none-any.whl/aerosandbox/__init__.py
@@ -0,0 +1,23 @@
+from .common import *
+from .atmosphere import *
+from .aerodynamics import *
+from .dynamics import *
+from .geometry import *
+import aerosandbox.numpy as numpy
+from .modeling import *
+from .optimization import *
+from .performance import *
+from .propulsion import *
+from .structures import *
+
+__version__ = "3.2.13"
+
+
+def docs():
+    """
+    Opens the AeroSandbox documentation.
+    """
+    import webbrowser
+    webbrowser.open_new(
+        "https://github.com/peterdsharpe/AeroSandbox/tree/master/aerosandbox"
+    )  # TODO: make this redirect to a hosted ReadTheDocs, or similar.
