8c359c47b947beddd800ddc3a3f3bd9c187007a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:05 +0000

    Add package arxiv-post/0.5.0/arxiv_post-0.5.0-py3-none-any.whl

---
+++ packages/arxiv-post/arxiv_post-0.5.0-py3-none-any.whl/arxiv_post/constants.py
@@ -0,0 +1,21 @@
+__all__ = [
+    "CATEGORIES",
+    "KEYWORDS",
+    "START_DATE",
+    "END_DATE",
+    "LANGUAGE_FROM",
+    "LANGUAGE_TO",
+    "N_CONCURRENT",
+    "TIMEOUT",
+]
+
+
+# constants
+CATEGORIES = ("astro-ph.*",)
+KEYWORDS = ()
+START_DATE = "3 days ago at midnight in UTC"
+END_DATE = "2 days ago at midnight in UTC"
+LANGUAGE_FROM = "en"
+LANGUAGE_TO = "auto"
+N_CONCURRENT = 5
+TIMEOUT = 30.0
