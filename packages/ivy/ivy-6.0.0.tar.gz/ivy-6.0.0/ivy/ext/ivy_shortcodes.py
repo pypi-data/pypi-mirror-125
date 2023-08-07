d4db40b21202b8d0cd0cee59e514348baf986fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:16 +0000

    Add package ivy/6.0.0/ivy-6.0.0.tar.gz

---
+++ packages/ivy/ivy-6.0.0.tar.gz/ivy-6.0.0/ivy/ext/ivy_shortcodes.py
@@ -0,0 +1,27 @@
+import ivy
+import sys
+
+parser = None
+
+try:
+    import shortcodes
+
+    @ivy.filters.register('node_text')
+    def render(text, node):
+        global parser
+        if parser is None:
+            settings = ivy.site.config.get('shortcode_settings') or {}
+            parser = shortcodes.Parser(**settings)
+
+        try:
+            return parser.parse(text, node)
+        except shortcodes.ShortcodeError as err:
+            msg = "Shortcode Error\n"
+            msg += f">> Node: {node.url}\n"
+            msg += f">> {err.__class__.__name__}: {err}"
+            if (cause := err.__cause__):
+                msg += f"\n>> Cause: {cause.__class__.__name__}: {cause}"
+            sys.exit(msg)
+
+except ImportError:
+    pass
