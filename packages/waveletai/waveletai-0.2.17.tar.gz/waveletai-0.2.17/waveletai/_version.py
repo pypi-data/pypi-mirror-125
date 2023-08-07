faa9eec9a9030c7191605f3429d8405aeca8e2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:03 +0000

    Add package waveletai/0.2.17/waveletai-0.2.17.tar.gz

---
+++ packages/waveletai/waveletai-0.2.17.tar.gz/waveletai-0.2.17/waveletai/_version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/4/6 6:10
+@Author  : WaveletAI-Product-Team Janus
+@license : (C) Copyright 2019-2022, Visionet(Tianjin)Information Technology Co.,Ltd.
+@Site    : plus.xiaobodata.com
+@File    : _version.py
+@Desc    : 
+"""
+
+VERSION = "0.2.17"
+
+
+def get_versions():
+    return VERSION
