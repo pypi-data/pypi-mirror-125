73818b7b4d85e4212d000722eaf48fbdd3c65607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:03:26 +0000

    Add package modern-logic-client/2.2.0/modern-logic-client-2.2.0.tar.gz

---
+++ packages/modern-logic-client/modern-logic-client-2.2.0.tar.gz/modern-logic-client-2.2.0/modern_logic_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from modern_logic_client.api.alerts_api import AlertsApi
+from modern_logic_client.api.customers_api import CustomersApi
+from modern_logic_client.api.data_sources_api import DataSourcesApi
+from modern_logic_client.api.executions_api import ExecutionsApi
+from modern_logic_client.api.webhooks_api import WebhooksApi
+from modern_logic_client.api.workflows_api import WorkflowsApi
