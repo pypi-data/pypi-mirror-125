137c28a16ba0b118acc0fcd03b73f38bbfc1a1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:33 +0000

    Add package ruslanpark-ulib/6.0/ruslanpark_ulib-6.0.tar.gz

---
+++ packages/ruslanpark-ulib/ruslanpark_ulib-6.0.tar.gz/ruslanpark_ulib-6.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import ruslanpark_ulib
+
+setup(
+    name='ruslanpark_ulib',
+    version='6.0',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    install_requires=['numpy<1.17'],
+)
