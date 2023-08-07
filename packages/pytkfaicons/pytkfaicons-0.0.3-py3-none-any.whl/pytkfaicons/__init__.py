b55da4ad2b15bd6e71d71fd247c8ac67bbd1e7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:03:36 +0000

    Add package pytkfaicons/0.0.3/pytkfaicons-0.0.3-py3-none-any.whl

---
+++ packages/pytkfaicons/pytkfaicons-0.0.3-py3-none-any.whl/pytkfaicons/__init__.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+ICONS = "icons"
+FONTS = "fonts"
+
+S_BRANDS = "brands"
+S_REGULAR = "regular"
+S_SOLID = "solid"
+
+FORMAT = "png"
+HEIGHT = 32
+
+VERSION = "v0.0.3"
+
+_icons = None
+
+_thisdir = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_icons():
+    global icons
+    if _icons is None:
+        read_icons()
+        assert _icons
+    return _icons
+
+
+def read_icons():
+    icons_src = os.path.join(_thisdir, ICONS, "icons.json")
+    with open(icons_src) as f:
+        global _icons
+        _icons = json.loads(f.read())
+
+
+def get_meta(name):
+    icons = get_icons()
+    ic = icons.get(name, None)
+    if ic is None:
+        raise Exception("not found", name)
+    return ic
---
+++ packages/pytkfaicons/pytkfaicons-0.0.3-py3-none-any.whl/pytkfaicons/__main__.py
@@ -0,0 +1,3 @@
+from .main import main_func
+
+main_func()
