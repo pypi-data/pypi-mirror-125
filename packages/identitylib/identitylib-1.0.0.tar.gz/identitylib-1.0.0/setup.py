cdbc75bcef58fdc9c6c7bc13d2d159d41eec3ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:03:22 +0000

    Add package identitylib/1.0.0/identitylib-1.0.0.tar.gz

---
+++ packages/identitylib/identitylib-1.0.0.tar.gz/identitylib-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='identitylib',
+    version='1.0.0',
+    description='identity function',
+    install_requires=[],
+    packages=['identity'],
+    author_email='avtahovfarit@gmail.com',
+)
