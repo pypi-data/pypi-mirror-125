81c20361eeee4fca43cdd129ac682a1db8334a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:53 +0000

    Add package felya2/0.1.0/felya2-0.1.0.tar.gz

---
+++ packages/felya2/felya2-0.1.0.tar.gz/felya2-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='felya2',
+    version='0.1.0',
+    description='A example Python package',    
+    author='Ofelia Epremyan',
+    install_requires=['numpy<1.15', 'statsmodels'],
+
+)
