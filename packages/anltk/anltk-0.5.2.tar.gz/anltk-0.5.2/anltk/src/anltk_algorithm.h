9c019b4cdf845b1de5c5b18f3db7a1e040f99700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:01:15 +0000

    Add package anltk/0.5.2/anltk-0.5.2.tar.gz

---
+++ packages/anltk/anltk-0.5.2.tar.gz/anltk-0.5.2/anltk/src/anltk_algorithm.h
@@ -0,0 +1,29 @@
+#ifndef ANLTK_ALGORIHTM_H
+#define ANLTK_ALGORIHTM_H
+
+#include "anltk/anltk_typedefs.h"
+#include "utf8.h"
+
+namespace anltk
+{
+template <typename Func>
+string_t anltk_erase_if(string_view_t input, Func&& f)
+{
+	auto start = input.begin();
+	auto end   = input.end();
+
+	string_t output;
+
+	while (start < end)
+	{
+		char_t next = utf8::next(start, end);
+		if (!f(next))
+		{
+			utf8::append(next, output);
+		}
+	}
+
+	return output;
+}
+} // namespace anltk
+#endif
