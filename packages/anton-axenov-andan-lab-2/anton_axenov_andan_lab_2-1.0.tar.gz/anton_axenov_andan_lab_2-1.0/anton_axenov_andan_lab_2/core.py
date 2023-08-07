da332124752ca513a99afc7249ad6d06a687f911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:26 +0000

    Add package anton-axenov-andan-lab-2/1.0/anton_axenov_andan_lab_2-1.0.tar.gz

---
+++ packages/anton-axenov-andan-lab-2/anton_axenov_andan_lab_2-1.0.tar.gz/anton_axenov_andan_lab_2-1.0/anton_axenov_andan_lab_2/core.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple, Union
+import math
+import numpy
+
+def fast_hist(array: List[Union[int, float]],
+              bins: int) -> Tuple[List[int], List[float]]:
+    
+    minBin = array.min()
+    binsContent = np.zeros((bins,), dtype=int)
+    bin_size = int(math.ceil((array.max()-minBin)/bins))
+
+    for element in array:
+        x = (element-minBin) // bin_size
+        binsContent[x]+=1
+    return (np.array([int(x) for x in binsContent]), [minBin + i*bin_size for i in range(0, bins)])
