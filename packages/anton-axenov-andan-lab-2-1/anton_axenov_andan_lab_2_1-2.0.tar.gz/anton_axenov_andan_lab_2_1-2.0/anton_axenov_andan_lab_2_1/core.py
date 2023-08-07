44020687ab89574a70c69f3335b5992e16099e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:52 +0000

    Add package anton-axenov-andan-lab-2-1/2.0/anton_axenov_andan_lab_2_1-2.0.tar.gz

---
+++ packages/anton-axenov-andan-lab-2-1/anton_axenov_andan_lab_2_1-2.0.tar.gz/anton_axenov_andan_lab_2_1-2.0/anton_axenov_andan_lab_2_1/core.py
@@ -0,0 +1,2 @@
+def lab_fun():
+    print("hello from anton_aksenov_andan_lab_2_1")
---
+++ packages/anton-axenov-andan-lab-2-1/anton_axenov_andan_lab_2_1-2.0.tar.gz/anton_axenov_andan_lab_2_1-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='anton_axenov_andan_lab_2_1',
+    version='2.0',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    install_requires=['scipy>1.7.0'],
+)
