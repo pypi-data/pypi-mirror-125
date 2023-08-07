7f33523a581ec7ba939805fe2b066f2d43d36e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:33 +0000

    Add package sokolpackage/0.1.0/sokolpackage-0.1.0.tar.gz

---
+++ packages/sokolpackage/sokolpackage-0.1.0.tar.gz/sokolpackage-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sokolpackage',
+    version='0.1.0',    
+    description='A example Python package',
+    url='',
+    author='Alexandr Sokolov',
+    author_email='sokolov-19.07@yandex.ru',
+    packages=['sokolpackage'],
+    requires=['pandas'],
+)
