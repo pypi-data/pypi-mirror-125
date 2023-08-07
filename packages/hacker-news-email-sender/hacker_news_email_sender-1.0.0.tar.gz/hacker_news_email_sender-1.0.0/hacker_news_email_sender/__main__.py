d434a9d58c759816533bc4b269aeafa8810ca0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:05 +0000

    Add package hacker-news-email-sender/1.0.0/hacker_news_email_sender-1.0.0.tar.gz

---
+++ packages/hacker-news-email-sender/hacker_news_email_sender-1.0.0.tar.gz/hacker_news_email_sender-1.0.0/hacker_news_email_sender/__main__.py
@@ -0,0 +1,36 @@
+import argparse
+from . import newsExctractor
+
+
+def main() -> None:  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m hacker_news_email_sender` and `$ hacker_news_email_sender `.
+
+    The program's entry point.
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Extract and send the Hacker News to an email.'
+    )
+    parser.add_argument(
+        "sender",
+        type=str,
+        help='The email address used to send the email'
+    )
+
+    parser.add_argument(
+        "receiver",
+        type=str,
+        help='The email address that will receive the news.'
+    )
+    args = parser.parse_args()
+    if args.sender and args.receiver:
+        extractor = newsExctractor.News_extractor()
+        extractor.extract_news()
+        extractor.send_email(args.sender, args.receiver)
+        print("\nEmail successfuly sended!")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
