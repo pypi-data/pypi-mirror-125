d0dadd3e4d65b40f697ba41e1fe6a1032db0f854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:04:03 +0000

    Add package Perceptron-pypi-gaurav98094/0.0.2/Perceptron_pypi-gaurav98094-0.0.2.tar.gz

---
+++ packages/Perceptron-pypi-gaurav98094/Perceptron_pypi-gaurav98094-0.0.2.tar.gz/Perceptron_pypi-gaurav98094-0.0.2/README.md
@@ -0,0 +1,15 @@
+# Perceptron Learning ALgorithms
+Implement your perceptron learning algorithms with ease.
+
+
+# Install
+```
+pip install Perceptron-pypi-gaurav98094
+``
+
+# Usage
+```
+from oneNeuron.perceptron import Perceptron
+model = Perceptron()
+model.fit(X,y,eta=0.01,epochs=10) 
+```
---
+++ packages/Perceptron-pypi-gaurav98094/Perceptron_pypi-gaurav98094-0.0.2.tar.gz/Perceptron_pypi-gaurav98094-0.0.2/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+PROJECT_NAME = 'Perceptron_pypi'
+USER_NAME = 'gaurav98094'
+
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.2",
+    author=f"{USER_NAME}",
+    author_email="kandelgaurav7@gmail.com",
+    description="Package to implement perceptron learning algorithms.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "tqdm"
+    ]
+    
+)
