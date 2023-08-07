8989abf0fde5f348db8cad731e41c9be774b615f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:16 +0000

    Add package ivy/6.0.0/ivy-6.0.0-py3-none-any.whl

---
+++ packages/ivy/ivy-6.0.0-py3-none-any.whl/ivy/ext/ivy_markdown.py
@@ -0,0 +1,19 @@
+import ivy
+
+try:
+    import markdown
+except ImportError:
+    markdown = None
+
+if markdown:
+
+    # Check the config file for custom settings for the markdown renderer.
+    settings = ivy.site.config.get('markdown_settings') or {}
+
+    # Initialize a markdown renderer.
+    renderer = markdown.Markdown(**settings)
+
+    # Register a callback to render files with a .md extension.
+    @ivy.renderers.register('md')
+    def render(text):
+        return renderer.reset().convert(text)
