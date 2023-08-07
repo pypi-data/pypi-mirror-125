522c958707a13cdf00d6f3af6abeead74e10d089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:14 +0000

    Add package pyconversions/0.0.3/pyconversions-0.0.3.tar.gz

---
+++ packages/pyconversions/pyconversions-0.0.3.tar.gz/pyconversions-0.0.3/libname/__init__.py
@@ -0,0 +1,11 @@
+import pyconversions
+from pyconversions import Inches
+from pyconversions import Feet
+from pyconversions import Yard
+from pyconversions import Millimeter
+from pyconversions import Centimeter
+from pyconversions import Decimeter
+from pyconversions import Decameter
+from pyconversions import Hectometer
+from pyconversions import Meter
+from pyconversions import Kilometers
