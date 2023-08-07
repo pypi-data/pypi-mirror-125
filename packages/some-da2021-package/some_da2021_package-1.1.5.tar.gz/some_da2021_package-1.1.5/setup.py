79b05c4e4ceea86122205f55f1ce9b65fda63d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:32 +0000

    Add package some-da2021-package/1.1.5/some_da2021_package-1.1.5.tar.gz

---
+++ packages/some-da2021-package/some_da2021_package-1.1.5.tar.gz/some_da2021_package-1.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='some_da2021_package',
+    version='1.1.5',  
+    url='https://github.com/lejabque',
+    description='A example Python package',
+    author='Denis Vorkozhokov',
+    author_email='vorkdenis1@gmail.com',
+    license='MIT',
+    packages=['some_da2021_package'],
+    install_requires=['scipy==1.7.1'],
+)
