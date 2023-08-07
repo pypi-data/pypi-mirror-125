181a597dbdb05edfd9317071b19d16ceafa6f182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 02:02:17 +0000

    Add package discord-continued/1.0.0/discord-continued-1.0.0.tar.gz

---
+++ packages/discord-continued/discord-continued-1.0.0.tar.gz/discord-continued-1.0.0/README.md
@@ -0,0 +1,8 @@
+discord-continued
+====================
+
+**A simple API wrapper for Discord interactions.**
+
+Forked from [Discord Interactions](https://github.com/goverfl0w/discord-interactions/tree/unstable)
+
+Work in progress
---
+++ packages/discord-continued/discord-continued-1.0.0.tar.gz/discord-continued-1.0.0/discord/__init__.py
@@ -0,0 +1,13 @@
+"""
+discord-continued
+
+An API wrapper for Discord.
+(c) 2021 sakurazaki.
+"""
+from .api import *  # noqa: F401 F403
+from .base import *  # noqa: F401 F403
+from .client import *  # noqa: F401 F403
+from .context import *  # noqa: F401 F403
+from .enums import *  # noqa: F401 F403
+from .ext import *  # noqa: F401 F403
+from .models import *  # noqa: F401 F403
