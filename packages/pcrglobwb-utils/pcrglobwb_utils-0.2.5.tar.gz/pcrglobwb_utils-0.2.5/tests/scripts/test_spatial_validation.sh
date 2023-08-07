c1ec5c7a0cf7417d37c679191f5f5cfb2c23adb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:58 +0000

    Add package pcrglobwb-utils/0.2.5/pcrglobwb_utils-0.2.5.tar.gz

---
+++ packages/pcrglobwb-utils/pcrglobwb_utils-0.2.5.tar.gz/pcrglobwb_utils-0.2.5/tests/scripts/test_spatial_validation.sh
@@ -0,0 +1,27 @@
+shp='../../examples/example_data/Tanzania_shp/waterProvinces.shp'
+obs='../../examples/example_data/GRACE/GRACE_data_2010_Tanzania.nc'
+sim='../../examples/example_data/GRACE/totalWaterStorageThickness_monthAvg_output_2010_Tanzania.nc'
+out='./eval_GRACE'
+
+echo
+echo VALIDATING WITH GRACE
+
+pcru_eval_poly -o lwe_thickness -s total_thickness_of_water_storage -cf 100 -id watprovID --plot --anomaly --verbose $shp $sim $obs $out
+
+obs='../../examples/example_data/GLEAM/GLEAM_data_2010_Tanzania.nc'
+sim='../../examples/example_data/GLEAM/totalEvaporation_monthTot_output_2010_Tanzania.nc'
+out='./eval_GLEAM'
+
+echo
+echo VALIDATING WITH GLEAM
+
+pcru_eval_poly -o E -s total_evaporation -cf 1000 -id watprovID --plot $shp $sim $obs $out
+
+obs='../../examples/example_data/GLEAM/GLEAM_data_2010_Tanzania_annualMean.nc'
+sim='../../examples/example_data/GLEAM/totalEvaporation_monthTot_output_2010_Tanzania_annualMean.nc'
+out='./eval_GLEAM_annualMean'
+
+echo
+echo VALIDATING WITH GLEAM - ANNUAL TIMESTEP
+
+pcru_eval_poly -o E -s total_evaporation -cf 1000 -id watprovID -tstep annual --plot $shp $sim $obs $out
