a881b353387a73f06bbad31a00da9c5eec55e5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:23 +0000

    Add package yt-idefix/0.6.9/yt_idefix-0.6.9.tar.gz

---
+++ packages/yt-idefix/yt_idefix-0.6.9.tar.gz/yt_idefix-0.6.9/yt_idefix/_io/commons.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import sys
+from enum import IntEnum
+from typing import Any, Dict, NamedTuple, Tuple
+
+import numpy as np
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+
+    Prec = Literal["d", "f", "i"]
+    Dim = Literal[1, 2, 3]
+else:
+    Prec = str
+    Dim = int
+
+
+class Shape(NamedTuple):
+    n1: int = 1
+    n2: int = 1
+    n3: int = 1
+
+    @property
+    def size(self):
+        return self.n1 * self.n2 * self.n3
+
+    def to_cell_centered(self) -> Shape:
+        vals: list[int] = [1, 1, 1]
+        for i, attr in enumerate(self):
+            vals[i] = max(1, attr - 1)
+        return Shape(*vals)
+
+
+class Coordinates(NamedTuple):
+    x1: np.ndarray
+    x2: np.ndarray
+    x3: np.ndarray
+
+    @property
+    def shape(self) -> Shape:
+        return Shape(len(self.x1), len(self.x2), len(self.x3))
+
+
+# map field name to numpy array init data:
+# precision (-> datatype), dimensionality, [nx, ny, nz]
+# the np.ndarray is assumed to contain *dim* elements
+IdefixFieldProperties = Dict[str, Tuple[Prec, Dim, np.ndarray]]
+
+# Map various str keys to scalars and arrays
+IdefixMetadata = Dict[str, Any]
+
+
+class ByteSize(IntEnum):
+    """Constant byte size for data types"""
+
+    CHAR = 1
+    INT = 4
+    FLOAT = 4
+    DOUBLE = 8
